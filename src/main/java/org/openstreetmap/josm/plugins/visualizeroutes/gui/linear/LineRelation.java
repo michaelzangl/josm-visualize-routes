JOSM/pt_assistant

Use platform in relations

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/LineRelation.java
@@ -55,7 +55,7 @@ public class LineRelation {
     protected Stream<RelationMember> streamRawStops() {
         return getRelation().getMembers()
             .stream()
-            .filter(it -> OSMTags.STOP_ROLES.contains(it.getRole()));
+            .filter(it -> OSMTags.STOPS_AND_PLATFORMS_ROLES.contains(it.getRole()));
     }
 
     public static class StopPositionEvent {

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/LineRelationTabManager.java
@@ -104,8 +104,7 @@ public class LineRelationTabManager extends AbstractTabManager<PublicTransportLi
                             // TODO: Platforms?
                             List<Relation> routeRelations = getRouteRelations(
                                 RelationEditorAccessUtils.streamMembers(editorAccess)
-                                .filter(it -> OSMTags.STOP_ROLES.contains(it.getRole())
-                                    || OSMTags.PLATFORM_ROLES.contains(it.getRole()))
+                                .filter(it -> OSMTags.STOPS_AND_PLATFORMS_ROLES.contains(it.getRole()))
                                 .flatMap(it -> it.getMember().getReferrers().stream()))
                                 .distinct()
                                 .collect(Collectors.toList());

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/StopCollector.java
@@ -213,7 +213,7 @@ public class StopCollector {
 
         CollectedStopsForRelation(LineRelation line) {
             this.line = line;
-            this.priority = line.getRelation().getMembers().stream().filter(it -> OSMTags.STOP_ROLES.contains(it.getRole())).count()
+            this.priority = line.getRelation().getMembers().stream().filter(it -> OSMTags.STOPS_AND_PLATFORMS_ROLES.contains(it.getRole())).count()
                 + (line.isPrimary() ? 1_000_000 : 0);
         }
 
@@ -264,7 +264,7 @@ public class StopCollector {
         private final RelationMember member;
 
         FoundStopPosition(RelationMember member) {
-            if (!OSMTags.STOP_ROLES.contains(member.getRole())) {
+            if (!OSMTags.STOPS_AND_PLATFORMS_ROLES.contains(member.getRole())) {
                 throw new IllegalArgumentException("Not a stop position: " + member);
             }
             this.member = member;

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/LineRelation.java
@@ -1,6 +1,5 @@
 package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
 
-import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
 import org.openstreetmap.josm.tools.ColorHelper;
@@ -54,7 +53,7 @@ public class LineRelation {
     protected Stream<RelationMember> streamRawStops() {
         return getRelation().getMembers()
             .stream()
-            .filter(it -> OsmRouteRelationTags.STOP_ROLES.contains(it.getRole()));
+            .filter(it -> OsmRouteRelationTags.STOP_AND_PLATFORM_ROLES.contains(it.getRole()));
     }
 
     public static class StopPositionEvent {

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/LineRelationTabManager.java
@@ -102,8 +102,7 @@ public class LineRelationTabManager extends AbstractTabManager<PublicTransportLi
                             // TODO: Platforms?
                             List<Relation> routeRelations = getRouteRelations(
                                 RelationEditorAccessUtils.streamMembers(editorAccess)
-                                .filter(it -> OsmRouteRelationTags.STOP_ROLES.contains(it.getRole())
-                                    || OsmRouteRelationTags.PLATFORM_ROLES.contains(it.getRole()))
+                                .filter(it -> OsmRouteRelationTags.STOP_AND_PLATFORM_ROLES.contains(it.getRole()))
                                 .flatMap(it -> it.getMember().getReferrers().stream()))
                                 .distinct()
                                 .collect(Collectors.toList());

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/StopCollector.java
@@ -211,7 +211,7 @@ public class StopCollector {
 
         CollectedStopsForRelation(LineRelation line) {
             this.line = line;
-            this.priority = line.getRelation().getMembers().stream().filter(it -> OsmRouteRelationTags.STOP_ROLES.contains(it.getRole())).count()
+            this.priority = line.getRelation().getMembers().stream().filter(it -> OsmRouteRelationTags.STOP_AND_PLATFORM_ROLES.contains(it.getRole())).count()
                 + (line.isPrimary() ? 1_000_000 : 0);
         }
 
@@ -262,7 +262,7 @@ public class StopCollector {
         private final RelationMember member;
 
         FoundStopPosition(RelationMember member) {
-            if (!OsmRouteRelationTags.STOP_ROLES.contains(member.getRole())) {
+            if (!OsmRouteRelationTags.STOP_AND_PLATFORM_ROLES.contains(member.getRole())) {
                 throw new IllegalArgumentException("Not a stop position: " + member);
             }
             this.member = member;