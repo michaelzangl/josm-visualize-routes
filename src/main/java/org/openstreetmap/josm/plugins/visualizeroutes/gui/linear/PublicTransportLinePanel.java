JOSM/pt_assistant

Make stop vicinity view interactive.

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/PublicTransportLinePanel.java
@@ -1,6 +1,6 @@
 package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
 
-import static org.openstreetmap.josm.plugins.pt_assistant.utils.RouteUtils.isVersionTwoPTRoute;
+import static org.openstreetmap.josm.plugins.pt_assistant.gui.linear.UnBoldLabel.safeHtml;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.Dimension;
@@ -8,7 +8,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.text.MessageFormat;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -19,7 +18,6 @@ import javax.swing.AbstractAction;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -27,10 +25,7 @@ import org.openstreetmap.josm.actions.relation.EditRelationAction;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 import org.openstreetmap.josm.data.osm.Relation;
-import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
-import org.openstreetmap.josm.plugins.pt_assistant.utils.RouteUtils;
-import org.openstreetmap.josm.plugins.pt_assistant.utils.StopUtils;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.ImageProvider;
 
@@ -42,20 +37,11 @@ public class PublicTransportLinePanel extends JPanel {
     public PublicTransportLinePanel(LineRelationsProvider p) {
         Optional<Relation> master = Objects.requireNonNull(p.getMasterRelation(), "p.getMasterRelation()");
         List<LineRelation> relations = Objects.requireNonNull(p.getRelations(), "p.getRelations()");
-        String color = master.map(it -> it.get("colour")).filter(Objects::nonNull).orElse("#888888");
 
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         setBorder(new EmptyBorder(5, 5, 5, 5));
 
-        if (master.isPresent()) {
-            Relation m = master.get();
-            String headline = MessageFormat.format("<font background=\"{0}\">{1}</font> {2}", safeHtml(color), safeHtml(m.get("ref")), safeHtml(m.get("name")));
-            String infos = safeHtml(m.get("operator")) + " " + safeHtml(m.get("network"));
-            String routeMasterText = MessageFormat.format("<html><h2>{0}</h2><div>{1}</div></html>", headline, infos);
-            add(new UnBoldLabel(routeMasterText));
-        } else {
-            add(new UnBoldLabel(tr("Route that is not in a parent relation")));
-        }
+        add(p.createHeadlinePanel());
 
         add(Box.createRigidArea(new Dimension(0, 5)));
 
@@ -139,25 +125,8 @@ public class PublicTransportLinePanel extends JPanel {
         });
     }
 
-    private String safeHtml(String text) {
-        return text == null ? "" : text.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;");
-    }
-
     public static boolean isRouteMaster(OsmPrimitive relation) {
         return relation.getType() == OsmPrimitiveType.RELATION && relation.hasTag(OSMTags.KEY_RELATION_TYPE, OSMTags.VALUE_TYPE_ROUTE_MASTER);
-    }
-
-    private static class UnBoldLabel extends JLabel {
-        public UnBoldLabel(String text) {
-            super(text);
-            setHorizontalAlignment(LEFT);
-            setFont(getFont().deriveFont(0));
-        }
-
-        @Override
-        public Dimension getMaximumSize() {
-            return new Dimension(Integer.MAX_VALUE, super.getMaximumSize().height);
-        }
     }
 
 }

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/UnBoldLabel.java
@@ -1,0 +1,23 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+
+class UnBoldLabel extends JLabel {
+    public UnBoldLabel(String text) {
+        super(text);
+        setHorizontalAlignment(LEFT);
+        setFont(getFont().deriveFont(0));
+    }
+
+    @Override
+    public Dimension getMaximumSize() {
+        return new Dimension(Integer.MAX_VALUE, super.getMaximumSize().height);
+    }
+
+    public static String safeHtml(String text) {
+        return text == null ? "" : text.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;");
+    }
+
+}

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/PublicTransportLinePanel.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.UnBoldLabel.safeHtml;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 /**
@@ -29,20 +30,11 @@ public class PublicTransportLinePanel extends JPanel {
     public PublicTransportLinePanel(LineRelationsProvider p) {
         Optional<Relation> master = Objects.requireNonNull(p.getMasterRelation(), "p.getMasterRelation()");
         List<LineRelation> relations = Objects.requireNonNull(p.getRelations(), "p.getRelations()");
-        String color = master.map(it -> it.get("colour")).filter(Objects::nonNull).orElse("#888888");
 
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         setBorder(new EmptyBorder(5, 5, 5, 5));
 
-        if (master.isPresent()) {
-            Relation m = master.get();
-            String headline = MessageFormat.format("<font background=\"{0}\">{1}</font> {2}", safeHtml(color), safeHtml(m.get("ref")), safeHtml(m.get("name")));
-            String infos = safeHtml(m.get("operator")) + " " + safeHtml(m.get("network"));
-            String routeMasterText = MessageFormat.format("<html><h2>{0}</h2><div>{1}</div></html>", headline, infos);
-            add(new UnBoldLabel(routeMasterText));
-        } else {
-            add(new UnBoldLabel(tr("Route that is not in a parent relation")));
-        }
+        add(p.createHeadlinePanel());
 
         add(Box.createRigidArea(new Dimension(0, 5)));
 
@@ -126,25 +118,8 @@ public class PublicTransportLinePanel extends JPanel {
         });
     }
 
-    private String safeHtml(String text) {
-        return text == null ? "" : text.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;");
-    }
-
     public static boolean isRouteMaster(OsmPrimitive relation) {
         return relation.getType() == OsmPrimitiveType.RELATION && OsmRouteMasterRelationTags.isRouteMaster(relation);
-    }
-
-    private static class UnBoldLabel extends JLabel {
-        public UnBoldLabel(String text) {
-            super(text);
-            setHorizontalAlignment(LEFT);
-            setFont(getFont().deriveFont(0));
-        }
-
-        @Override
-        public Dimension getMaximumSize() {
-            return new Dimension(Integer.MAX_VALUE, super.getMaximumSize().height);
-        }
     }
 
 }

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/UnBoldLabel.java
@@ -1,0 +1,23 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+
+class UnBoldLabel extends JLabel {
+    public UnBoldLabel(String text) {
+        super(text);
+        setHorizontalAlignment(LEFT);
+        setFont(getFont().deriveFont(0));
+    }
+
+    @Override
+    public Dimension getMaximumSize() {
+        return new Dimension(Integer.MAX_VALUE, super.getMaximumSize().height);
+    }
+
+    public static String safeHtml(String text) {
+        return text == null ? "" : text.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;");
+    }
+
+}