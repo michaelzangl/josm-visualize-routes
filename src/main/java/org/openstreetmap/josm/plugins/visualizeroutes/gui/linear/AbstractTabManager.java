JOSM/pt_assistant

Add vicinity rendering to public transport areas

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/AbstractTabManager.java
@@ -1,0 +1,85 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+
+import java.awt.Container;
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeListener;
+
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.gui.dialogs.relation.RelationEditor;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+
+/**
+ * Allows adding / removing a tab from the Relation window.
+ */
+public abstract class AbstractTabManager {
+    private final JTabbedPane tabPanel;
+    private final Supplier<Relation> relationGetter;
+    private JScrollPane tabContent = null;
+    private ChangeListener tabListener;
+
+    public AbstractTabManager(IRelationEditorActionAccess editorAccess) {
+        Container editorComponent = ((JDialog) editorAccess.getEditor()).getContentPane();
+
+        this.tabPanel = Stream.of(editorComponent.getComponents())
+            .filter(it -> it instanceof JTabbedPane)
+            .map(it -> (JTabbedPane) it)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Relation editor did not have a tab panel"));
+
+        this.relationGetter = editorAccess.getEditor()::getRelation;
+
+        updateTab();
+        editorComponent.addPropertyChangeListener(RelationEditor.RELATION_PROP, __ -> updateTab());
+        editorAccess.getMemberTableModel().addTableModelListener(__ -> updateTab());
+        editorAccess.getTagModel().addPropertyChangeListener(__ -> updateTab());
+    }
+
+    private void updateTab() {
+        Relation relation = relationGetter.get();
+        TabAndDisplay toShow = getTabToShow(relation);
+
+        if (toShow.shouldDisplay()) {
+            if (tabContent == null) {
+                tabContent = new JScrollPane();
+                tabPanel.add(toShow.getTitle(), tabContent);
+                tabListener = e -> {
+                    showIfVisible(toShow);
+                };
+                tabPanel.addChangeListener(tabListener);
+            }
+            this.tabContent.getViewport().setView(null);
+            // This makes adding the component lazy => complex layouts don't need to be computed immediately
+            showIfVisible(toShow);
+        } else {
+            if (tabContent != null) {
+                tabPanel.remove(tabContent);
+                tabPanel.removeChangeListener(tabListener);
+                tabContent = null;
+            }
+        }
+    }
+
+    private void showIfVisible(TabAndDisplay toShow) {
+        if (tabPanel.getSelectedComponent() == tabContent
+                && this.tabContent.getViewport().getView() == null) {
+            JPanel newContent = toShow.getTabContent();
+            Objects.requireNonNull(newContent, "newContent");
+            this.tabContent.getViewport().setView(newContent);
+        }
+    }
+
+    protected abstract TabAndDisplay getTabToShow(Relation relation);
+
+    public interface TabAndDisplay {
+        boolean shouldDisplay();
+        JPanel getTabContent();
+        String getTitle();
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/stopvicinity/DataSetClone.java
@@ -1,0 +1,154 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.stopvicinity;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.data.DataSource;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.DownloadPolicy;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.data.osm.UploadPolicy;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.data.osm.event.AbstractDatasetChangedEvent;
+import org.openstreetmap.josm.data.osm.event.DataChangedEvent;
+import org.openstreetmap.josm.data.osm.event.DataSetListener;
+import org.openstreetmap.josm.data.osm.event.DataSourceAddedEvent;
+import org.openstreetmap.josm.data.osm.event.NodeMovedEvent;
+import org.openstreetmap.josm.data.osm.event.PrimitivesAddedEvent;
+import org.openstreetmap.josm.data.osm.event.PrimitivesRemovedEvent;
+import org.openstreetmap.josm.data.osm.event.RelationMembersChangedEvent;
+import org.openstreetmap.josm.data.osm.event.TagsChangedEvent;
+import org.openstreetmap.josm.data.osm.event.WayNodesChangedEvent;
+
+/**
+ * An OSM dataset may not be used for two renderers (global things like style cache)
+ *
+ * So we need to do a read-only copy of it and sync it with the main data set.
+ *
+ * Advantage for us: We have our own highlight flags, our own other stuff.
+ */
+public class DataSetClone {
+    private final DataSet copyFrom;
+    private final Collection<OsmPrimitive> toSelect;
+    private final DataSet clone = new DataSet();
+    private final Map<OsmPrimitive, OsmPrimitive> originalToCopy = new HashMap<>();
+
+    // TODO: Optimize some of these to make them faster
+    private final DataSetListener dataSetListener = new DataSetListener() {
+        @Override
+        public void primitivesAdded(PrimitivesAddedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void primitivesRemoved(PrimitivesRemovedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void tagsChanged(TagsChangedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void nodeMoved(NodeMovedEvent event) {
+            Node myNode = (Node) originalToCopy.get(event.getNode());
+            if (myNode != null) {
+                myNode.setCoor(event.getNode().getCoor());
+            }
+            refreshAll();
+        }
+
+        @Override
+        public void wayNodesChanged(WayNodesChangedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void relationMembersChanged(RelationMembersChangedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void otherDatasetChange(AbstractDatasetChangedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void dataChanged(DataChangedEvent event) {
+            refreshAll();
+        }
+    };
+
+    public DataSetClone(DataSet copyFrom, Collection<OsmPrimitive> toSelect) {
+        this.copyFrom = copyFrom;
+        this.toSelect = toSelect;
+        copyFrom.addDataSetListener(dataSetListener);
+        refreshAll();
+        clone.setUploadPolicy(UploadPolicy.BLOCKED);
+        clone.setDownloadPolicy(DownloadPolicy.BLOCKED);
+    }
+
+    private void refreshAll() {
+        // Code copied from new DataSet(DataSet) constructor
+        copyFrom.getReadLock().lock();
+        try {
+            clone.clear();
+            originalToCopy.clear();
+
+            for (Node n : copyFrom.getNodes()) {
+                Node newNode = new Node(n);
+                originalToCopy.put(n, newNode);
+                clone.addPrimitive(newNode);
+            }
+            for (Way w : copyFrom.getWays()) {
+                Way newWay = new Way(w, false, false);
+                originalToCopy.put(w, newWay);
+                List<Node> newNodes = w.getNodes().stream()
+                    .map(n -> (Node) originalToCopy.get(n))
+                    .collect(Collectors.toList());
+                newWay.setNodes(newNodes);
+                clone.addPrimitive(newWay);
+            }
+            // Because relations can have other relations as members we first clone all relations
+            // and then get the cloned members
+            Collection<Relation> relations = copyFrom.getRelations();
+            for (Relation r : relations) {
+                Relation newRelation = new Relation(r, false, false);
+                originalToCopy.put(r, newRelation);
+                clone.addPrimitive(newRelation);
+            }
+            for (Relation r : relations) {
+                ((Relation) originalToCopy.get(r)).setMembers(r.getMembers().stream()
+                    .map(rm -> new RelationMember(rm.getRole(), originalToCopy.get(rm.getMember())))
+                    .collect(Collectors.toList()));
+            }
+            // For now, ignore the part of the selection that got deleted/changed in parent
+            clone.setSelected(toSelect.stream()
+                .map(originalToCopy::get)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList()));
+            // Ignore data source
+            clone.setVersion(copyFrom.getVersion());
+        } finally {
+            copyFrom.getReadLock().unlock();
+        }
+    }
+
+    public DataSet getClone() {
+        return clone;
+    }
+
+    public void dispose() {
+        copyFrom.removeDataSetListener(dataSetListener);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/stopvicinity/StopVicinityTabManager.java
@@ -1,0 +1,36 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.stopvicinity;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import javax.swing.JPanel;
+
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.AbstractTabManager;
+import org.openstreetmap.josm.plugins.pt_assistant.utils.StopUtils;
+
+public class StopVicinityTabManager extends AbstractTabManager {
+    public StopVicinityTabManager(IRelationEditorActionAccess editorAccess) {
+        super(editorAccess);
+    }
+
+    @Override
+    protected TabAndDisplay getTabToShow(Relation relation) {
+        return new TabAndDisplay() {
+            @Override
+            public boolean shouldDisplay() {
+                return StopUtils.isStopArea(relation);
+            }
+
+            @Override
+            public JPanel getTabContent() {
+                return new StopVicinityPanel(relation);
+            }
+
+            @Override
+            public String getTitle() {
+                return tr("Vicinity");
+            }
+        };
+    }
+}

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/AbstractTabManager.java
@@ -1,0 +1,85 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+
+import java.awt.Container;
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeListener;
+
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.gui.dialogs.relation.RelationEditor;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+
+/**
+ * Allows adding / removing a tab from the Relation window.
+ */
+public abstract class AbstractTabManager {
+    private final JTabbedPane tabPanel;
+    private final Supplier<Relation> relationGetter;
+    private JScrollPane tabContent = null;
+    private ChangeListener tabListener;
+
+    public AbstractTabManager(IRelationEditorActionAccess editorAccess) {
+        Container editorComponent = ((JDialog) editorAccess.getEditor()).getContentPane();
+
+        this.tabPanel = Stream.of(editorComponent.getComponents())
+            .filter(it -> it instanceof JTabbedPane)
+            .map(it -> (JTabbedPane) it)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Relation editor did not have a tab panel"));
+
+        this.relationGetter = editorAccess.getEditor()::getRelation;
+
+        updateTab();
+        editorComponent.addPropertyChangeListener(RelationEditor.RELATION_PROP, __ -> updateTab());
+        editorAccess.getMemberTableModel().addTableModelListener(__ -> updateTab());
+        editorAccess.getTagModel().addPropertyChangeListener(__ -> updateTab());
+    }
+
+    private void updateTab() {
+        Relation relation = relationGetter.get();
+        TabAndDisplay toShow = getTabToShow(relation);
+
+        if (toShow.shouldDisplay()) {
+            if (tabContent == null) {
+                tabContent = new JScrollPane();
+                tabPanel.add(toShow.getTitle(), tabContent);
+                tabListener = e -> {
+                    showIfVisible(toShow);
+                };
+                tabPanel.addChangeListener(tabListener);
+            }
+            this.tabContent.getViewport().setView(null);
+            // This makes adding the component lazy => complex layouts don't need to be computed immediately
+            showIfVisible(toShow);
+        } else {
+            if (tabContent != null) {
+                tabPanel.remove(tabContent);
+                tabPanel.removeChangeListener(tabListener);
+                tabContent = null;
+            }
+        }
+    }
+
+    private void showIfVisible(TabAndDisplay toShow) {
+        if (tabPanel.getSelectedComponent() == tabContent
+                && this.tabContent.getViewport().getView() == null) {
+            JPanel newContent = toShow.getTabContent();
+            Objects.requireNonNull(newContent, "newContent");
+            this.tabContent.getViewport().setView(newContent);
+        }
+    }
+
+    protected abstract TabAndDisplay getTabToShow(Relation relation);
+
+    public interface TabAndDisplay {
+        boolean shouldDisplay();
+        JPanel getTabContent();
+        String getTitle();
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/stopvicinity/DataSetClone.java
@@ -1,0 +1,154 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.stopvicinity;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.data.DataSource;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.DownloadPolicy;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.data.osm.UploadPolicy;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.data.osm.event.AbstractDatasetChangedEvent;
+import org.openstreetmap.josm.data.osm.event.DataChangedEvent;
+import org.openstreetmap.josm.data.osm.event.DataSetListener;
+import org.openstreetmap.josm.data.osm.event.DataSourceAddedEvent;
+import org.openstreetmap.josm.data.osm.event.NodeMovedEvent;
+import org.openstreetmap.josm.data.osm.event.PrimitivesAddedEvent;
+import org.openstreetmap.josm.data.osm.event.PrimitivesRemovedEvent;
+import org.openstreetmap.josm.data.osm.event.RelationMembersChangedEvent;
+import org.openstreetmap.josm.data.osm.event.TagsChangedEvent;
+import org.openstreetmap.josm.data.osm.event.WayNodesChangedEvent;
+
+/**
+ * An OSM dataset may not be used for two renderers (global things like style cache)
+ *
+ * So we need to do a read-only copy of it and sync it with the main data set.
+ *
+ * Advantage for us: We have our own highlight flags, our own other stuff.
+ */
+public class DataSetClone {
+    private final DataSet copyFrom;
+    private final Collection<OsmPrimitive> toSelect;
+    private final DataSet clone = new DataSet();
+    private final Map<OsmPrimitive, OsmPrimitive> originalToCopy = new HashMap<>();
+
+    // TODO: Optimize some of these to make them faster
+    private final DataSetListener dataSetListener = new DataSetListener() {
+        @Override
+        public void primitivesAdded(PrimitivesAddedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void primitivesRemoved(PrimitivesRemovedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void tagsChanged(TagsChangedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void nodeMoved(NodeMovedEvent event) {
+            Node myNode = (Node) originalToCopy.get(event.getNode());
+            if (myNode != null) {
+                myNode.setCoor(event.getNode().getCoor());
+            }
+            refreshAll();
+        }
+
+        @Override
+        public void wayNodesChanged(WayNodesChangedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void relationMembersChanged(RelationMembersChangedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void otherDatasetChange(AbstractDatasetChangedEvent event) {
+            refreshAll();
+        }
+
+        @Override
+        public void dataChanged(DataChangedEvent event) {
+            refreshAll();
+        }
+    };
+
+    public DataSetClone(DataSet copyFrom, Collection<OsmPrimitive> toSelect) {
+        this.copyFrom = copyFrom;
+        this.toSelect = toSelect;
+        copyFrom.addDataSetListener(dataSetListener);
+        refreshAll();
+        clone.setUploadPolicy(UploadPolicy.BLOCKED);
+        clone.setDownloadPolicy(DownloadPolicy.BLOCKED);
+    }
+
+    private void refreshAll() {
+        // Code copied from new DataSet(DataSet) constructor
+        copyFrom.getReadLock().lock();
+        try {
+            clone.clear();
+            originalToCopy.clear();
+
+            for (Node n : copyFrom.getNodes()) {
+                Node newNode = new Node(n);
+                originalToCopy.put(n, newNode);
+                clone.addPrimitive(newNode);
+            }
+            for (Way w : copyFrom.getWays()) {
+                Way newWay = new Way(w, false, false);
+                originalToCopy.put(w, newWay);
+                List<Node> newNodes = w.getNodes().stream()
+                    .map(n -> (Node) originalToCopy.get(n))
+                    .collect(Collectors.toList());
+                newWay.setNodes(newNodes);
+                clone.addPrimitive(newWay);
+            }
+            // Because relations can have other relations as members we first clone all relations
+            // and then get the cloned members
+            Collection<Relation> relations = copyFrom.getRelations();
+            for (Relation r : relations) {
+                Relation newRelation = new Relation(r, false, false);
+                originalToCopy.put(r, newRelation);
+                clone.addPrimitive(newRelation);
+            }
+            for (Relation r : relations) {
+                ((Relation) originalToCopy.get(r)).setMembers(r.getMembers().stream()
+                    .map(rm -> new RelationMember(rm.getRole(), originalToCopy.get(rm.getMember())))
+                    .collect(Collectors.toList()));
+            }
+            // For now, ignore the part of the selection that got deleted/changed in parent
+            clone.setSelected(toSelect.stream()
+                .map(originalToCopy::get)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList()));
+            // Ignore data source
+            clone.setVersion(copyFrom.getVersion());
+        } finally {
+            copyFrom.getReadLock().unlock();
+        }
+    }
+
+    public DataSet getClone() {
+        return clone;
+    }
+
+    public void dispose() {
+        copyFrom.removeDataSetListener(dataSetListener);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/stopvicinity/StopVicinityTabManager.java
@@ -1,0 +1,36 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.stopvicinity;
+
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmStopAreaRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.AbstractTabManager;
+
+import javax.swing.*;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+public class StopVicinityTabManager extends AbstractTabManager {
+    public StopVicinityTabManager(IRelationEditorActionAccess editorAccess) {
+        super(editorAccess);
+    }
+
+    @Override
+    protected TabAndDisplay getTabToShow(Relation relation) {
+        return new TabAndDisplay() {
+            @Override
+            public boolean shouldDisplay() {
+                return OsmStopAreaRelationTags.isStopArea(relation);
+            }
+
+            @Override
+            public JPanel getTabContent() {
+                return new StopVicinityPanel(relation);
+            }
+
+            @Override
+            public String getTitle() {
+                return tr("Vicinity");
+            }
+        };
+    }
+}