150b1ee8ae85719c0ea2054c9f482eacf224c0d1
Michael Zangl <git@michaelzangl.de>
2021-05-23 22:33:17 +0200

    Add initial, empty plugin.

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import java.net.URL
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+plugins {
+  java
+  jacoco
+  id("org.openstreetmap.josm") version "0.7.1"
+}
+
+
+repositories {
+  jcenter()
+}
+dependencies {
+  testImplementation("org.openstreetmap.josm:josm-unittest:SNAPSHOT"){isChanging=true}
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+  // For parsing gtfs files
+  implementation("com.opencsv:opencsv:5.3")
+}
+
+tasks.withType(JavaCompile::class) {
+  options.compilerArgs.addAll(
+    arrayOf("-Xlint:all", "-Xlint:-serial")
+  )
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+  withSourcesJar()
+  withJavadocJar()
+}
+
+josm {
+  manifest {
+  }
+  i18n {
+  }
+}
+
+tasks.jacocoTestReport {
+  reports {
+    xml.isEnabled = true
+    html.isEnabled = true
+  }
+  dependsOn(tasks.test)
+  tasks.check.get().dependsOn(this)
+}
---
+++ src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/VisualizeRoutesPlugin.java
@@ -0,0 +1,16 @@
+package org.openstreetmap.josm.plugins.visualizeroutes;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class VisualizeRoutesPlugin extends Plugin {
+    /**
+     * Creates the plugin
+     *
+     * @param info the plugin information describing the plugin.
+     */
+    public VisualizeRoutesPlugin(PluginInformation info) {
+        super(info);
+    }
+
+}
