JOSM/pt_assistant

Render line relations around a given stop position.

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/LineRelation.java
@@ -1,12 +1,14 @@
 package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
 
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
 import org.openstreetmap.josm.tools.ColorHelper;
 
 import java.awt.Color;
-import java.util.ArrayList;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public class LineRelation {
 
@@ -40,4 +42,54 @@ public class LineRelation {
             return Color.LIGHT_GRAY;
         }
     }
+
+    /**
+     * The stops to display on the panel
+     * @return The stops
+     */
+    public Stream<StopPositionEvent> streamStops() {
+        return streamRawStops()
+            .map(it -> new StopPositionEvent(it, false, false));
+    }
+
+    protected Stream<RelationMember> streamRawStops() {
+        return getRelation().getMembers()
+            .stream()
+            .filter(it -> OSMTags.STOP_ROLES.contains(it.getRole()));
+    }
+
+    public static class StopPositionEvent {
+        private final RelationMember stop;
+        private final boolean skippedBefore;
+        private final boolean skippedAfter;
+
+        public StopPositionEvent(RelationMember stop, boolean skippedBefore, boolean skippedAfter) {
+            this.stop = stop;
+            this.skippedBefore = skippedBefore;
+            this.skippedAfter = skippedAfter;
+        }
+
+        public RelationMember getStop() {
+            return stop;
+        }
+
+        public boolean isSkippedAfter() {
+            return skippedAfter;
+        }
+
+        public boolean isSkippedBefore() {
+            return skippedBefore;
+        }
+
+
+        @Override
+        public String toString() {
+            return "StopPositionEvent{" +
+                "stop=" + stop +
+                ", skippedBefore=" + skippedBefore +
+                ", skippedAfter=" + skippedAfter +
+                '}';
+        }
+    }
+
 }

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/LineRelationAroundStop.java
@@ -1,0 +1,42 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+
+/**
+ * Same as a {@link LineRelation}, but only the part around a given stop position
+ */
+public class LineRelationAroundStop extends LineRelation {
+    private final Predicate<OsmPrimitive> isStop;
+
+    public LineRelationAroundStop(Relation relation, boolean primary, Predicate<OsmPrimitive> isStop) {
+        super(relation, primary);
+        this.isStop = isStop;
+    }
+
+    @Override
+    public Stream<StopPositionEvent> streamStops() {
+        List<RelationMember> stops = streamRawStops().collect(Collectors.toList());
+        List<Integer> indexesAtWhichOurStopIs = IntStream.range(0, stops.size())
+            .filter(i -> isStop.test(stops.get(i).getMember()))
+            .boxed()
+            .collect(Collectors.toList());
+
+        IntPredicate contains = index -> indexesAtWhichOurStopIs.stream().anyMatch(test -> Math.abs(index - test) <= 3);
+
+        return IntStream.range(0, stops.size())
+            .filter(contains)
+            .mapToObj(i -> new StopPositionEvent(stops.get(i),
+                i > 0 && !contains.test(i - 1),
+                i < stops.size() - 1 && !contains.test(i + 1)));
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/StopCollector.java
@@ -1,6 +1,5 @@
 package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
 
-import static org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags.STOP_AREA_TAG_VALUE;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.Color;
@@ -15,8 +14,8 @@ import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
-import org.openstreetmap.josm.plugins.pt_assistant.utils.ColorPalette;
-import org.openstreetmap.josm.tools.ColorHelper;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.LineRelation.StopPositionEvent;
+import org.openstreetmap.josm.plugins.pt_assistant.utils.StopUtils;
 
 /**
  * Collects all stops that there are in several relations
@@ -104,7 +103,16 @@ public class StopCollector {
                 nextCell.addUpwardsStop(col, nextStop.entryExit);
             }
 
-            currentDrawingIndex = nextIndex;
+            if (currentlyDrawingDown ? nextStop.skippedBefore : nextStop.skippedAfter) {
+                forRelation.cells.get(nextIndex - 1).addContinuityUp(col);
+                nextCell.addUpwardsConnection(col);
+            }
+            if (currentlyDrawingDown ? nextStop.skippedAfter : nextStop.skippedBefore) {
+                forRelation.cells.get(nextIndex + 1).addContinuityDown(col);
+                nextCell.addDownwardsConnection(col);
+            }
+
+            currentDrawingIndex = nextStop.skippedAfter ? -1 : nextIndex;
         }
     }
 
@@ -124,8 +132,9 @@ public class StopCollector {
         int nextInsertPosition = 0;
         boolean isCollectingDownward = true;
         int lastIndex = -1;
-        List<FoundStopPosition> stopPositions = collectFor.findStopPositions();
-        for (FoundStopPosition stop : stopPositions) {
+        List<StopPositionEvent> stopEvents = collectFor.findStopPositions();
+        for (StopPositionEvent event : stopEvents) {
+            FoundStopPosition stop = new FoundStopPosition(event.getStop());
             Optional<FoundStop> alreadyFoundStop = findStop(stop);
             FoundStop actualStop;
             if (alreadyFoundStop.isPresent()) {
@@ -145,7 +154,10 @@ public class StopCollector {
                 if (lastIndex == -1) {
                     // This is the first node. Guess the best insert position for now.
                     // For this, we need to skip ahead and search the first stop that matches
-                    List<Integer> nextPositions = stopPositions.stream()
+                    // TODO: This code is not really efficcient
+                    List<Integer> nextPositions = stopEvents.stream()
+                        .map(StopPositionEvent::getStop)
+                        .map(FoundStopPosition::new)
                         .map(this::findStop)
                         .filter(Optional::isPresent)
                         .map(Optional::get)
@@ -167,7 +179,8 @@ public class StopCollector {
                     nextInsertPosition++;
                 }
             }
-            StopWithNotes stopFound = new StopWithNotes(actualStop, stop.entryExit(), stop.member.getMember().get("ref"));
+            StopWithNotes stopFound = new StopWithNotes(actualStop, stop.entryExit(),
+                stop.member.getMember().get("ref"), event.isSkippedBefore(), event.isSkippedAfter());
             collectFor.stops.add(stopFound);
             lastIndex = index(stopFound);
         }
@@ -202,12 +215,8 @@ public class StopCollector {
                 + (line.isPrimary() ? 1_000_000 : 0);
         }
 
-        public List<FoundStopPosition> findStopPositions() {
-            return line.getRelation().getMembers()
-                .stream()
-                .filter(it -> OSMTags.STOP_ROLES.contains(it.getRole()))
-                .map(FoundStopPosition::new)
-                .collect(Collectors.toList());
+        public List<StopPositionEvent> findStopPositions() {
+            return line.streamStops().collect(Collectors.toList());
         }
 
         public Color getColor() {
@@ -225,11 +234,15 @@ public class StopCollector {
         private final FoundStop stop;
         private final EntryExit entryExit;
         private final String notes;
+        private final boolean skippedBefore;
+        private final boolean skippedAfter;
 
-        public StopWithNotes(FoundStop stop, EntryExit entryExit, String notes) {
+        public StopWithNotes(FoundStop stop, EntryExit entryExit, String notes, boolean skippedBefore, boolean skippedAfter) {
             this.stop = stop;
             this.entryExit = entryExit;
             this.notes = notes;
+            this.skippedBefore = skippedBefore;
+            this.skippedAfter = skippedAfter;
         }
     }
 
@@ -256,7 +269,7 @@ public class StopCollector {
                 .stream()
                 .filter(it -> it.getType() == OsmPrimitiveType.RELATION
                     && it.hasTag(OSMTags.KEY_RELATION_TYPE, "public_transport")
-                    && it.hasTag(OSMTags.PUBLIC_TRANSPORT_TAG, STOP_AREA_TAG_VALUE)
+                    && StopUtils.isStopArea((Relation) it)
                 )
                 .map(it -> (Relation) it)
                 .findFirst();

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/LineRelation.java
@@ -1,10 +1,13 @@
 package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
 
 import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
 import org.openstreetmap.josm.tools.ColorHelper;
 
-import java.awt.Color;
+import java.awt.*;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public class LineRelation {
 
@@ -38,4 +41,54 @@ public class LineRelation {
             return Color.LIGHT_GRAY;
         }
     }
+
+    /**
+     * The stops to display on the panel
+     * @return The stops
+     */
+    public Stream<StopPositionEvent> streamStops() {
+        return streamRawStops()
+            .map(it -> new StopPositionEvent(it, false, false));
+    }
+
+    protected Stream<RelationMember> streamRawStops() {
+        return getRelation().getMembers()
+            .stream()
+            .filter(it -> OsmRouteRelationTags.STOP_ROLES.contains(it.getRole()));
+    }
+
+    public static class StopPositionEvent {
+        private final RelationMember stop;
+        private final boolean skippedBefore;
+        private final boolean skippedAfter;
+
+        public StopPositionEvent(RelationMember stop, boolean skippedBefore, boolean skippedAfter) {
+            this.stop = stop;
+            this.skippedBefore = skippedBefore;
+            this.skippedAfter = skippedAfter;
+        }
+
+        public RelationMember getStop() {
+            return stop;
+        }
+
+        public boolean isSkippedAfter() {
+            return skippedAfter;
+        }
+
+        public boolean isSkippedBefore() {
+            return skippedBefore;
+        }
+
+
+        @Override
+        public String toString() {
+            return "StopPositionEvent{" +
+                "stop=" + stop +
+                ", skippedBefore=" + skippedBefore +
+                ", skippedAfter=" + skippedAfter +
+                '}';
+        }
+    }
+
 }

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/LineRelationAroundStop.java
@@ -1,0 +1,42 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+
+/**
+ * Same as a {@link LineRelation}, but only the part around a given stop position
+ */
+public class LineRelationAroundStop extends LineRelation {
+    private final Predicate<OsmPrimitive> isStop;
+
+    public LineRelationAroundStop(Relation relation, boolean primary, Predicate<OsmPrimitive> isStop) {
+        super(relation, primary);
+        this.isStop = isStop;
+    }
+
+    @Override
+    public Stream<StopPositionEvent> streamStops() {
+        List<RelationMember> stops = streamRawStops().collect(Collectors.toList());
+        List<Integer> indexesAtWhichOurStopIs = IntStream.range(0, stops.size())
+            .filter(i -> isStop.test(stops.get(i).getMember()))
+            .boxed()
+            .collect(Collectors.toList());
+
+        IntPredicate contains = index -> indexesAtWhichOurStopIs.stream().anyMatch(test -> Math.abs(index - test) <= 3);
+
+        return IntStream.range(0, stops.size())
+            .filter(contains)
+            .mapToObj(i -> new StopPositionEvent(stops.get(i),
+                i > 0 && !contains.test(i - 1),
+                i < stops.size() - 1 && !contains.test(i + 1)));
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/StopCollector.java
@@ -6,6 +6,7 @@ import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
 import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmStopAreaRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.LineRelation.StopPositionEvent;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -102,7 +103,16 @@ public class StopCollector {
                 nextCell.addUpwardsStop(col, nextStop.entryExit);
             }
 
-            currentDrawingIndex = nextIndex;
+            if (currentlyDrawingDown ? nextStop.skippedBefore : nextStop.skippedAfter) {
+                forRelation.cells.get(nextIndex - 1).addContinuityUp(col);
+                nextCell.addUpwardsConnection(col);
+            }
+            if (currentlyDrawingDown ? nextStop.skippedAfter : nextStop.skippedBefore) {
+                forRelation.cells.get(nextIndex + 1).addContinuityDown(col);
+                nextCell.addDownwardsConnection(col);
+            }
+
+            currentDrawingIndex = nextStop.skippedAfter ? -1 : nextIndex;
         }
     }
 
@@ -122,8 +132,9 @@ public class StopCollector {
         int nextInsertPosition = 0;
         boolean isCollectingDownward = true;
         int lastIndex = -1;
-        List<FoundStopPosition> stopPositions = collectFor.findStopPositions();
-        for (FoundStopPosition stop : stopPositions) {
+        List<StopPositionEvent> stopEvents = collectFor.findStopPositions();
+        for (StopPositionEvent event : stopEvents) {
+            FoundStopPosition stop = new FoundStopPosition(event.getStop());
             Optional<FoundStop> alreadyFoundStop = findStop(stop);
             FoundStop actualStop;
             if (alreadyFoundStop.isPresent()) {
@@ -143,7 +154,10 @@ public class StopCollector {
                 if (lastIndex == -1) {
                     // This is the first node. Guess the best insert position for now.
                     // For this, we need to skip ahead and search the first stop that matches
-                    List<Integer> nextPositions = stopPositions.stream()
+                    // TODO: This code is not really efficcient
+                    List<Integer> nextPositions = stopEvents.stream()
+                        .map(StopPositionEvent::getStop)
+                        .map(FoundStopPosition::new)
                         .map(this::findStop)
                         .filter(Optional::isPresent)
                         .map(Optional::get)
@@ -165,7 +179,8 @@ public class StopCollector {
                     nextInsertPosition++;
                 }
             }
-            StopWithNotes stopFound = new StopWithNotes(actualStop, stop.entryExit(), stop.member.getMember().get("ref"));
+            StopWithNotes stopFound = new StopWithNotes(actualStop, stop.entryExit(),
+                stop.member.getMember().get("ref"), event.isSkippedBefore(), event.isSkippedAfter());
             collectFor.stops.add(stopFound);
             lastIndex = index(stopFound);
         }
@@ -200,12 +215,8 @@ public class StopCollector {
                 + (line.isPrimary() ? 1_000_000 : 0);
         }
 
-        public List<FoundStopPosition> findStopPositions() {
-            return line.getRelation().getMembers()
-                .stream()
-                .filter(it -> OsmRouteRelationTags.STOP_ROLES.contains(it.getRole()))
-                .map(FoundStopPosition::new)
-                .collect(Collectors.toList());
+        public List<StopPositionEvent> findStopPositions() {
+            return line.streamStops().collect(Collectors.toList());
         }
 
         public Color getColor() {
@@ -223,11 +234,15 @@ public class StopCollector {
         private final FoundStop stop;
         private final EntryExit entryExit;
         private final String notes;
+        private final boolean skippedBefore;
+        private final boolean skippedAfter;
 
-        public StopWithNotes(FoundStop stop, EntryExit entryExit, String notes) {
+        public StopWithNotes(FoundStop stop, EntryExit entryExit, String notes, boolean skippedBefore, boolean skippedAfter) {
             this.stop = stop;
             this.entryExit = entryExit;
             this.notes = notes;
+            this.skippedBefore = skippedBefore;
+            this.skippedAfter = skippedAfter;
         }
     }
 
@@ -253,7 +268,7 @@ public class StopCollector {
             return member.getMember().getReferrers()
                 .stream()
                 .filter(it -> it.getType() == OsmPrimitiveType.RELATION
-                    && OsmStopAreaRelationTags.isStopArea(it)
+                    && OsmStopAreaRelationTags.isStopArea((Relation) it)
                 )
                 .map(it -> (Relation) it)
                 .findFirst();