JOSM/pt_assistant

Add a "Create stop area" action

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/stoparea/StopVicinityPanel.java
@@ -1,11 +1,10 @@
-package org.openstreetmap.josm.plugins.pt_assistant.gui.stopvicinity;
+package org.openstreetmap.josm.plugins.pt_assistant.gui.stoparea;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.geom.Point2D;
@@ -189,7 +188,7 @@ public class StopVicinityPanel extends AbstractVicinityPanel {
 
     @Override
     protected String getStylePath() {
-        return "org/openstreetmap/josm/plugins/pt_assistant/gui/stopvicinity/vicinitystyle.mapcss";
+        return "org/openstreetmap/josm/plugins/pt_assistant/gui/stoparea/stopareavicinity.mapcss";
     }
 
     @Override

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/stoparea/StopVicinityTabManager.java
@@ -1,4 +1,4 @@
-package org.openstreetmap.josm.plugins.pt_assistant.gui.stopvicinity;
+package org.openstreetmap.josm.plugins.pt_assistant.gui.stoparea;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/stoparea/StopVicinityPanel.java
@@ -1,4 +1,4 @@
-package org.openstreetmap.josm.plugins.visualizeroutes.gui.stopvicinity;
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.stoparea;
 
 import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.data.osm.*;
@@ -170,7 +170,7 @@ public class StopVicinityPanel extends AbstractVicinityPanel {
 
     @Override
     protected String getStylePath() {
-        return "org/openstreetmap/josm/plugins/visualizeroutes/gui/stopvicinity/vicinitystyle.mapcss";
+        return "org/openstreetmap/josm/plugins/visualizeroutes/gui/stoparea/stopareavicinity.mapcss";
     }
 
     @Override

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/stoparea/StopVicinityTabManager.java
@@ -1,4 +1,6 @@
-package org.openstreetmap.josm.plugins.visualizeroutes.gui.stopvicinity;
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.stoparea;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
 
 import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
 import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmStopAreaRelationTags;