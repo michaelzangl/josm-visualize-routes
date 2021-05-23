JOSM/pt_assistant

Added download panel to the vicinity panel.

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/NoRouteMasterHeadline.java
@@ -1,23 +1,18 @@
 package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
 
-import static org.openstreetmap.josm.plugins.pt_assistant.gui.linear.UnBoldLabel.safeHtml;
+import static org.openstreetmap.josm.plugins.pt_assistant.gui.utils.UnBoldLabel.safeHtml;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -30,6 +25,7 @@ import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.gui.ExtendedDialog;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.utils.UnBoldLabel;
 import org.openstreetmap.josm.plugins.pt_assistant.utils.DownloadUtils;
 import org.openstreetmap.josm.tools.GBC;
 

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/PublicTransportLinePanel.java
@@ -1,6 +1,6 @@
 package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
 
-import static org.openstreetmap.josm.plugins.pt_assistant.gui.linear.UnBoldLabel.safeHtml;
+import static org.openstreetmap.josm.plugins.pt_assistant.gui.utils.UnBoldLabel.safeHtml;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.Component;
@@ -12,7 +12,6 @@ import java.awt.event.ActionEvent;
 import java.text.MessageFormat;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,8 +25,8 @@ import javax.swing.border.EmptyBorder;
 import org.openstreetmap.josm.actions.relation.EditRelationAction;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
-import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.utils.UnBoldLabel;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.ImageProvider;
 

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/RouteMasterHeadlinePanel.java
@@ -1,30 +1,19 @@
 package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
 
-import static org.openstreetmap.josm.plugins.pt_assistant.gui.linear.UnBoldLabel.safeHtml;
+import static org.openstreetmap.josm.plugins.pt_assistant.gui.utils.UnBoldLabel.safeHtml;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import org.openstreetmap.josm.actions.JosmAction;
-import org.openstreetmap.josm.actions.downloadtasks.DownloadOsmTask;
-import org.openstreetmap.josm.actions.downloadtasks.DownloadParams;
-import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
-import org.openstreetmap.josm.data.osm.RelationMember;
-import org.openstreetmap.josm.gui.download.OverpassDownloadSource;
-import org.openstreetmap.josm.io.OverpassDownloadReader;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.utils.UnBoldLabel;
 import org.openstreetmap.josm.plugins.pt_assistant.utils.DownloadUtils;
-import org.openstreetmap.josm.tools.Utils;
 
 public class RouteMasterHeadlinePanel extends JPanel {
     public RouteMasterHeadlinePanel(RelationAccess masterRelation) {

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/utils/UnBoldLabel.java
@@ -1,10 +1,10 @@
-package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+package org.openstreetmap.josm.plugins.pt_assistant.gui.utils;
 
 import java.awt.Dimension;
 
 import javax.swing.JLabel;
 
-class UnBoldLabel extends JLabel {
+public class UnBoldLabel extends JLabel {
     public UnBoldLabel(String text) {
         super(text);
         setHorizontalAlignment(LEFT);

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/NoRouteMasterHeadline.java
@@ -6,6 +6,7 @@ import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.gui.ExtendedDialog;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.utils.UnBoldLabel;
 import org.openstreetmap.josm.plugins.visualizeroutes.utils.DownloadUtils;
 import org.openstreetmap.josm.tools.GBC;
 
@@ -15,7 +16,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.Comparator;
 
-import static org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.UnBoldLabel.safeHtml;
+import static org.openstreetmap.josm.plugins.visualizeroutes.gui.utils.UnBoldLabel.safeHtml;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 public class NoRouteMasterHeadline extends JPanel {

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/PublicTransportLinePanel.java
@@ -4,6 +4,7 @@ import org.openstreetmap.josm.actions.relation.EditRelationAction;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteMasterRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.utils.UnBoldLabel;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.ImageProvider;
 
@@ -17,7 +18,7 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.UnBoldLabel.safeHtml;
+import static org.openstreetmap.josm.plugins.visualizeroutes.gui.utils.UnBoldLabel.safeHtml;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 /**

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/RouteMasterHeadlinePanel.java
@@ -1,8 +1,8 @@
 package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
 
-
 import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.utils.UnBoldLabel;
 import org.openstreetmap.josm.plugins.visualizeroutes.utils.DownloadUtils;
 
 import javax.swing.*;
@@ -10,7 +10,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.text.MessageFormat;
 
-import static org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.UnBoldLabel.safeHtml;
+import static org.openstreetmap.josm.plugins.visualizeroutes.gui.utils.UnBoldLabel.safeHtml;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 public class RouteMasterHeadlinePanel extends JPanel {

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/utils/UnBoldLabel.java
@@ -1,10 +1,10 @@
-package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.utils;
 
 import java.awt.Dimension;
 
 import javax.swing.JLabel;
 
-class UnBoldLabel extends JLabel {
+public class UnBoldLabel extends JLabel {
     public UnBoldLabel(String text) {
         super(text);
         setHorizontalAlignment(LEFT);