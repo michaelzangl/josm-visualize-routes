JOSM/pt_assistant

Add a tab to the relation dialog that allows comparing all routes in the route master relation.

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/LineGridCell.java
@@ -1,0 +1,125 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import javax.swing.JPanel;
+
+/**
+ * A cell in the line grid.
+ * Displays a stop, one or multiple lines and other symbols
+ */
+public class LineGridCell extends JPanel {
+    private static final int COL_WIDTH = 20;
+    private List<Consumer<Graphics2D>> toPaint = new ArrayList<>();
+    private int colCount = 0;
+    private Color color;
+
+    public LineGridCell(Color color) {
+        this.color = color;
+    }
+
+    public void addDownwardsStop(int colIndex, EntryExit entryExit) {
+        addStop(colIndex, entryExit, 1);
+    }
+    public void addUpwardsStop(int colIndex, EntryExit entryExit) {
+        addStop(colIndex, entryExit, -1);
+    }
+    private void addStop(int colIndex, EntryExit entryExit, int dir) {
+        addToPaint(colIndex, true, (graphics2D, xMin, xMax, yMin, yMax) -> {
+            double cx = (xMax + xMin) / 2.0;
+            double cy = (yMax + yMin) / 2.0;
+            Path2D tri = new Path2D.Double();
+            tri.moveTo(cx - 8 * dir, cy - 3 * dir);
+            tri.lineTo(cx + 8 * dir, cy - 3 * dir);
+            tri.lineTo(cx, cy + 5 * dir);
+            tri.closePath();
+            graphics2D.setColor(Color.WHITE);
+            graphics2D.fill(tri);
+            graphics2D.setStroke(new BasicStroke(2));
+            graphics2D.setColor(entryExit == EntryExit.ENTRY ? Color.GREEN
+                : entryExit == EntryExit.EXIT ? Color.RED : Color.BLACK);
+            graphics2D.draw(tri);
+        });
+    }
+
+    public void addToPaint(int colIndex, boolean fg, SymbolToPaint symbol) {
+        toPaint.add(fg ? toPaint.size() : 0, graphics2D -> symbol.paintAt(graphics2D, COL_WIDTH * colIndex, COL_WIDTH * (colIndex + 1),
+            0, getHeight()));
+        colCount = Math.max(colIndex + 1, colCount);
+    }
+
+    public void addDownwardsU(int col) {
+        addU(col, true);
+    }
+    public void addUpwardsU(int col) {
+        addU(col, false);
+    }
+
+    private void addU(int col, boolean downwards) {
+        addToPaint(col, false, (graphics2D, xMin, xMax, yMin, yMax) -> {
+            int x1 = (xMin + xMax) / 2;
+            int x2 = (3 * xMax - xMin) / 2;
+            int cy = (yMax + yMin) / 2;
+            int y = downwards ? yMax : yMin;
+            Path2D.Double path = new Path2D.Double();
+            path.moveTo(x1, y);
+            path.quadTo(x1, cy, xMax, cy);
+            path.quadTo(x2, cy, x2, y);
+            setLinePaint(graphics2D);
+            graphics2D.draw(path);
+        });
+    }
+
+    public void addDownwardsConnection(int col) {
+        addConnection(col, 0.5, 1);
+    }
+
+    public void addUpwardsConnection(int col) {
+        addConnection(col, 0, 0.5);
+    }
+
+    public void addThroughConnection(int col) {
+        addConnection(col, 0, 1);
+    }
+
+    private void addConnection(int col, double start, double end) {
+        addToPaint(col, false, (graphics2D, xMin, xMax, yMin, yMax) -> {
+            setLinePaint(graphics2D);
+            int cx = (xMax + xMin) / 2;
+            graphics2D.drawLine(cx, (int) (yMin * (1 - start) + yMax * start),
+                cx, (int) (yMin * (1 - end) + yMax * end));
+        });
+    }
+
+    private void setLinePaint(Graphics2D graphics2D) {
+        graphics2D.setStroke(new BasicStroke(4));
+        graphics2D.setColor(color);
+    }
+
+    @Override
+    public Dimension getMinimumSize() {
+        return new Dimension(COL_WIDTH * colCount, toPaint.isEmpty() ? 0 : 20);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return getMinimumSize();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        toPaint.forEach(it -> it.accept((Graphics2D) g));
+    }
+
+    private interface SymbolToPaint {
+        void paintAt(Graphics2D graphics2D, int xMin, int xMax, int yMin, int yMax);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/LineGridHorizontalColumnArrow.java
@@ -1,0 +1,37 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.geom.Path2D;
+
+/**
+ * Used on the top of the line to display an arrow that points to a column
+ */
+public class LineGridHorizontalColumnArrow extends JPanel  {
+    public LineGridHorizontalColumnArrow() {
+        setMinimumSize(new Dimension(20, 20));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(2));
+        g2d.setColor(Color.GRAY);
+
+        double height = getHeight();
+        double cy = height / 2.0;
+        double offsetX = 15; // How much the arrow is offsetted from the left side
+        double roundness = 4.0;
+
+        Path2D.Double path = new Path2D.Double();
+        path.moveTo(offsetX - 4, height - 4);
+        path.lineTo(offsetX, height + 1);
+        path.lineTo(offsetX + 4, height - 4);
+        path.moveTo(offsetX, height - 2);
+        path.lineTo(offsetX, cy + roundness);
+        path.quadTo(offsetX, cy, offsetX + roundness, cy);
+        path.lineTo(getWidth(), cy);
+        g2d.draw(path);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/LineRelation.java
@@ -1,0 +1,43 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
+import org.openstreetmap.josm.tools.ColorHelper;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class LineRelation {
+
+    private final Relation relation;
+    private final boolean primary;
+
+    public LineRelation(Relation relation, boolean primary) {
+        this.relation = Objects.requireNonNull(relation, "relation");
+        this.primary = primary;
+    }
+
+    public Relation getRelation() {
+        return relation;
+    }
+
+    public boolean isPrimary() {
+        return primary;
+    }
+
+    @Override
+    public String toString() {
+        return "LinearRelation{" + "relation=" + relation + ", primary=" + primary + '}';
+    }
+
+    public Color getColor() {
+        if (isPrimary()) {
+            String colour = relation.get("colour");
+            Color fromOsm = colour != null ? ColorHelper.html2color(colour) : null;
+            return fromOsm != null ? fromOsm : Color.BLACK;
+        } else {
+            return Color.LIGHT_GRAY;
+        }
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/LineRelationTabManager.java
@@ -1,0 +1,63 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.Container;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.gui.dialogs.relation.RelationEditor;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+
+/**
+ * Adds or removes the tab for the linear relation
+ */
+public class LineRelationTabManager {
+    private final JTabbedPane tabPanel;
+    private final Supplier<Relation> relationGetter;
+    private JScrollPane tabContent = null;
+
+
+
+    public LineRelationTabManager(IRelationEditorActionAccess editorAccess) {
+        Container editorComponent = ((JDialog) editorAccess.getEditor()).getContentPane();
+
+        this.tabPanel = Stream.of(editorComponent.getComponents())
+            .filter(it -> it instanceof JTabbedPane)
+            .map(it -> (JTabbedPane) it)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Relation editor did not have a tab panel"));
+
+        this.relationGetter = editorAccess.getEditor()::getRelation;
+
+        updateTab();
+        editorComponent.addPropertyChangeListener(RelationEditor.RELATION_PROP, __ -> updateTab());
+        editorAccess.getMemberTableModel().addTableModelListener(__ -> updateTab());
+        editorAccess.getTagModel().addPropertyChangeListener(__ -> updateTab());
+    }
+
+    private void updateTab() {
+        Relation relation = relationGetter.get();
+        PublicTransportLinePanel linePanel = PublicTransportLinePanel.forRelation(relation);
+        boolean shouldDisplayTab = linePanel != null;
+
+        if (shouldDisplayTab) {
+            if (tabContent == null) {
+                tabContent = new JScrollPane();
+                tabPanel.add(tr("Transport line"), tabContent);
+            }
+
+            tabContent.getViewport().setView(linePanel);
+        } else {
+            if (tabContent != null) {
+                tabPanel.remove(tabContent);
+                tabContent = null;
+            }
+        }
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/LineRelationsProvider.java
@@ -1,0 +1,16 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+
+
+import org.openstreetmap.josm.data.osm.Relation;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Provides the relations for createing a {@link PublicTransportLinePanel}
+ */
+public interface LineRelationsProvider {
+    Optional<Relation> getMasterRelation();
+
+    List<LineRelation> getRelations();
+}

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/LineGridCell.java
@@ -1,0 +1,125 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import javax.swing.JPanel;
+
+/**
+ * A cell in the line grid.
+ * Displays a stop, one or multiple lines and other symbols
+ */
+public class LineGridCell extends JPanel {
+    private static final int COL_WIDTH = 20;
+    private List<Consumer<Graphics2D>> toPaint = new ArrayList<>();
+    private int colCount = 0;
+    private Color color;
+
+    public LineGridCell(Color color) {
+        this.color = color;
+    }
+
+    public void addDownwardsStop(int colIndex, EntryExit entryExit) {
+        addStop(colIndex, entryExit, 1);
+    }
+    public void addUpwardsStop(int colIndex, EntryExit entryExit) {
+        addStop(colIndex, entryExit, -1);
+    }
+    private void addStop(int colIndex, EntryExit entryExit, int dir) {
+        addToPaint(colIndex, true, (graphics2D, xMin, xMax, yMin, yMax) -> {
+            double cx = (xMax + xMin) / 2.0;
+            double cy = (yMax + yMin) / 2.0;
+            Path2D tri = new Path2D.Double();
+            tri.moveTo(cx - 8 * dir, cy - 3 * dir);
+            tri.lineTo(cx + 8 * dir, cy - 3 * dir);
+            tri.lineTo(cx, cy + 5 * dir);
+            tri.closePath();
+            graphics2D.setColor(Color.WHITE);
+            graphics2D.fill(tri);
+            graphics2D.setStroke(new BasicStroke(2));
+            graphics2D.setColor(entryExit == EntryExit.ENTRY ? Color.GREEN
+                : entryExit == EntryExit.EXIT ? Color.RED : Color.BLACK);
+            graphics2D.draw(tri);
+        });
+    }
+
+    public void addToPaint(int colIndex, boolean fg, SymbolToPaint symbol) {
+        toPaint.add(fg ? toPaint.size() : 0, graphics2D -> symbol.paintAt(graphics2D, COL_WIDTH * colIndex, COL_WIDTH * (colIndex + 1),
+            0, getHeight()));
+        colCount = Math.max(colIndex + 1, colCount);
+    }
+
+    public void addDownwardsU(int col) {
+        addU(col, true);
+    }
+    public void addUpwardsU(int col) {
+        addU(col, false);
+    }
+
+    private void addU(int col, boolean downwards) {
+        addToPaint(col, false, (graphics2D, xMin, xMax, yMin, yMax) -> {
+            int x1 = (xMin + xMax) / 2;
+            int x2 = (3 * xMax - xMin) / 2;
+            int cy = (yMax + yMin) / 2;
+            int y = downwards ? yMax : yMin;
+            Path2D.Double path = new Path2D.Double();
+            path.moveTo(x1, y);
+            path.quadTo(x1, cy, xMax, cy);
+            path.quadTo(x2, cy, x2, y);
+            setLinePaint(graphics2D);
+            graphics2D.draw(path);
+        });
+    }
+
+    public void addDownwardsConnection(int col) {
+        addConnection(col, 0.5, 1);
+    }
+
+    public void addUpwardsConnection(int col) {
+        addConnection(col, 0, 0.5);
+    }
+
+    public void addThroughConnection(int col) {
+        addConnection(col, 0, 1);
+    }
+
+    private void addConnection(int col, double start, double end) {
+        addToPaint(col, false, (graphics2D, xMin, xMax, yMin, yMax) -> {
+            setLinePaint(graphics2D);
+            int cx = (xMax + xMin) / 2;
+            graphics2D.drawLine(cx, (int) (yMin * (1 - start) + yMax * start),
+                cx, (int) (yMin * (1 - end) + yMax * end));
+        });
+    }
+
+    private void setLinePaint(Graphics2D graphics2D) {
+        graphics2D.setStroke(new BasicStroke(4));
+        graphics2D.setColor(color);
+    }
+
+    @Override
+    public Dimension getMinimumSize() {
+        return new Dimension(COL_WIDTH * colCount, toPaint.isEmpty() ? 0 : 20);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return getMinimumSize();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        toPaint.forEach(it -> it.accept((Graphics2D) g));
+    }
+
+    private interface SymbolToPaint {
+        void paintAt(Graphics2D graphics2D, int xMin, int xMax, int yMin, int yMax);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/LineGridHorizontalColumnArrow.java
@@ -1,0 +1,36 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Path2D;
+
+/**
+ * Used on the top of the line to display an arrow that points to a column
+ */
+public class LineGridHorizontalColumnArrow extends JPanel  {
+    public LineGridHorizontalColumnArrow() {
+        setMinimumSize(new Dimension(20, 20));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(2));
+        g2d.setColor(Color.GRAY);
+
+        double height = getHeight();
+        double cy = height / 2.0;
+        double offsetX = 15; // How much the arrow is offsetted from the left side
+        double roundness = 4.0;
+
+        Path2D.Double path = new Path2D.Double();
+        path.moveTo(offsetX - 4, height - 4);
+        path.lineTo(offsetX, height + 1);
+        path.lineTo(offsetX + 4, height - 4);
+        path.moveTo(offsetX, height - 2);
+        path.lineTo(offsetX, cy + roundness);
+        path.quadTo(offsetX, cy, offsetX + roundness, cy);
+        path.lineTo(getWidth(), cy);
+        g2d.draw(path);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/LineRelation.java
@@ -1,0 +1,41 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.tools.ColorHelper;
+
+import java.awt.Color;
+import java.util.Objects;
+
+public class LineRelation {
+
+    private final Relation relation;
+    private final boolean primary;
+
+    public LineRelation(Relation relation, boolean primary) {
+        this.relation = Objects.requireNonNull(relation, "relation");
+        this.primary = primary;
+    }
+
+    public Relation getRelation() {
+        return relation;
+    }
+
+    public boolean isPrimary() {
+        return primary;
+    }
+
+    @Override
+    public String toString() {
+        return "LinearRelation{" + "relation=" + relation + ", primary=" + primary + '}';
+    }
+
+    public Color getColor() {
+        if (isPrimary()) {
+            String colour = relation.get("colour");
+            Color fromOsm = colour != null ? ColorHelper.html2color(colour) : null;
+            return fromOsm != null ? fromOsm : Color.BLACK;
+        } else {
+            return Color.LIGHT_GRAY;
+        }
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/LineRelationTabManager.java
@@ -1,0 +1,63 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.Container;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.gui.dialogs.relation.RelationEditor;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+
+/**
+ * Adds or removes the tab for the linear relation
+ */
+public class LineRelationTabManager {
+    private final JTabbedPane tabPanel;
+    private final Supplier<Relation> relationGetter;
+    private JScrollPane tabContent = null;
+
+
+
+    public LineRelationTabManager(IRelationEditorActionAccess editorAccess) {
+        Container editorComponent = ((JDialog) editorAccess.getEditor()).getContentPane();
+
+        this.tabPanel = Stream.of(editorComponent.getComponents())
+            .filter(it -> it instanceof JTabbedPane)
+            .map(it -> (JTabbedPane) it)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Relation editor did not have a tab panel"));
+
+        this.relationGetter = editorAccess.getEditor()::getRelation;
+
+        updateTab();
+        editorComponent.addPropertyChangeListener(RelationEditor.RELATION_PROP, __ -> updateTab());
+        editorAccess.getMemberTableModel().addTableModelListener(__ -> updateTab());
+        editorAccess.getTagModel().addPropertyChangeListener(__ -> updateTab());
+    }
+
+    private void updateTab() {
+        Relation relation = relationGetter.get();
+        PublicTransportLinePanel linePanel = PublicTransportLinePanel.forRelation(relation);
+        boolean shouldDisplayTab = linePanel != null;
+
+        if (shouldDisplayTab) {
+            if (tabContent == null) {
+                tabContent = new JScrollPane();
+                tabPanel.add(tr("Transport line"), tabContent);
+            }
+
+            tabContent.getViewport().setView(linePanel);
+        } else {
+            if (tabContent != null) {
+                tabPanel.remove(tabContent);
+                tabContent = null;
+            }
+        }
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/LineRelationsProvider.java
@@ -1,0 +1,16 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+
+
+import org.openstreetmap.josm.data.osm.Relation;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Provides the relations for createing a {@link PublicTransportLinePanel}
+ */
+public interface LineRelationsProvider {
+    Optional<Relation> getMasterRelation();
+
+    List<LineRelation> getRelations();
+}