JOSM/pt_assistant

Add ability to download relevant route data.

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/RouteMasterHeadlinePanel.java
@@ -1,17 +1,33 @@
 package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
 
 import static org.openstreetmap.josm.plugins.pt_assistant.gui.linear.UnBoldLabel.safeHtml;
+import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
-import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.actions.JosmAction;
+import org.openstreetmap.josm.actions.downloadtasks.DownloadOsmTask;
+import org.openstreetmap.josm.actions.downloadtasks.DownloadParams;
+import org.openstreetmap.josm.data.Bounds;
+import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.gui.download.OverpassDownloadSource;
+import org.openstreetmap.josm.io.OverpassDownloadReader;
+import org.openstreetmap.josm.plugins.pt_assistant.utils.DownloadUtils;
+import org.openstreetmap.josm.tools.Utils;
 
 public class RouteMasterHeadlinePanel extends JPanel {
-    public RouteMasterHeadlinePanel(Relation masterRelation) {
+    public RouteMasterHeadlinePanel(RelationAccess masterRelation) {
         setLayout(new BorderLayout());
         String color = masterRelation.get("colour");
             if (color == null) {
@@ -22,5 +38,17 @@ public class RouteMasterHeadlinePanel extends JPanel {
         String infos = safeHtml(masterRelation.get("operator")) + " " + safeHtml(masterRelation.get("network"));
         String routeMasterText = MessageFormat.format("<html><h2>{0}</h2><div>{1}</div></html>", headline, infos);
         add(new UnBoldLabel(routeMasterText));
+
+        add(new JButton(new JosmAction(tr("Download routes"), "download", null, null, false) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DownloadUtils.downloadUsingOverpass(
+                    "org/openstreetmap/josm/plugins/pt_assistant/gui/linear/downloadRouteMaster.query.txt",
+                    line -> line
+                        .replace("##ROUTEIDS##", DownloadUtils.collectMemberIds(masterRelation, OsmPrimitiveType.RELATION))
+                        .replace("##MASTERID##", masterRelation.getRelation() != null ? masterRelation.getRelation().getId() + "" : Long.MAX_VALUE + ""));
+            }
+        }), BorderLayout.EAST);
     }
+
 }

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/StopCollector.java
@@ -3,12 +3,19 @@ package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.swing.JButton;
+
+import org.openstreetmap.josm.actions.AutoScaleAction;
+import org.openstreetmap.josm.actions.JosmAction;
+import org.openstreetmap.josm.actions.relation.EditRelationAction;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 import org.openstreetmap.josm.data.osm.Relation;
@@ -16,6 +23,7 @@ import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
 import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.LineRelation.StopPositionEvent;
 import org.openstreetmap.josm.plugins.pt_assistant.utils.StopUtils;
+import org.openstreetmap.josm.tools.ImageProvider;
 
 /**
  * Collects all stops that there are in several relations
@@ -252,6 +260,10 @@ public class StopCollector {
         boolean matches(OsmPrimitive p);
 
         String getNameAndInfos();
+
+        default Component createActionButtons() {
+            return null;
+        }
     }
 
     private static class FoundStopPosition implements FoundStop {
@@ -294,6 +306,20 @@ public class StopCollector {
             String name = member.getMember().get("name");
             return (name == null ? tr("- Stop without name -") : name);
         }
+
+        @Override
+        public Component createActionButtons() {
+            return PublicTransportLinePanel.createActions(
+                PublicTransportLinePanel.createAction(
+                    tr("Zoom to stop position"),
+                    new ImageProvider("mapmode", "zoom"),
+                    () -> {
+                        member.getMember().getDataSet().setSelected(member.getMember());
+                        AutoScaleAction.autoScale(AutoScaleAction.AutoScaleMode.SELECTION);
+                    }
+                )
+            );
+        }
     }
 
     private static class FoundStopArea implements FoundStop {
@@ -303,7 +329,7 @@ public class StopCollector {
         FoundStopArea(Relation relation) {
             stops = relation.getMembers()
                 .stream()
-                .filter(m -> "stop".equals(m.getRole()))
+                .filter(m -> OSMTags.STOP_ROLE.equals(m.getRole()))
                 .collect(Collectors.toList());
             this.relation = relation;
         }
@@ -315,14 +341,31 @@ public class StopCollector {
 
         @Override
         public boolean matches(OsmPrimitive p) {
-            return this.stops.stream().anyMatch(s -> s.getMember().equals(p));
+            // Many are not marked as stop => we still want to get them.
+            return this.relation
+                .getMembers()
+                .stream().anyMatch(s -> s.getMember().equals(p));
         }
 
         @Override
         public String getNameAndInfos() {
             String name = relation.get("name");
-            return (name == null ? tr("- No name -") : name) + " (" + tr("area relation " + relation.getId()) + ")";
+            return (name == null ? tr("- No name -") : name) + " (" + tr("area relation {0}", relation.getId()) + ")";
         }
+
+        @Override
+        public Component createActionButtons() {
+            return PublicTransportLinePanel.createActions(
+                PublicTransportLinePanel.createAction(tr("Zoom to stop area relation"), new ImageProvider("mapmode", "zoom").setMaxSize(12),
+                    () -> {
+                        relation.getDataSet().setSelected(relation);
+                        AutoScaleAction.autoScale(AutoScaleAction.AutoScaleMode.SELECTION);
+                    }),
+                PublicTransportLinePanel.createAction(tr("Edit stop area"), new ImageProvider("dialogs", "edit"),
+                    () -> EditRelationAction.launchEditor(relation))
+            );
+        }
+
     }
 
     private static class NopStop implements FoundStop {
@@ -340,5 +383,14 @@ public class StopCollector {
         public String getNameAndInfos() {
             return "";
         }
+    }
+
+    private static JButton createActionButton(final String iconName, final String tool, final Runnable action) {
+        return new JButton(new JosmAction("", iconName, tool, null, false) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                action.run();
+            }
+        });
     }
 }

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/stopvicinity/StopVicinityTabManager.java
@@ -2,34 +2,32 @@ package org.openstreetmap.josm.plugins.pt_assistant.gui.stopvicinity;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
-import javax.swing.JPanel;
-
-import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
 import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.AbstractTabManager;
-import org.openstreetmap.josm.plugins.pt_assistant.utils.StopUtils;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.RelationAccess;
 
-public class StopVicinityTabManager extends AbstractTabManager {
-    private final IRelationEditorActionAccess editorAccess;
+public class StopVicinityTabManager extends AbstractTabManager<StopVicinityPanel> {
     // Used so that zoom gets not lost when re-creating the view due to changes in the relation.
     private final ZoomSaver zoomSaver = new ZoomSaver();
 
     public StopVicinityTabManager(IRelationEditorActionAccess editorAccess) {
         super(editorAccess);
-        this.editorAccess = editorAccess;
     }
 
     @Override
-    protected TabAndDisplay getTabToShow(Relation relation) {
-        return new TabAndDisplay() {
+    protected TabAndDisplay<StopVicinityPanel> getTabToShow(IRelationEditorActionAccess editorAccess) {
+        return new TabAndDisplay<StopVicinityPanel>() {
             @Override
             public boolean shouldDisplay() {
-                return StopUtils.isStopArea(relation);
+                RelationAccess tagged = RelationAccess.of(editorAccess);
+                return OSMTags.PUBLIC_TRANSPORT_TAG.equals(tagged.get(OSMTags.KEY_RELATION_TYPE))
+                    && OSMTags.STOP_AREA_TAG_VALUE.equals(tagged.get(OSMTags.PUBLIC_TRANSPORT_TAG));
             }
 
             @Override
-            public JPanel getTabContent() {
-                return new StopVicinityPanel(relation, editorAccess, zoomSaver);
+            public StopVicinityPanel getTabContent() {
+                return new StopVicinityPanel(editorAccess, zoomSaver);
             }
 
             @Override
@@ -37,5 +35,10 @@ public class StopVicinityTabManager extends AbstractTabManager {
                 return tr("Vicinity");
             }
         };
+    }
+
+    @Override
+    protected void dispose(StopVicinityPanel view) {
+        view.dispose();
     }
 }

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/utils/DownloadUtils.java
@@ -1,0 +1,65 @@
+package org.openstreetmap.josm.plugins.pt_assistant.utils;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.actions.downloadtasks.DownloadOsmTask;
+import org.openstreetmap.josm.actions.downloadtasks.DownloadParams;
+import org.openstreetmap.josm.data.Bounds;
+import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.gui.progress.swing.PleaseWaitProgressMonitor;
+import org.openstreetmap.josm.io.OverpassDownloadReader;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.RelationAccess;
+import org.openstreetmap.josm.tools.Utils;
+
+public class DownloadUtils {
+    private DownloadUtils(){
+    }
+
+    public static void downloadUsingOverpass(String resource, Function<String, String> replacements) {
+        downloadUsingOverpass(resource, replacements, () -> {});
+    }
+    public static void downloadUsingOverpass(String resource, Function<String, String> replacements, Runnable onFinish) {
+        String query = new BufferedReader(new InputStreamReader(
+            Utils.getResourceAsStream(DownloadUtils.class, resource), StandardCharsets.UTF_8))
+            .lines()
+            .map(replacements)
+            .collect(Collectors.joining("\n"));
+
+
+        Bounds area = new Bounds(0, 0, 0, 0);
+        DownloadOsmTask task = new DownloadOsmTask();
+        task.setZoomAfterDownload(false);
+        task.download(
+            new OverpassDownloadReader(area, OverpassDownloadReader.OVERPASS_SERVER.get(), query),
+            new DownloadParams().withNewLayer(false), area, new PleaseWaitProgressMonitor(tr("Downloading data from overpass.")) {
+                @Override
+                public void doFinishTask() {
+                    onFinish.run();
+                }
+            });
+    }
+
+    public static String collectMemberIds(RelationAccess relation, OsmPrimitiveType type) {
+        // No need to filter => We can filter on overpass side.
+        String ids = relation.getMembers()
+            .stream()
+            .map(RelationMember::getMember)
+            .filter(it -> it.getType() == type)
+            .filter(it -> !it.isNew())
+            .map(it -> "" + it.getId())
+            .collect(Collectors.joining(","));
+        return ids.isEmpty() ? Long.MAX_VALUE + "": ids; // < a fake id, since Overpass requires at least one item
+    }
+
+    public static CharSequence escape(String s) {
+        // TODO
+        return s.replace("\"", "");
+    }
+}

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/RouteMasterHeadlinePanel.java
@@ -1,16 +1,20 @@
 package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
 
 
-import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.actions.JosmAction;
+import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.plugins.visualizeroutes.utils.DownloadUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.text.MessageFormat;
 
 import static org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.UnBoldLabel.safeHtml;
+import static org.openstreetmap.josm.tools.I18n.tr;
 
 public class RouteMasterHeadlinePanel extends JPanel {
-    public RouteMasterHeadlinePanel(Relation masterRelation) {
+    public RouteMasterHeadlinePanel(RelationAccess masterRelation) {
         setLayout(new BorderLayout());
         String color = masterRelation.get("colour");
             if (color == null) {
@@ -21,5 +25,17 @@ public class RouteMasterHeadlinePanel extends JPanel {
         String infos = safeHtml(masterRelation.get("operator")) + " " + safeHtml(masterRelation.get("network"));
         String routeMasterText = MessageFormat.format("<html><h2>{0}</h2><div>{1}</div></html>", headline, infos);
         add(new UnBoldLabel(routeMasterText));
+
+        add(new JButton(new JosmAction(tr("Download routes"), "download", null, null, false) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DownloadUtils.downloadUsingOverpass(
+                        "org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/downloadRouteMaster.query.txt",
+                    line -> line
+                        .replace("##ROUTEIDS##", DownloadUtils.collectMemberIds(masterRelation, OsmPrimitiveType.RELATION))
+                        .replace("##MASTERID##", masterRelation.getRelation() != null ? masterRelation.getRelation().getId() + "" : Long.MAX_VALUE + ""));
+            }
+        }), BorderLayout.EAST);
     }
+
 }

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/StopCollector.java
@@ -1,5 +1,8 @@
 package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
 
+import org.openstreetmap.josm.actions.AutoScaleAction;
+import org.openstreetmap.josm.actions.JosmAction;
+import org.openstreetmap.josm.actions.relation.EditRelationAction;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 import org.openstreetmap.josm.data.osm.Relation;
@@ -7,8 +10,11 @@ import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
 import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmStopAreaRelationTags;
 import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.LineRelation.StopPositionEvent;
+import org.openstreetmap.josm.tools.ImageProvider;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -252,6 +258,10 @@ public class StopCollector {
         boolean matches(OsmPrimitive p);
 
         String getNameAndInfos();
+
+        default Component createActionButtons() {
+            return null;
+        }
     }
 
     private static class FoundStopPosition implements FoundStop {
@@ -293,6 +303,20 @@ public class StopCollector {
             String name = member.getMember().get("name");
             return (name == null ? tr("- Stop without name -") : name);
         }
+
+        @Override
+        public Component createActionButtons() {
+            return PublicTransportLinePanel.createActions(
+                PublicTransportLinePanel.createAction(
+                    tr("Zoom to stop position"),
+                    new ImageProvider("mapmode", "zoom"),
+                    () -> {
+                        member.getMember().getDataSet().setSelected(member.getMember());
+                        AutoScaleAction.autoScale(AutoScaleAction.AutoScaleMode.SELECTION);
+                    }
+                )
+            );
+        }
     }
 
     private static class FoundStopArea implements FoundStop {
@@ -302,7 +326,7 @@ public class StopCollector {
         FoundStopArea(Relation relation) {
             stops = relation.getMembers()
                 .stream()
-                .filter(m -> "stop".equals(m.getRole()))
+                .filter(m -> OsmStopAreaRelationTags.ROLE_STOP.equals(m.getRole()))
                 .collect(Collectors.toList());
             this.relation = relation;
         }
@@ -314,14 +338,31 @@ public class StopCollector {
 
         @Override
         public boolean matches(OsmPrimitive p) {
-            return this.stops.stream().anyMatch(s -> s.getMember().equals(p));
+            // Many are not marked as stop => we still want to get them.
+            return this.relation
+                .getMembers()
+                .stream().anyMatch(s -> s.getMember().equals(p));
         }
 
         @Override
         public String getNameAndInfos() {
             String name = relation.get("name");
-            return (name == null ? tr("- No name -") : name) + " (" + tr("area relation " + relation.getId()) + ")";
+            return (name == null ? tr("- No name -") : name) + " (" + tr("area relation {0}", relation.getId()) + ")";
         }
+
+        @Override
+        public Component createActionButtons() {
+            return PublicTransportLinePanel.createActions(
+                PublicTransportLinePanel.createAction(tr("Zoom to stop area relation"), new ImageProvider("mapmode", "zoom").setMaxSize(12),
+                    () -> {
+                        relation.getDataSet().setSelected(relation);
+                        AutoScaleAction.autoScale(AutoScaleAction.AutoScaleMode.SELECTION);
+                    }),
+                PublicTransportLinePanel.createAction(tr("Edit stop area"), new ImageProvider("dialogs", "edit"),
+                    () -> EditRelationAction.launchEditor(relation))
+            );
+        }
+
     }
 
     private static class NopStop implements FoundStop {
@@ -339,5 +380,14 @@ public class StopCollector {
         public String getNameAndInfos() {
             return "";
         }
+    }
+
+    private static JButton createActionButton(final String iconName, final String tool, final Runnable action) {
+        return new JButton(new JosmAction("", iconName, tool, null, false) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                action.run();
+            }
+        });
     }
 }

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/stopvicinity/StopVicinityTabManager.java
@@ -1,35 +1,32 @@
 package org.openstreetmap.josm.plugins.visualizeroutes.gui.stopvicinity;
 
-import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
 import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmStopAreaRelationTags;
 import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.AbstractTabManager;
-
-import javax.swing.*;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationAccess;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
-public class StopVicinityTabManager extends AbstractTabManager {
-    private final IRelationEditorActionAccess editorAccess;
+public class StopVicinityTabManager extends AbstractTabManager<StopVicinityPanel> {
     // Used so that zoom gets not lost when re-creating the view due to changes in the relation.
     private final ZoomSaver zoomSaver = new ZoomSaver();
 
     public StopVicinityTabManager(IRelationEditorActionAccess editorAccess) {
         super(editorAccess);
-        this.editorAccess = editorAccess;
     }
 
     @Override
-    protected TabAndDisplay getTabToShow(Relation relation) {
-        return new TabAndDisplay() {
+    protected TabAndDisplay<StopVicinityPanel> getTabToShow(IRelationEditorActionAccess editorAccess) {
+        return new TabAndDisplay<StopVicinityPanel>() {
             @Override
             public boolean shouldDisplay() {
-                return OsmStopAreaRelationTags.isStopArea(relation);
+                RelationAccess tagged = RelationAccess.of(editorAccess);
+                return OsmStopAreaRelationTags.isStopArea(tagged);
             }
 
             @Override
-            public JPanel getTabContent() {
-                return new StopVicinityPanel(relation, editorAccess, zoomSaver);
+            public StopVicinityPanel getTabContent() {
+                return new StopVicinityPanel(editorAccess, zoomSaver);
             }
 
             @Override
@@ -37,5 +34,10 @@ public class StopVicinityTabManager extends AbstractTabManager {
                 return tr("Vicinity");
             }
         };
+    }
+
+    @Override
+    protected void dispose(StopVicinityPanel view) {
+        view.dispose();
     }
 }

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/utils/DownloadUtils.java
@@ -1,0 +1,65 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.utils;
+
+import org.openstreetmap.josm.actions.downloadtasks.DownloadOsmTask;
+import org.openstreetmap.josm.actions.downloadtasks.DownloadParams;
+import org.openstreetmap.josm.data.Bounds;
+import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.gui.progress.swing.PleaseWaitProgressMonitor;
+import org.openstreetmap.josm.io.OverpassDownloadReader;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationAccess;
+import org.openstreetmap.josm.tools.Utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+public class DownloadUtils {
+    private DownloadUtils(){
+    }
+
+    public static void downloadUsingOverpass(String resource, Function<String, String> replacements) {
+        downloadUsingOverpass(resource, replacements, () -> {});
+    }
+    public static void downloadUsingOverpass(String resource, Function<String, String> replacements, Runnable onFinish) {
+        String query = new BufferedReader(new InputStreamReader(
+            Utils.getResourceAsStream(DownloadUtils.class, resource), StandardCharsets.UTF_8))
+            .lines()
+            .map(replacements)
+            .collect(Collectors.joining("\n"));
+
+
+        Bounds area = new Bounds(0, 0, 0, 0);
+        DownloadOsmTask task = new DownloadOsmTask();
+        task.setZoomAfterDownload(false);
+        task.download(
+            new OverpassDownloadReader(area, OverpassDownloadReader.OVERPASS_SERVER.get(), query),
+            new DownloadParams().withNewLayer(false), area, new PleaseWaitProgressMonitor(tr("Downloading data from overpass.")) {
+                @Override
+                public void doFinishTask() {
+                    onFinish.run();
+                }
+            });
+    }
+
+    public static String collectMemberIds(RelationAccess relation, OsmPrimitiveType type) {
+        // No need to filter => We can filter on overpass side.
+        String ids = relation.getMembers()
+            .stream()
+            .map(RelationMember::getMember)
+            .filter(it -> it.getType() == type)
+            .filter(it -> !it.isNew())
+            .map(it -> "" + it.getId())
+            .collect(Collectors.joining(","));
+        return ids.isEmpty() ? Long.MAX_VALUE + "": ids; // < a fake id, since Overpass requires at least one item
+    }
+
+    public static CharSequence escape(String s) {
+        // TODO
+        return s.replace("\"", "");
+    }
+}