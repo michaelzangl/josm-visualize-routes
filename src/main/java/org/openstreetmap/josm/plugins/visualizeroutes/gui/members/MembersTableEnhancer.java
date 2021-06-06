JOSM/pt_assistant

In the route list, mark stop indexes.

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/members/MembersTableEnhancer.java
@@ -1,0 +1,114 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.members;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.Objects;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+
+import org.openstreetmap.josm.gui.dialogs.relation.MemberTableColumnModel;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.RelationAccess;
+import org.openstreetmap.josm.tools.ImageProvider;
+
+public class MembersTableEnhancer {
+    private final IRelationEditorActionAccess editorAccess;
+
+    private RelationMemberValidator validator;
+
+    public MembersTableEnhancer(IRelationEditorActionAccess editorAccess) {
+        this.editorAccess = Objects.requireNonNull(editorAccess, "editorAccess");
+
+        updateValidator();
+
+        // Role column
+        enhanceRoleColumn();
+
+        // Primitive column
+        enhancePrimitiveColumn();
+
+        // event listeners
+        editorAccess.getTagModel().addTableModelListener(l -> {
+            updateValidator();
+        });
+        editorAccess.getMemberTableModel().addTableModelListener(l -> {
+            updateValidator();
+        });
+    }
+
+    private void enhancePrimitiveColumn() {
+        JLabel primitiveLabel = new JLabel();
+        primitiveLabel.setOpaque(false);
+        enhanceTableRenderer(getTableColumn(1), (table, row, column) -> {
+            String text = validator.getPrimitiveText(row, editorAccess.getMemberTableModel().getValue(row));
+            if (text != null && !text.isEmpty()) {
+                primitiveLabel.setText(text);
+                return primitiveLabel;
+            } else {
+                return null;
+            }
+        });
+    }
+
+    private TableColumn getTableColumn(int columnIndex) {
+        MemberTableColumnModel model = (MemberTableColumnModel) editorAccess.getMemberTable().getColumnModel();
+        return model.getColumn(columnIndex);
+    }
+
+    private void enhanceRoleColumn() {
+        TableColumn tableColumn = getTableColumn(0);
+        JLabel icon = new JLabel();
+        icon.setOpaque(false);
+        enhanceTableRenderer(tableColumn, (table, row, column) -> {
+            RoleValidationResult result = validator.validateAndSuggest(row, editorAccess.getMemberTableModel().getValue(row));
+            if (!result.isValid()) {
+                Dimension cellSize = table.getCellRect(row, column, false).getSize();
+                int size = Math.min(cellSize.width, cellSize.height);
+                icon.setIcon(new ImageProvider("warning-small").setSize(new Dimension(size, size)).get());
+                icon.setToolTipText(tr("Expected this role to be: " + result.getCorrectedRole()));
+                return icon;
+            } else {
+                return null;
+            }
+        });
+    }
+
+    private void updateValidator() {
+        RelationAccess relation = RelationAccess.of(editorAccess);
+        if (relation.hasTag(OSMTags.KEY_RELATION_TYPE, OSMTags.KEY_ROUTE)) {
+            validator = new RouteRelationMemberValidator(relation);
+        } else {
+            validator = new PresetRelationMemberValidator(relation);
+        }
+        // Triggers a repaint
+        editorAccess.getMemberTable().invalidate();
+    }
+
+    private void enhanceTableRenderer(TableColumn tableColumn, EastComponentSupplier eastComponent) {
+        TableCellRenderer parent = tableColumn.getCellRenderer();
+        JPanel panel = new JPanel(new BorderLayout());
+        tableColumn.setCellRenderer((table, value, isSelected, hasFocus, row, column) -> {
+            panel.removeAll();
+
+            panel.add(parent.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column));
+
+            JComponent e = eastComponent.get(table, row, column);
+            if (e != null) {
+                panel.add(e, BorderLayout.EAST);
+            }
+            return panel;
+        });
+    }
+
+    interface EastComponentSupplier {
+        JComponent get(JTable table, int row, int column);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/members/PresetRelationMemberValidator.java
@@ -1,0 +1,46 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.members;
+
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPreset;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPresetType;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPresets;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.RelationAccess;
+
+public class PresetRelationMemberValidator implements RelationMemberValidator {
+    private final Collection<TaggingPreset> presets;
+
+    public PresetRelationMemberValidator(RelationAccess relation) {
+        presets = TaggingPresets.getMatchingPresets(
+            EnumSet.of(relation.isMultipolygon() ? TaggingPresetType.MULTIPOLYGON : TaggingPresetType.RELATION),
+            relation.getTags(), false);
+    }
+
+    @Override
+    public RoleValidationResult validateAndSuggest(int memberIndex, RelationMember member) {
+        Set<String> roles = presets.stream()
+            .map(preset -> preset.suggestRoleForOsmPrimitive(member.getMember()))
+            .filter(Objects::nonNull)
+            .collect(Collectors.toSet());
+        if (roles.size() == 1) {
+            String role = roles.iterator().next();
+            if (role.equals(member.getRole())) {
+                return RoleValidationResult.valid();
+            } else {
+                return RoleValidationResult.wrongRole(role);
+            }
+        } else {
+            return RoleValidationResult.valid();
+        }
+    }
+
+    @Override
+    public String getPrimitiveText(int memberIndex, RelationMember member) {
+        return null;
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/members/RelationMemberValidator.java
@@ -1,0 +1,10 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.members;
+
+import org.openstreetmap.josm.data.osm.RelationMember;
+
+public interface RelationMemberValidator {
+
+    RoleValidationResult validateAndSuggest(int memberIndex, RelationMember member);
+
+    String getPrimitiveText(int memberIndex, RelationMember member);
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/members/RoleValidationResult.java
@@ -1,0 +1,38 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.members;
+
+import java.util.Objects;
+
+public class RoleValidationResult {
+
+    private final boolean valid;
+    private final String correctedRole;
+
+    private RoleValidationResult(boolean valid, String correctedRole) {
+        this.valid = valid;
+        this.correctedRole = correctedRole;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public String getCorrectedRole() {
+        return correctedRole;
+    }
+
+    @Override
+    public String toString() {
+        return "RoleValidationResult{" +
+            "valid=" + valid +
+            ", correctedRole='" + correctedRole + '\'' +
+            '}';
+    }
+
+    public static RoleValidationResult valid() {
+        return new RoleValidationResult(true, null);
+    }
+
+    public static RoleValidationResult wrongRole(String correctedRole) {
+        return new RoleValidationResult(false, Objects.requireNonNull(correctedRole, "correctedRole"));
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/members/RouteRelationMemberValidator.java
@@ -1,0 +1,64 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.members;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.RelationAccess;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.RouteStop;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.RouteStopPositionOnWay;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.RouteTraverser;
+
+public class RouteRelationMemberValidator implements RelationMemberValidator {
+    private final RouteTraverser route;
+    private final List<RouteStop> stops;
+    private final Map<OsmPrimitive, List<RouteStopPositionOnWay>> stopsReverse;
+
+    public RouteRelationMemberValidator(RelationAccess relation) {
+        route = new RouteTraverser(relation);
+        stops = route.findStopPositions();
+
+        stopsReverse = stops
+            .stream()
+            .filter(stop -> stop instanceof RouteStopPositionOnWay)
+            .map(stop -> (RouteStopPositionOnWay) stop)
+            .collect(Collectors.groupingBy(stop -> stop.getPosition().getWay().getWay()));
+    }
+
+    @Override
+    public RoleValidationResult validateAndSuggest(int memberIndex, RelationMember member) {
+        return RoleValidationResult.valid();
+    }
+
+    @Override
+    public String getPrimitiveText(int memberIndex, RelationMember member) {
+        if (OSMTags.STOPS_AND_PLATFORMS_ROLES.contains(member.getRole())) {
+            return stops.stream()
+                // TODO: Handle duplicate stops
+                .filter(stop -> stop.getRelationMembers().contains(member.getMember()))
+                .map(stop -> "<html>" + getStopText(stop) + "</html>")
+                .findFirst()
+                .orElse(null);
+        } else {
+            // Normal route part
+            List<RouteStopPositionOnWay> stops = stopsReverse.get(member.getMember());
+            if (stops != null) {
+                return "<html>" + stops.stream()
+                    .sorted(Comparator.comparing(stop -> stop.getPosition().getOffsetInRoute()))
+                    .map(this::getStopText)
+                    .collect(Collectors.joining(" ")) + "</html>";
+            } else {
+                return null;
+            }
+        }
+    }
+
+    private String getStopText(RouteStop stop) {
+        return "<font color=\"" + (stop instanceof RouteStopPositionOnWay ? "#4d9bff" : "#ff3636") + "\">"
+            + stop.getStopIndex() + "</font>";
+    }
+}

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/members/MembersTableEnhancer.java
@@ -1,0 +1,114 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.members;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.Objects;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+
+import org.openstreetmap.josm.gui.dialogs.relation.MemberTableColumnModel;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationAccess;
+import org.openstreetmap.josm.tools.ImageProvider;
+
+public class MembersTableEnhancer {
+    private final IRelationEditorActionAccess editorAccess;
+
+    private RelationMemberValidator validator;
+
+    public MembersTableEnhancer(IRelationEditorActionAccess editorAccess) {
+        this.editorAccess = Objects.requireNonNull(editorAccess, "editorAccess");
+
+        updateValidator();
+
+        // Role column
+        enhanceRoleColumn();
+
+        // Primitive column
+        enhancePrimitiveColumn();
+
+        // event listeners
+        editorAccess.getTagModel().addTableModelListener(l -> {
+            updateValidator();
+        });
+        editorAccess.getMemberTableModel().addTableModelListener(l -> {
+            updateValidator();
+        });
+    }
+
+    private void enhancePrimitiveColumn() {
+        JLabel primitiveLabel = new JLabel();
+        primitiveLabel.setOpaque(false);
+        enhanceTableRenderer(getTableColumn(1), (table, row, column) -> {
+            String text = validator.getPrimitiveText(row, editorAccess.getMemberTableModel().getValue(row));
+            if (text != null && !text.isEmpty()) {
+                primitiveLabel.setText(text);
+                return primitiveLabel;
+            } else {
+                return null;
+            }
+        });
+    }
+
+    private TableColumn getTableColumn(int columnIndex) {
+        MemberTableColumnModel model = (MemberTableColumnModel) editorAccess.getMemberTable().getColumnModel();
+        return model.getColumn(columnIndex);
+    }
+
+    private void enhanceRoleColumn() {
+        TableColumn tableColumn = getTableColumn(0);
+        JLabel icon = new JLabel();
+        icon.setOpaque(false);
+        enhanceTableRenderer(tableColumn, (table, row, column) -> {
+            RoleValidationResult result = validator.validateAndSuggest(row, editorAccess.getMemberTableModel().getValue(row));
+            if (!result.isValid()) {
+                Dimension cellSize = table.getCellRect(row, column, false).getSize();
+                int size = Math.min(cellSize.width, cellSize.height);
+                icon.setIcon(new ImageProvider("warning-small").setSize(new Dimension(size, size)).get());
+                icon.setToolTipText(tr("Expected this role to be: " + result.getCorrectedRole()));
+                return icon;
+            } else {
+                return null;
+            }
+        });
+    }
+
+    private void updateValidator() {
+        RelationAccess relation = RelationAccess.of(editorAccess);
+        if (OsmRouteRelationTags.isRoute(relation)) {
+            validator = new RouteRelationMemberValidator(relation);
+        } else {
+            validator = new PresetRelationMemberValidator(relation);
+        }
+        // Triggers a repaint
+        editorAccess.getMemberTable().invalidate();
+    }
+
+    private void enhanceTableRenderer(TableColumn tableColumn, EastComponentSupplier eastComponent) {
+        TableCellRenderer parent = tableColumn.getCellRenderer();
+        JPanel panel = new JPanel(new BorderLayout());
+        tableColumn.setCellRenderer((table, value, isSelected, hasFocus, row, column) -> {
+            panel.removeAll();
+
+            panel.add(parent.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column));
+
+            JComponent e = eastComponent.get(table, row, column);
+            if (e != null) {
+                panel.add(e, BorderLayout.EAST);
+            }
+            return panel;
+        });
+    }
+
+    interface EastComponentSupplier {
+        JComponent get(JTable table, int row, int column);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/members/PresetRelationMemberValidator.java
@@ -1,0 +1,46 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.members;
+
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPreset;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPresetType;
+import org.openstreetmap.josm.gui.tagging.presets.TaggingPresets;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationAccess;
+
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class PresetRelationMemberValidator implements RelationMemberValidator {
+    private final Collection<TaggingPreset> presets;
+
+    public PresetRelationMemberValidator(RelationAccess relation) {
+        presets = TaggingPresets.getMatchingPresets(
+            EnumSet.of(relation.isMultipolygon() ? TaggingPresetType.MULTIPOLYGON : TaggingPresetType.RELATION),
+            relation.getKeys(), false);
+    }
+
+    @Override
+    public RoleValidationResult validateAndSuggest(int memberIndex, RelationMember member) {
+        Set<String> roles = presets.stream()
+            .map(preset -> preset.suggestRoleForOsmPrimitive(member.getMember()))
+            .filter(Objects::nonNull)
+            .collect(Collectors.toSet());
+        if (roles.size() == 1) {
+            String role = roles.iterator().next();
+            if (role.equals(member.getRole())) {
+                return RoleValidationResult.valid();
+            } else {
+                return RoleValidationResult.wrongRole(role);
+            }
+        } else {
+            return RoleValidationResult.valid();
+        }
+    }
+
+    @Override
+    public String getPrimitiveText(int memberIndex, RelationMember member) {
+        return null;
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/members/RelationMemberValidator.java
@@ -1,0 +1,10 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.members;
+
+import org.openstreetmap.josm.data.osm.RelationMember;
+
+public interface RelationMemberValidator {
+
+    RoleValidationResult validateAndSuggest(int memberIndex, RelationMember member);
+
+    String getPrimitiveText(int memberIndex, RelationMember member);
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/members/RoleValidationResult.java
@@ -1,0 +1,38 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.members;
+
+import java.util.Objects;
+
+public class RoleValidationResult {
+
+    private final boolean valid;
+    private final String correctedRole;
+
+    private RoleValidationResult(boolean valid, String correctedRole) {
+        this.valid = valid;
+        this.correctedRole = correctedRole;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public String getCorrectedRole() {
+        return correctedRole;
+    }
+
+    @Override
+    public String toString() {
+        return "RoleValidationResult{" +
+            "valid=" + valid +
+            ", correctedRole='" + correctedRole + '\'' +
+            '}';
+    }
+
+    public static RoleValidationResult valid() {
+        return new RoleValidationResult(true, null);
+    }
+
+    public static RoleValidationResult wrongRole(String correctedRole) {
+        return new RoleValidationResult(false, Objects.requireNonNull(correctedRole, "correctedRole"));
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/members/RouteRelationMemberValidator.java
@@ -1,0 +1,64 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.members;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationAccess;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.RouteStop;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.RouteStopPositionOnWay;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.RouteTraverser;
+
+public class RouteRelationMemberValidator implements RelationMemberValidator {
+    private final RouteTraverser route;
+    private final List<RouteStop> stops;
+    private final Map<OsmPrimitive, List<RouteStopPositionOnWay>> stopsReverse;
+
+    public RouteRelationMemberValidator(RelationAccess relation) {
+        route = new RouteTraverser(relation);
+        stops = route.findStopPositions();
+
+        stopsReverse = stops
+            .stream()
+            .filter(stop -> stop instanceof RouteStopPositionOnWay)
+            .map(stop -> (RouteStopPositionOnWay) stop)
+            .collect(Collectors.groupingBy(stop -> stop.getPosition().getWay().getWay()));
+    }
+
+    @Override
+    public RoleValidationResult validateAndSuggest(int memberIndex, RelationMember member) {
+        return RoleValidationResult.valid();
+    }
+
+    @Override
+    public String getPrimitiveText(int memberIndex, RelationMember member) {
+        if (OsmRouteRelationTags.STOP_AND_PLATFORM_ROLES.contains(member.getRole())) {
+            return stops.stream()
+                // TODO: Handle duplicate stops
+                .filter(stop -> stop.getRelationMembers().contains(member.getMember()))
+                .map(stop -> "<html>" + getStopText(stop) + "</html>")
+                .findFirst()
+                .orElse(null);
+        } else {
+            // Normal route part
+            List<RouteStopPositionOnWay> stops = stopsReverse.get(member.getMember());
+            if (stops != null) {
+                return "<html>" + stops.stream()
+                    .sorted(Comparator.comparing(stop -> stop.getPosition().getOffsetInRoute()))
+                    .map(this::getStopText)
+                    .collect(Collectors.joining(" ")) + "</html>";
+            } else {
+                return null;
+            }
+        }
+    }
+
+    private String getStopText(RouteStop stop) {
+        return "<font color=\"" + (stop instanceof RouteStopPositionOnWay ? "#4d9bff" : "#ff3636") + "\">"
+            + stop.getStopIndex() + "</font>";
+    }
+}