JOSM/pt_assistant

Add GTFS file handler

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gtfs/data/Route.java
@@ -1,0 +1,98 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gtfs.data;
+
+
+import java.awt.Color;
+import java.util.Objects;
+
+/**
+ * Hierarcy:
+ * - Route: Top level route. Potentially the same as a route_master in OSM
+ * - TripPath: A sequence of stops. Potentially the same as a route (if used often enough)
+ * - Trip: The acutal trip. We don't care much about it.
+ */
+public class Route {
+
+    private final String id;
+    private final Agency agency;
+    private final String shortName;
+    private final String longName;
+    private final String desc;
+    private final GtfsRouteType type;
+    private final Color color;
+    private final Color textColor;
+    private final int sortOrder;
+
+    public Route(String id, Agency agency, String shortName, String longName,
+                 String desc, GtfsRouteType type,
+                 Color color, Color textColor, int sortOrder) {
+        this.id = Objects.requireNonNull(id, "id");
+        this.agency = Objects.requireNonNull(agency, "agency");
+        this.shortName = Objects.requireNonNull(shortName, "shortName");
+        this.longName = Objects.requireNonNull(longName, "longName");
+        this.desc = Objects.requireNonNull(desc, "desc");
+        this.type = type;
+        this.color = color;
+        this.textColor = textColor;
+        this.sortOrder = sortOrder;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Agency getAgency() {
+        return agency;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public String getLongName() {
+        return longName;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public GtfsRouteType getType() {
+        return type;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Color getColorSafe() {
+        return color == null ? Color.GRAY : color;
+    }
+
+    public Color getTextColor() {
+        return textColor;
+    }
+
+    public Color getTextColorSafe() {
+        return textColor == null ? Color.WHITE : textColor;
+    }
+
+    public int getSortOrder() {
+        return sortOrder;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Route{" +
+            "id='" + id + '\'' +
+            ", agency=" + agency +
+            ", shortName='" + shortName + '\'' +
+            ", longName='" + longName + '\'' +
+            ", desc='" + desc + '\'' +
+            ", type=" + type +
+            ", color='" + color + '\'' +
+            ", textColor='" + textColor + '\'' +
+            ", sortOrder=" + sortOrder +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gtfs/data/Stop.java
@@ -1,0 +1,54 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gtfs.data;
+
+import java.util.Objects;
+
+import org.openstreetmap.josm.data.coor.LatLon;
+
+public class Stop {
+
+    private final String id;
+    private final String name;
+    private final String desc;
+    private final LatLon latLon;
+
+    public Stop(Stop copyFrom) {
+        this(copyFrom.id, copyFrom.name, copyFrom.desc, copyFrom.latLon);
+    }
+
+    public Stop(String id, String name, String desc, LatLon latLon) {
+        this.id = Objects.requireNonNull(id, "id");
+        this.name = Objects.requireNonNull(name, "name");
+        this.desc = Objects.requireNonNull(desc, "desc");
+        this.latLon = Objects.requireNonNull(latLon, "latLon");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public LatLon getLatLon() {
+        return latLon;
+    }
+
+    public Stop getMostGenericStop() {
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Stop{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            ", desc='" + desc + '\'' +
+            ", latLon=" + latLon +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gtfs/data/StopGenericNode.java
@@ -1,0 +1,21 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gtfs.data;
+
+import java.util.Objects;
+
+public class StopGenericNode extends Stop {
+    private final StopStation station;
+
+    public StopGenericNode(Stop base, StopStation station) {
+        super(base);
+        this.station = Objects.requireNonNull(station, "station");
+    }
+
+    @Override
+    public Stop getMostGenericStop() {
+        return station.getMostGenericStop();
+    }
+
+    public StopStation getStation() {
+        return station;
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gtfs/data/StopStation.java
@@ -1,0 +1,7 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gtfs.data;
+
+public class StopStation extends Stop {
+    public StopStation(Stop base) {
+        super(base);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gtfs/readers/ShapeFileReader.java
@@ -1,0 +1,46 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gtfs.readers;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.plugins.pt_assistant.gtfs.data.Shape;
+import org.openstreetmap.josm.plugins.pt_assistant.gtfs.data.ShapePoint;
+import org.openstreetmap.josm.tools.Pair;
+
+public class ShapeFileReader extends AbstractGtfsFileReader {
+    @Override
+    protected String getFileName() {
+        return "shapes.txt";
+    }
+
+    private Map<String, Shape> getShapes() {
+        FieldAccessor shape = getRequiredAccessor("shape_id");
+        FieldAccessor lat = getRequiredAccessor("shape_pt_lat");
+        FieldAccessor lon = getRequiredAccessor("shape_pt_lon");
+        FieldAccessor sequence = getRequiredAccessor("shape_pt_sequence");
+        FieldAccessor distance = getOptionalAccessor("shape_dist_traveled", "0");
+
+        return streamLinesAs(lineAccess -> {
+            ShapePoint point = new ShapePoint(
+                new LatLon(Double.parseDouble(lineAccess.get(lat)), Double.parseDouble(lineAccess.get(lon))),
+                Integer.parseInt(lineAccess.get(sequence)),
+                Double.parseDouble(lineAccess.get(distance))
+            );
+            return new Pair<>(lineAccess.get(shape), point);
+        }).collect(Collectors.groupingBy(it -> it.a, Collectors.collectingAndThen(Collectors.mapping(it -> it.b, Collectors.toList()), Shape::new)));
+    }
+
+    public static Map<String, Shape> read(FileSystem zip) throws IOException {
+        ShapeFileReader reader = new ShapeFileReader();
+        if (reader.isFilePresent(zip)) {
+            reader.readFrom(zip);
+            return reader.getShapes();
+        } else {
+            return Collections.emptyMap();
+        }
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gtfs/readers/StopFileReader.java
@@ -1,0 +1,78 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gtfs.readers;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.plugins.pt_assistant.gtfs.data.Stop;
+import org.openstreetmap.josm.plugins.pt_assistant.gtfs.data.StopBoardingArea;
+import org.openstreetmap.josm.plugins.pt_assistant.gtfs.data.StopGenericNode;
+import org.openstreetmap.josm.plugins.pt_assistant.gtfs.data.StopPlatform;
+import org.openstreetmap.josm.plugins.pt_assistant.gtfs.data.StopStation;
+
+public class StopFileReader extends AbstractGtfsFileReader {
+    @Override
+    protected String getFileName() {
+        return "stops.txt";
+    }
+
+
+    public Map<String, Stop> getAll() {
+        FieldAccessor id = getRequiredAccessor("stop_id");
+        FieldAccessor name = getRequiredAccessor("stop_name");
+        FieldAccessor desc = getOptionalAccessor("stop_desc", "");
+        FieldAccessor lat = getRequiredAccessor("stop_lat");
+        FieldAccessor lon = getRequiredAccessor("stop_lon");
+        FieldAccessor type = getOptionalAccessor("location_type", "0");
+        FieldAccessor parentId = getOptionalAccessor("parent_station", "");
+
+        Function<LineAccess, Stop> base = lineAccess -> new Stop(
+            lineAccess.get(id),
+            lineAccess.get(name),
+            lineAccess.get(desc),
+            new LatLon(
+                Double.parseDouble(lineAccess.get(lat)),
+                Double.parseDouble(lineAccess.get(lon))
+            ));
+
+        Map<String, StopStation> stations = streamLinesAs(lineAccess -> lineAccess.get(type).equals("1"),
+            lineAccess -> new StopStation(
+                base.apply(lineAccess)
+            )).collect(Collectors.toConcurrentMap(Stop::getId, Function.identity()));
+
+        Map<String, StopPlatform> platforms = streamLinesAs(lineAccess -> lineAccess.get(type).equals("0"),
+            lineAccess -> new StopPlatform(
+                base.apply(lineAccess),
+                stations.get(lineAccess.get(parentId))
+            )).collect(Collectors.toConcurrentMap(Stop::getId, Function.identity()));
+
+        Map<String, Stop> all = Collections.synchronizedMap(new HashMap<>(stations));
+        all.putAll(platforms);
+        // Entrance/exit or Generic Node
+        streamLinesAs(lineAccess -> lineAccess.get(type).equals("2") || lineAccess.get(type).equals("3"),
+            lineAccess -> new StopGenericNode(
+                base.apply(lineAccess),
+                stations.get(lineAccess.get(parentId))
+            )).forEach(stop -> all.put(stop.getId(), stop));
+
+        // Boarding Area
+        streamLinesAs(lineAccess -> lineAccess.get(type).equals("4"),
+            lineAccess -> new StopBoardingArea(
+                base.apply(lineAccess),
+                platforms.get(lineAccess.get(parentId))
+            )).forEach(stop -> all.put(stop.getId(), stop));
+
+        return all;
+    }
+
+    public static Map<String, Stop> read(FileSystem zip) throws IOException {
+        StopFileReader stop = new StopFileReader();
+        stop.readFrom(zip);
+        return stop.getAll();
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gtfs/readers/StopTimeFileReader.java
@@ -1,0 +1,56 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gtfs.readers;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.plugins.pt_assistant.gtfs.data.Stop;
+import org.openstreetmap.josm.plugins.pt_assistant.gtfs.data.StopTime;
+import org.openstreetmap.josm.tools.Pair;
+
+public class StopTimeFileReader extends AbstractGtfsFileReader {
+    @Override
+    protected String getFileName() {
+        return "stop_times.txt";
+    }
+
+    private Map<String, List<StopTime>> getStopsByTrip(Map<String, Stop> stops) {
+        FieldAccessor trip = getRequiredAccessor("trip_id");
+        FieldAccessor stop = getRequiredAccessor("stop_id");
+        FieldAccessor distTraveled = getOptionalAccessor("shape_dist_traveled", "0");
+        FieldAccessor sequence = getRequiredAccessor("stop_sequence");
+
+        return streamLinesAs(lineAccess -> {
+            Stop myStop = stops.get(lineAccess.get(stop));
+            if (myStop == null) {
+                throw new IllegalArgumentException("Unknown stop: " + lineAccess.get(stop));
+            }
+            StopTime time = new StopTime(
+                myStop,
+                Double.parseDouble(lineAccess.get(distTraveled)),
+                Integer.parseInt(lineAccess.get(sequence))
+            );
+            return new Pair<>(lineAccess.get(trip), time);
+        })
+            .collect(Collectors.groupingBy(it -> it.a, Collectors.mapping(it -> it.b,
+                Collectors.collectingAndThen(Collectors.toList(),
+                    // Sort after collect => much more performant.
+                    list -> { list.sort(Comparator.comparing(StopTime::getSequence)); return list; } ))));
+    }
+
+    /**
+     * Read the stop times
+     * @return The stop times by trip_id
+     * @param zip ZIP file
+     * @param stops The stops that were already read
+     */
+    public static Map<String, List<StopTime>> read(FileSystem zip, Map<String, Stop> stops) throws IOException {
+        StopTimeFileReader reader = new StopTimeFileReader();
+        reader.readFrom(zip);
+        return reader.getStopsByTrip(stops);
+    }
+
+}

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gtfs/data/Route.java
@@ -1,0 +1,98 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data;
+
+
+import java.awt.Color;
+import java.util.Objects;
+
+/**
+ * Hierarcy:
+ * - Route: Top level route. Potentially the same as a route_master in OSM
+ * - TripPath: A sequence of stops. Potentially the same as a route (if used often enough)
+ * - Trip: The acutal trip. We don't care much about it.
+ */
+public class Route {
+
+    private final String id;
+    private final Agency agency;
+    private final String shortName;
+    private final String longName;
+    private final String desc;
+    private final GtfsRouteType type;
+    private final Color color;
+    private final Color textColor;
+    private final int sortOrder;
+
+    public Route(String id, Agency agency, String shortName, String longName,
+                 String desc, GtfsRouteType type,
+                 Color color, Color textColor, int sortOrder) {
+        this.id = Objects.requireNonNull(id, "id");
+        this.agency = Objects.requireNonNull(agency, "agency");
+        this.shortName = Objects.requireNonNull(shortName, "shortName");
+        this.longName = Objects.requireNonNull(longName, "longName");
+        this.desc = Objects.requireNonNull(desc, "desc");
+        this.type = type;
+        this.color = color;
+        this.textColor = textColor;
+        this.sortOrder = sortOrder;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Agency getAgency() {
+        return agency;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public String getLongName() {
+        return longName;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public GtfsRouteType getType() {
+        return type;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Color getColorSafe() {
+        return color == null ? Color.GRAY : color;
+    }
+
+    public Color getTextColor() {
+        return textColor;
+    }
+
+    public Color getTextColorSafe() {
+        return textColor == null ? Color.WHITE : textColor;
+    }
+
+    public int getSortOrder() {
+        return sortOrder;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Route{" +
+            "id='" + id + '\'' +
+            ", agency=" + agency +
+            ", shortName='" + shortName + '\'' +
+            ", longName='" + longName + '\'' +
+            ", desc='" + desc + '\'' +
+            ", type=" + type +
+            ", color='" + color + '\'' +
+            ", textColor='" + textColor + '\'' +
+            ", sortOrder=" + sortOrder +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gtfs/data/Stop.java
@@ -1,0 +1,54 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data;
+
+import java.util.Objects;
+
+import org.openstreetmap.josm.data.coor.LatLon;
+
+public class Stop {
+
+    private final String id;
+    private final String name;
+    private final String desc;
+    private final LatLon latLon;
+
+    public Stop(Stop copyFrom) {
+        this(copyFrom.id, copyFrom.name, copyFrom.desc, copyFrom.latLon);
+    }
+
+    public Stop(String id, String name, String desc, LatLon latLon) {
+        this.id = Objects.requireNonNull(id, "id");
+        this.name = Objects.requireNonNull(name, "name");
+        this.desc = Objects.requireNonNull(desc, "desc");
+        this.latLon = Objects.requireNonNull(latLon, "latLon");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public LatLon getLatLon() {
+        return latLon;
+    }
+
+    public Stop getMostGenericStop() {
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Stop{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            ", desc='" + desc + '\'' +
+            ", latLon=" + latLon +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gtfs/data/StopGenericNode.java
@@ -1,0 +1,21 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data;
+
+import java.util.Objects;
+
+public class StopGenericNode extends Stop {
+    private final StopStation station;
+
+    public StopGenericNode(Stop base, StopStation station) {
+        super(base);
+        this.station = Objects.requireNonNull(station, "station");
+    }
+
+    @Override
+    public Stop getMostGenericStop() {
+        return station.getMostGenericStop();
+    }
+
+    public StopStation getStation() {
+        return station;
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gtfs/data/StopStation.java
@@ -1,0 +1,7 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data;
+
+public class StopStation extends Stop {
+    public StopStation(Stop base) {
+        super(base);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gtfs/readers/ShapeFileReader.java
@@ -1,0 +1,46 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gtfs.readers;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data.Shape;
+import org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data.ShapePoint;
+import org.openstreetmap.josm.tools.Pair;
+
+public class ShapeFileReader extends AbstractGtfsFileReader {
+    @Override
+    protected String getFileName() {
+        return "shapes.txt";
+    }
+
+    private Map<String, Shape> getShapes() {
+        FieldAccessor shape = getRequiredAccessor("shape_id");
+        FieldAccessor lat = getRequiredAccessor("shape_pt_lat");
+        FieldAccessor lon = getRequiredAccessor("shape_pt_lon");
+        FieldAccessor sequence = getRequiredAccessor("shape_pt_sequence");
+        FieldAccessor distance = getOptionalAccessor("shape_dist_traveled", "0");
+
+        return streamLinesAs(lineAccess -> {
+            ShapePoint point = new ShapePoint(
+                new LatLon(Double.parseDouble(lineAccess.get(lat)), Double.parseDouble(lineAccess.get(lon))),
+                Integer.parseInt(lineAccess.get(sequence)),
+                Double.parseDouble(lineAccess.get(distance))
+            );
+            return new Pair<>(lineAccess.get(shape), point);
+        }).collect(Collectors.groupingBy(it -> it.a, Collectors.collectingAndThen(Collectors.mapping(it -> it.b, Collectors.toList()), Shape::new)));
+    }
+
+    public static Map<String, Shape> read(FileSystem zip) throws IOException {
+        ShapeFileReader reader = new ShapeFileReader();
+        if (reader.isFilePresent(zip)) {
+            reader.readFrom(zip);
+            return reader.getShapes();
+        } else {
+            return Collections.emptyMap();
+        }
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gtfs/readers/StopFileReader.java
@@ -1,0 +1,78 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gtfs.readers;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data.Stop;
+import org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data.StopBoardingArea;
+import org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data.StopGenericNode;
+import org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data.StopPlatform;
+import org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data.StopStation;
+
+public class StopFileReader extends AbstractGtfsFileReader {
+    @Override
+    protected String getFileName() {
+        return "stops.txt";
+    }
+
+
+    public Map<String, Stop> getAll() {
+        FieldAccessor id = getRequiredAccessor("stop_id");
+        FieldAccessor name = getRequiredAccessor("stop_name");
+        FieldAccessor desc = getOptionalAccessor("stop_desc", "");
+        FieldAccessor lat = getRequiredAccessor("stop_lat");
+        FieldAccessor lon = getRequiredAccessor("stop_lon");
+        FieldAccessor type = getOptionalAccessor("location_type", "0");
+        FieldAccessor parentId = getOptionalAccessor("parent_station", "");
+
+        Function<LineAccess, Stop> base = lineAccess -> new Stop(
+            lineAccess.get(id),
+            lineAccess.get(name),
+            lineAccess.get(desc),
+            new LatLon(
+                Double.parseDouble(lineAccess.get(lat)),
+                Double.parseDouble(lineAccess.get(lon))
+            ));
+
+        Map<String, StopStation> stations = streamLinesAs(lineAccess -> lineAccess.get(type).equals("1"),
+            lineAccess -> new StopStation(
+                base.apply(lineAccess)
+            )).collect(Collectors.toConcurrentMap(Stop::getId, Function.identity()));
+
+        Map<String, StopPlatform> platforms = streamLinesAs(lineAccess -> lineAccess.get(type).equals("0"),
+            lineAccess -> new StopPlatform(
+                base.apply(lineAccess),
+                stations.get(lineAccess.get(parentId))
+            )).collect(Collectors.toConcurrentMap(Stop::getId, Function.identity()));
+
+        Map<String, Stop> all = Collections.synchronizedMap(new HashMap<>(stations));
+        all.putAll(platforms);
+        // Entrance/exit or Generic Node
+        streamLinesAs(lineAccess -> lineAccess.get(type).equals("2") || lineAccess.get(type).equals("3"),
+            lineAccess -> new StopGenericNode(
+                base.apply(lineAccess),
+                stations.get(lineAccess.get(parentId))
+            )).forEach(stop -> all.put(stop.getId(), stop));
+
+        // Boarding Area
+        streamLinesAs(lineAccess -> lineAccess.get(type).equals("4"),
+            lineAccess -> new StopBoardingArea(
+                base.apply(lineAccess),
+                platforms.get(lineAccess.get(parentId))
+            )).forEach(stop -> all.put(stop.getId(), stop));
+
+        return all;
+    }
+
+    public static Map<String, Stop> read(FileSystem zip) throws IOException {
+        StopFileReader stop = new StopFileReader();
+        stop.readFrom(zip);
+        return stop.getAll();
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gtfs/readers/StopTimeFileReader.java
@@ -1,0 +1,56 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gtfs.readers;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data.Stop;
+import org.openstreetmap.josm.plugins.visualizeroutes.gtfs.data.StopTime;
+import org.openstreetmap.josm.tools.Pair;
+
+public class StopTimeFileReader extends AbstractGtfsFileReader {
+    @Override
+    protected String getFileName() {
+        return "stop_times.txt";
+    }
+
+    private Map<String, List<StopTime>> getStopsByTrip(Map<String, Stop> stops) {
+        FieldAccessor trip = getRequiredAccessor("trip_id");
+        FieldAccessor stop = getRequiredAccessor("stop_id");
+        FieldAccessor distTraveled = getOptionalAccessor("shape_dist_traveled", "0");
+        FieldAccessor sequence = getRequiredAccessor("stop_sequence");
+
+        return streamLinesAs(lineAccess -> {
+            Stop myStop = stops.get(lineAccess.get(stop));
+            if (myStop == null) {
+                throw new IllegalArgumentException("Unknown stop: " + lineAccess.get(stop));
+            }
+            StopTime time = new StopTime(
+                myStop,
+                Double.parseDouble(lineAccess.get(distTraveled)),
+                Integer.parseInt(lineAccess.get(sequence))
+            );
+            return new Pair<>(lineAccess.get(trip), time);
+        })
+            .collect(Collectors.groupingBy(it -> it.a, Collectors.mapping(it -> it.b,
+                Collectors.collectingAndThen(Collectors.toList(),
+                    // Sort after collect => much more performant.
+                    list -> { list.sort(Comparator.comparing(StopTime::getSequence)); return list; } ))));
+    }
+
+    /**
+     * Read the stop times
+     * @return The stop times by trip_id
+     * @param zip ZIP file
+     * @param stops The stops that were already read
+     */
+    public static Map<String, List<StopTime>> read(FileSystem zip, Map<String, Stop> stops) throws IOException {
+        StopTimeFileReader reader = new StopTimeFileReader();
+        reader.readFrom(zip);
+        return reader.getStopsByTrip(stops);
+    }
+
+}