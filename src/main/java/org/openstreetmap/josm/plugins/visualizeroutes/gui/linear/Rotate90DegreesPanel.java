JOSM/pt_assistant

Added platform for stop areas, put labels on top for big stops.

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/Rotate90DegreesPanel.java
@@ -1,0 +1,77 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import javax.swing.JPanel;
+import javax.swing.RepaintManager;
+
+public class Rotate90DegreesPanel extends JPanel {
+    public Rotate90DegreesPanel(Component child) {
+        super(new BorderLayout());
+        add(child);
+    }
+
+    @Override
+    protected void paintChildren(Graphics g) {
+        synchronized(getTreeLock()) {
+            for (int i = 0; i < getComponentCount(); i++) {
+                Component component = getComponent(i);
+                Graphics2D g2 = (Graphics2D) g.create(0, 0, getWidth(), getHeight());
+                g2.setColor(component.getForeground());
+                g2.setBackground(component.getBackground());
+
+                g2.rotate(-Math.PI / 2);
+                g2.translate(-getHeight(), 0);
+                g2.setClip(0, 0, getHeight(), getWidth());
+
+                Rectangle clipBounds = g.getClipBounds();
+                if (clipBounds == null) {
+                    clipBounds = new Rectangle(0, 0, getWidth(),
+                        getHeight());
+                }
+                //noinspection SuspiciousNameCombination
+                Rectangle rect = new Rectangle(getHeight() - clipBounds.y - clipBounds.height,
+                    clipBounds.x, clipBounds.height, clipBounds.width);
+                g2.setClip(rect);
+
+                // Need to disable repaint manager => it would trac the paint rect
+                RepaintManager cm = RepaintManager.currentManager(this);
+                RepaintManager.setCurrentManager(null);
+                component.paint(g2);
+                RepaintManager.setCurrentManager(cm);
+            }
+        }
+    }
+
+    @Override
+    public void doLayout() {
+        for (int i = 0; i < getComponentCount(); i++) {
+            getComponent(i).setSize(rot(getSize()));
+        }
+    }
+
+    @Override
+    public Dimension getMinimumSize() {
+        return rot(super.getMinimumSize());
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return rot(super.getPreferredSize());
+    }
+
+    @Override
+    public Dimension getMaximumSize() {
+        return rot(super.getMaximumSize());
+    }
+
+    private Dimension rot(Dimension size) {
+        //noinspection SuspiciousNameCombination
+        return new Dimension(size.height, size.width);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/lines/LineRefKeyPlatforms.java
@@ -1,0 +1,42 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear.lines;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+
+public class LineRefKeyPlatforms implements LineRefKey {
+    private final List<OsmPrimitive> platforms;
+
+    public LineRefKeyPlatforms(List<OsmPrimitive> platforms) {
+        this.platforms = platforms;
+    }
+
+    @Override
+    public String getRef() {
+        return tr("- multiple - ");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LineRefKeyPlatforms that = (LineRefKeyPlatforms) o;
+        return Objects.equals(platforms, that.platforms);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(platforms);
+    }
+
+
+    @Override
+    public String toString() {
+        return "LineRefKeyPlatforms{" +
+            "platforms=" + platforms +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/lines/LineRelationAroundStop.java
@@ -1,11 +1,8 @@
-package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear.lines;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -15,6 +12,7 @@ import java.util.stream.Stream;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.RelationAccess;
 import org.openstreetmap.josm.plugins.pt_assistant.utils.StopUtils;
 
 /**
@@ -34,6 +32,24 @@ public class LineRelationAroundStop extends LineRelation {
     }
 
     @Override
+    public LineRefKey getLineRefKey() {
+        List<OsmPrimitive> platforms = getRelation().getMembers()
+            .stream()
+            .filter(it -> it.getRole().equals("platform"))
+            .map(RelationMember::getMember)
+            .filter(isStop)
+            .distinct()
+            .collect(Collectors.toList());
+        if (platforms.size() == 1) {
+            return new LineRefKeyPlatform(platforms.get(0));
+        } else if (platforms.isEmpty()) {
+            return new LineRefKeyEmpty();
+        } else {
+            return new LineRefKeyPlatforms(platforms);
+        }
+    }
+
+    @Override
     public Stream<StopPositionEvent> streamStops() {
         List<RelationMember> stops = streamRawStops().collect(Collectors.toList());
 
@@ -49,8 +65,8 @@ public class LineRelationAroundStop extends LineRelation {
             lastArea = area;
         }
 
-        List<Integer> indexesAtWhichOurStopIs = IntStream.range(0, stops.size())
-            .filter(i -> stopsByStopArea.stream().anyMatch(s -> isStop.test(stops.get(i).getMember())))
+        List<Integer> indexesAtWhichOurStopIs = IntStream.range(0, stopsByStopArea.size())
+            .filter(i -> stopsByStopArea.get(i).stream().anyMatch(s -> isStop.test(s.getMember())))
             .boxed()
             .collect(Collectors.toList());
 

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/stops/EntryExit.java
@@ -1,8 +1,8 @@
-package org.openstreetmap.josm.plugins.pt_assistant.gui.linear;
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear.stops;
 
 import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
 
-enum EntryExit {
+public enum EntryExit {
     ENTRY,
     EXIT,
     BOTH;

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/stops/FoundStop.java
@@ -1,0 +1,17 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear.stops;
+
+import java.awt.Component;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+
+public interface FoundStop {
+    boolean isIncomplete();
+
+    boolean matches(OsmPrimitive p);
+
+    String getNameAndInfos();
+
+    default Component createActionButtons() {
+        return null;
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/stops/FoundStopArea.java
@@ -1,0 +1,62 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear.stops;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.Component;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.actions.AutoScaleAction;
+import org.openstreetmap.josm.actions.relation.EditRelationAction;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.PublicTransportLinePanel;
+import org.openstreetmap.josm.tools.ImageProvider;
+
+class FoundStopArea implements FoundStop {
+    private final List<RelationMember> stops;
+    private final Relation relation;
+
+    FoundStopArea(Relation relation) {
+        stops = relation.getMembers()
+            .stream()
+            .filter(m -> OSMTags.STOP_ROLE.equals(m.getRole()))
+            .collect(Collectors.toList());
+        this.relation = relation;
+    }
+
+    @Override
+    public boolean isIncomplete() {
+        return relation.isIncomplete();
+    }
+
+    @Override
+    public boolean matches(OsmPrimitive p) {
+        // Many are not marked as stop => we still want to get them.
+        return this.relation.equals(p) || this.relation
+            .getMembers()
+            .stream().anyMatch(s -> s.getMember().equals(p));
+    }
+
+    @Override
+    public String getNameAndInfos() {
+        String name = relation.get("name");
+        return (name == null ? tr("- No name -") : name) + " (" + tr("area relation {0}", relation.getId()) + ")";
+    }
+
+    @Override
+    public Component createActionButtons() {
+        return PublicTransportLinePanel.createActions(
+            PublicTransportLinePanel.createAction(tr("Zoom to stop area relation"), new ImageProvider("mapmode", "zoom").setMaxSize(12),
+                () -> {
+                    relation.getDataSet().setSelected(relation);
+                    AutoScaleAction.autoScale(AutoScaleAction.AutoScaleMode.SELECTION);
+                }),
+            PublicTransportLinePanel.createAction(tr("Edit stop area"), new ImageProvider("dialogs", "edit"),
+                () -> EditRelationAction.launchEditor(relation))
+        );
+    }
+
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/linear/stops/NopStop.java
@@ -1,0 +1,20 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.linear.stops;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+
+class NopStop implements FoundStop {
+    @Override
+    public boolean isIncomplete() {
+        return false;
+    }
+
+    @Override
+    public boolean matches(OsmPrimitive p) {
+        return false;
+    }
+
+    @Override
+    public String getNameAndInfos() {
+        return "";
+    }
+}

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/Rotate90DegreesPanel.java
@@ -1,0 +1,77 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import javax.swing.JPanel;
+import javax.swing.RepaintManager;
+
+public class Rotate90DegreesPanel extends JPanel {
+    public Rotate90DegreesPanel(Component child) {
+        super(new BorderLayout());
+        add(child);
+    }
+
+    @Override
+    protected void paintChildren(Graphics g) {
+        synchronized(getTreeLock()) {
+            for (int i = 0; i < getComponentCount(); i++) {
+                Component component = getComponent(i);
+                Graphics2D g2 = (Graphics2D) g.create(0, 0, getWidth(), getHeight());
+                g2.setColor(component.getForeground());
+                g2.setBackground(component.getBackground());
+
+                g2.rotate(-Math.PI / 2);
+                g2.translate(-getHeight(), 0);
+                g2.setClip(0, 0, getHeight(), getWidth());
+
+                Rectangle clipBounds = g.getClipBounds();
+                if (clipBounds == null) {
+                    clipBounds = new Rectangle(0, 0, getWidth(),
+                        getHeight());
+                }
+                //noinspection SuspiciousNameCombination
+                Rectangle rect = new Rectangle(getHeight() - clipBounds.y - clipBounds.height,
+                    clipBounds.x, clipBounds.height, clipBounds.width);
+                g2.setClip(rect);
+
+                // Need to disable repaint manager => it would trac the paint rect
+                RepaintManager cm = RepaintManager.currentManager(this);
+                RepaintManager.setCurrentManager(null);
+                component.paint(g2);
+                RepaintManager.setCurrentManager(cm);
+            }
+        }
+    }
+
+    @Override
+    public void doLayout() {
+        for (int i = 0; i < getComponentCount(); i++) {
+            getComponent(i).setSize(rot(getSize()));
+        }
+    }
+
+    @Override
+    public Dimension getMinimumSize() {
+        return rot(super.getMinimumSize());
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return rot(super.getPreferredSize());
+    }
+
+    @Override
+    public Dimension getMaximumSize() {
+        return rot(super.getMaximumSize());
+    }
+
+    private Dimension rot(Dimension size) {
+        //noinspection SuspiciousNameCombination
+        return new Dimension(size.height, size.width);
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/lines/LineRefKeyPlatforms.java
@@ -1,0 +1,42 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.lines;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+
+public class LineRefKeyPlatforms implements LineRefKey {
+    private final List<OsmPrimitive> platforms;
+
+    public LineRefKeyPlatforms(List<OsmPrimitive> platforms) {
+        this.platforms = platforms;
+    }
+
+    @Override
+    public String getRef() {
+        return tr("- multiple - ");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LineRefKeyPlatforms that = (LineRefKeyPlatforms) o;
+        return Objects.equals(platforms, that.platforms);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(platforms);
+    }
+
+
+    @Override
+    public String toString() {
+        return "LineRefKeyPlatforms{" +
+            "platforms=" + platforms +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/lines/LineRelationAroundStop.java
@@ -1,8 +1,9 @@
-package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.lines;
 
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationAccess;
 import org.openstreetmap.josm.plugins.visualizeroutes.gui.utils.StopAreaUtils;
 
 import java.util.ArrayList;
@@ -31,6 +32,24 @@ public class LineRelationAroundStop extends LineRelation {
     }
 
     @Override
+    public LineRefKey getLineRefKey() {
+        List<OsmPrimitive> platforms = getRelation().getMembers()
+            .stream()
+            .filter(it -> it.getRole().equals("platform"))
+            .map(RelationMember::getMember)
+            .filter(isStop)
+            .distinct()
+            .collect(Collectors.toList());
+        if (platforms.size() == 1) {
+            return new LineRefKeyPlatform(platforms.get(0));
+        } else if (platforms.isEmpty()) {
+            return new LineRefKeyEmpty();
+        } else {
+            return new LineRefKeyPlatforms(platforms);
+        }
+    }
+
+    @Override
     public Stream<StopPositionEvent> streamStops() {
         List<RelationMember> stops = streamRawStops().collect(Collectors.toList());
 
@@ -46,8 +65,8 @@ public class LineRelationAroundStop extends LineRelation {
             lastArea = area;
         }
 
-        List<Integer> indexesAtWhichOurStopIs = IntStream.range(0, stops.size())
-            .filter(i -> stopsByStopArea.stream().anyMatch(s -> isStop.test(stops.get(i).getMember())))
+        List<Integer> indexesAtWhichOurStopIs = IntStream.range(0, stopsByStopArea.size())
+            .filter(i -> stopsByStopArea.get(i).stream().anyMatch(s -> isStop.test(s.getMember())))
             .boxed()
             .collect(Collectors.toList());
 

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/stops/EntryExit.java
@@ -1,8 +1,8 @@
-package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear;
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.stops;
 
 import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
 
-enum EntryExit {
+public enum EntryExit {
     ENTRY,
     EXIT,
     BOTH;

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/stops/FoundStop.java
@@ -1,0 +1,17 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.stops;
+
+import java.awt.Component;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+
+public interface FoundStop {
+    boolean isIncomplete();
+
+    boolean matches(OsmPrimitive p);
+
+    String getNameAndInfos();
+
+    default Component createActionButtons() {
+        return null;
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/stops/FoundStopArea.java
@@ -1,0 +1,63 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.stops;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.Component;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openstreetmap.josm.actions.AutoScaleAction;
+import org.openstreetmap.josm.actions.relation.EditRelationAction;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmStopAreaGroupRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.PublicTransportLinePanel;
+import org.openstreetmap.josm.tools.ImageProvider;
+
+class FoundStopArea implements FoundStop {
+    private final List<RelationMember> stops;
+    private final Relation relation;
+
+    FoundStopArea(Relation relation) {
+        stops = relation.getMembers()
+            .stream()
+            .filter(m -> OsmStopAreaGroupRelationTags.ROLE_STOP.equals(m.getRole()))
+            .collect(Collectors.toList());
+        this.relation = relation;
+    }
+
+    @Override
+    public boolean isIncomplete() {
+        return relation.isIncomplete();
+    }
+
+    @Override
+    public boolean matches(OsmPrimitive p) {
+        // Many are not marked as stop => we still want to get them.
+        return this.relation.equals(p) || this.relation
+            .getMembers()
+            .stream().anyMatch(s -> s.getMember().equals(p));
+    }
+
+    @Override
+    public String getNameAndInfos() {
+        String name = relation.get("name");
+        return (name == null ? tr("- No name -") : name) + " (" + tr("area relation {0}", relation.getId()) + ")";
+    }
+
+    @Override
+    public Component createActionButtons() {
+        return PublicTransportLinePanel.createActions(
+            PublicTransportLinePanel.createAction(tr("Zoom to stop area relation"), new ImageProvider("mapmode", "zoom").setMaxSize(12),
+                () -> {
+                    relation.getDataSet().setSelected(relation);
+                    AutoScaleAction.autoScale(AutoScaleAction.AutoScaleMode.SELECTION);
+                }),
+            PublicTransportLinePanel.createAction(tr("Edit stop area"), new ImageProvider("dialogs", "edit"),
+                () -> EditRelationAction.launchEditor(relation))
+        );
+    }
+
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/linear/stops/NopStop.java
@@ -1,0 +1,20 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.stops;
+
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+
+class NopStop implements FoundStop {
+    @Override
+    public boolean isIncomplete() {
+        return false;
+    }
+
+    @Override
+    public boolean matches(OsmPrimitive p) {
+        return false;
+    }
+
+    @Override
+    public String getNameAndInfos() {
+        return "";
+    }
+}