JOSM/pt_assistant

Add the ability to start routing at a given point (for empty routes), allow to split ways.

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/router/FromNodeRouter.java
@@ -1,0 +1,44 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing.router;
+
+import java.util.Collections;
+import java.util.stream.Stream;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.RouteSegmentWay;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.transportmode.RouteType;
+import org.openstreetmap.josm.tools.Pair;
+
+/**
+ * Router that starts at a node.
+ */
+public class FromNodeRouter extends AbstractRouter {
+    private final Node node;
+    private final int insertionIndexInRelation;
+
+    public FromNodeRouter(Node node, int insertionIndexInRelation, RouteType type) {
+        super(type);
+        this.node = node;
+        this.insertionIndexInRelation = insertionIndexInRelation;
+    }
+
+    @Override
+    protected Stream<Pair<RouteSegmentWay, Double>> getRouterStartSegments() {
+        // All ways starting at node
+        return node
+            .referrers(Way.class)
+            .filter(way -> way.isFirstLastNode(node))
+            .map(way -> new Pair<>(type.createRouteSegmentWay(way, way.firstNode().equals(node),
+                insertionIndexInRelation, Collections.emptyList()), 0.0));
+    }
+
+    @Override
+    protected Node getRoutingStartNode() {
+        return node;
+    }
+
+    @Override
+    public int getIndexInMembersToAddAfter() {
+        return insertionIndexInRelation;
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/router/FromSegmentRouter.java
@@ -1,0 +1,71 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing.router;
+
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.RouteSegmentWay;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.RouteTarget;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.transportmode.RouteType;
+import org.openstreetmap.josm.tools.Pair;
+
+/**
+ * Allows to find possible routes after a given way segment.
+ */
+public class FromSegmentRouter extends AbstractRouter {
+    private final RouteSegmentWay startAfter;
+
+    public FromSegmentRouter(RouteSegmentWay startAfter, RouteType type) {
+        super(type);
+        this.startAfter = startAfter;
+        if (startAfter.lastNode().getDataSet() == null) {
+            throw new IllegalArgumentException("The route segment is not in the dataset");
+        }
+    }
+
+    public RouteSegmentWay getStartAfter() {
+        return startAfter;
+    }
+
+    @Override
+    protected RouteTarget createRouteTarget(LinkedList<RouteSegmentWay> trace) {
+        if (trace.size() == 1) {
+            // Only start segment
+            return null;
+        } else {
+            // Trace always contains the start segment, since we started at a segment
+            return new RouteTarget(trace.subList(1, trace.size()));
+        }
+    }
+
+    /**
+     * Find the ways at which this router should start searching for more ways.
+     * @return The set of ways. Includes the initial direciton and the distance of the start node of the way (may be negative)
+     */
+    @Override
+    protected Stream<Pair<RouteSegmentWay, Double>> getRouterStartSegments() {
+        return Stream.of(new Pair<>(startAfter, -startAfter.getLength()));
+    }
+
+    /**
+     * Get the start node at which the router should start searching.
+     * @return The start node
+     */
+    @Override
+    protected Node getRoutingStartNode() {
+        return startAfter.lastNode();
+    }
+
+    @Override
+    public int getIndexInMembersToAddAfter() {
+        return startAfter.getIndexInMembers();
+    }
+
+    @Override
+    protected Stream<Way> findWaysThatCanBeSplit() {
+        return super.findWaysThatCanBeSplit()
+            // Don't go back to previous way
+            .filter(it -> it != startAfter.getWay());
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/router/RouteSplitSuggestion.java
@@ -1,0 +1,78 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing.router;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.openstreetmap.josm.data.coor.ILatLon;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+
+/**
+ * A way that should be split at startAtIndex and endAtIndex
+ * <br/>
+ * Both may be at the end of the way, indicating that that index should be ignored
+ * <br/>
+ * startAtIndex and endAtIndex may be in any order on the way.
+ * They define the direciton the new route segment should go: it starts at startAtIndex and goes towards endAtIndex
+ */
+public class RouteSplitSuggestion {
+    private final Way way;
+    private final int startAtIndex;
+    private final int endAtIndex;
+    private final Node startAtNode;
+    private final Node endAtNode;
+
+    public RouteSplitSuggestion(Way way, int startAtIndex, int endAtIndex) {
+        this.way = way;
+        this.startAtIndex = startAtIndex;
+        this.endAtIndex = endAtIndex;
+        if (startAtIndex == endAtIndex) {
+            throw new IllegalArgumentException("Invalid split segment: Start and end are the same.");
+        }
+        this.startAtNode = way.getNode(startAtIndex);
+        this.endAtNode = way.getNode(endAtIndex);
+    }
+
+    public Way getWay() {
+        return way;
+    }
+
+    public Node getStartAtNode() {
+        return startAtNode;
+    }
+
+    public Node getEndAtNode() {
+        return endAtNode;
+    }
+
+    /**
+     * Get the segment that is before the new split segment (in the direction of the way, not of the split)
+     * @return The nodes of that segment. A list with one node if it is empty.
+     */
+    public List<Node> getSegmentBefore() {
+        return way.getNodes().subList(0, Math.min(startAtIndex, endAtIndex) + 1);
+    }
+
+    public List<Node> getSegment() {
+        return way.getNodes().subList(Math.min(startAtIndex, endAtIndex), Math.max(startAtIndex, endAtIndex) + 1);
+    }
+
+    public List<Node> getSegmentAfter() {
+        return way.getNodes().subList(Math.max(startAtIndex, endAtIndex), way.getNodesCount());
+    }
+
+    public Stream<Node> streamSplitNodes() {
+        return Stream.of(getStartAtNode(), getEndAtNode())
+            .filter(it -> !way.isFirstLastNode(it));
+    }
+
+    @Override
+    public String toString() {
+        return "RouteSplitSuggestion{" +
+            "way=" + way +
+            ", startAtNode=" + startAtNode +
+            ", endAtNode=" + endAtNode +
+            '}';
+    }
+
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/utils/AbstractVicinityPanel.java
@@ -6,6 +6,10 @@ import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+import java.awt.event.HierarchyEvent;
+import java.awt.event.HierarchyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -59,6 +63,7 @@ public abstract class AbstractVicinityPanel<D extends DerivedDataSet> extends JP
     protected final IRelationEditorActionAccess editorAccess;
     protected final MapView mapView;
     private final List<MapCSSStyleSource> style = Collections.unmodifiableList(readStyles());
+    private JComponent actionButtons;
 
     public AbstractVicinityPanel(D dataSetCopy,
                                  IRelationEditorActionAccess editorAccess,
@@ -112,27 +117,28 @@ public abstract class AbstractVicinityPanel<D extends DerivedDataSet> extends JP
     }
 
     private void addActionButtons() {
-        JComponent actionButtons = generateActionButtons();
+        actionButtons = generateActionButtons();
         if (actionButtons == null) {
             return;
         }
-        actionButtons.setSize(actionButtons.getPreferredSize());
-        setLocationToTopRight(mapView, actionButtons);
         mapView.add(actionButtons);
-        mapView.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                setLocationToTopRight(mapView, actionButtons);
-            }
-
-            @Override
-            public void componentShown(ComponentEvent e) {
-                setLocationToTopRight(mapView, actionButtons);
-            }
-        });
+        reLayoutActionButtons();
     }
 
-    private void setLocationToTopRight(MapView mapView, JComponent actionButtons) {
+    @Override
+    public void doLayout() {
+        super.doLayout();
+        reLayoutActionButtons();
+    }
+
+    protected void reLayoutActionButtons() {
+        if (actionButtons != null) {
+            actionButtons.setSize(actionButtons.getPreferredSize());
+            setLocationToTopRight(actionButtons);
+        }
+    }
+
+    private void setLocationToTopRight(JComponent actionButtons) {
         actionButtons.setLocation(mapView.getWidth() - actionButtons.getWidth() - 10, 10);
     }
 

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/router/FromNodeRouter.java
@@ -1,0 +1,44 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.router;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.RouteSegmentWay;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.transportmode.RouteType;
+import org.openstreetmap.josm.tools.Pair;
+
+import java.util.Collections;
+import java.util.stream.Stream;
+
+/**
+ * Router that starts at a node.
+ */
+public class FromNodeRouter extends AbstractRouter {
+    private final Node node;
+    private final int insertionIndexInRelation;
+
+    public FromNodeRouter(Node node, int insertionIndexInRelation, RouteType type) {
+        super(type);
+        this.node = node;
+        this.insertionIndexInRelation = insertionIndexInRelation;
+    }
+
+    @Override
+    protected Stream<Pair<RouteSegmentWay, Double>> getRouterStartSegments() {
+        // All ways starting at node
+        return node
+            .referrers(Way.class)
+            .filter(way -> way.isFirstLastNode(node))
+            .map(way -> new Pair<>(type.createRouteSegmentWay(way, way.firstNode().equals(node),
+                insertionIndexInRelation, Collections.emptyList()), 0.0));
+    }
+
+    @Override
+    protected Node getRoutingStartNode() {
+        return node;
+    }
+
+    @Override
+    public int getIndexInMembersToAddAfter() {
+        return insertionIndexInRelation;
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/router/FromSegmentRouter.java
@@ -1,0 +1,70 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.router;
+
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.RouteSegmentWay;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.transportmode.RouteType;
+import org.openstreetmap.josm.tools.Pair;
+
+/**
+ * Allows to find possible routes after a given way segment.
+ */
+public class FromSegmentRouter extends AbstractRouter {
+    private final RouteSegmentWay startAfter;
+
+    public FromSegmentRouter(RouteSegmentWay startAfter, RouteType type) {
+        super(type);
+        this.startAfter = startAfter;
+        if (startAfter.lastNode().getDataSet() == null) {
+            throw new IllegalArgumentException("The route segment is not in the dataset");
+        }
+    }
+
+    public RouteSegmentWay getStartAfter() {
+        return startAfter;
+    }
+
+    @Override
+    protected RouteTarget createRouteTarget(LinkedList<RouteSegmentWay> trace) {
+        if (trace.size() == 1) {
+            // Only start segment
+            return null;
+        } else {
+            // Trace always contains the start segment, since we started at a segment
+            return new RouteTarget(trace.subList(1, trace.size()));
+        }
+    }
+
+    /**
+     * Find the ways at which this router should start searching for more ways.
+     * @return The set of ways. Includes the initial direciton and the distance of the start node of the way (may be negative)
+     */
+    @Override
+    protected Stream<Pair<RouteSegmentWay, Double>> getRouterStartSegments() {
+        return Stream.of(new Pair<>(startAfter, -startAfter.getLength()));
+    }
+
+    /**
+     * Get the start node at which the router should start searching.
+     * @return The start node
+     */
+    @Override
+    protected Node getRoutingStartNode() {
+        return startAfter.lastNode();
+    }
+
+    @Override
+    public int getIndexInMembersToAddAfter() {
+        return startAfter.getIndexInMembers();
+    }
+
+    @Override
+    protected Stream<Way> findWaysThatCanBeSplit() {
+        return super.findWaysThatCanBeSplit()
+            // Don't go back to previous way
+            .filter(it -> it != startAfter.getWay());
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/router/RouteSplitSuggestion.java
@@ -1,0 +1,78 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.router;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.openstreetmap.josm.data.coor.ILatLon;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+
+/**
+ * A way that should be split at startAtIndex and endAtIndex
+ * <br/>
+ * Both may be at the end of the way, indicating that that index should be ignored
+ * <br/>
+ * startAtIndex and endAtIndex may be in any order on the way.
+ * They define the direciton the new route segment should go: it starts at startAtIndex and goes towards endAtIndex
+ */
+public class RouteSplitSuggestion {
+    private final Way way;
+    private final int startAtIndex;
+    private final int endAtIndex;
+    private final Node startAtNode;
+    private final Node endAtNode;
+
+    public RouteSplitSuggestion(Way way, int startAtIndex, int endAtIndex) {
+        this.way = way;
+        this.startAtIndex = startAtIndex;
+        this.endAtIndex = endAtIndex;
+        if (startAtIndex == endAtIndex) {
+            throw new IllegalArgumentException("Invalid split segment: Start and end are the same.");
+        }
+        this.startAtNode = way.getNode(startAtIndex);
+        this.endAtNode = way.getNode(endAtIndex);
+    }
+
+    public Way getWay() {
+        return way;
+    }
+
+    public Node getStartAtNode() {
+        return startAtNode;
+    }
+
+    public Node getEndAtNode() {
+        return endAtNode;
+    }
+
+    /**
+     * Get the segment that is before the new split segment (in the direction of the way, not of the split)
+     * @return The nodes of that segment. A list with one node if it is empty.
+     */
+    public List<Node> getSegmentBefore() {
+        return way.getNodes().subList(0, Math.min(startAtIndex, endAtIndex) + 1);
+    }
+
+    public List<Node> getSegment() {
+        return way.getNodes().subList(Math.min(startAtIndex, endAtIndex), Math.max(startAtIndex, endAtIndex) + 1);
+    }
+
+    public List<Node> getSegmentAfter() {
+        return way.getNodes().subList(Math.max(startAtIndex, endAtIndex), way.getNodesCount());
+    }
+
+    public Stream<Node> streamSplitNodes() {
+        return Stream.of(getStartAtNode(), getEndAtNode())
+            .filter(it -> !way.isFirstLastNode(it));
+    }
+
+    @Override
+    public String toString() {
+        return "RouteSplitSuggestion{" +
+            "way=" + way +
+            ", startAtNode=" + startAtNode +
+            ", endAtNode=" + endAtNode +
+            '}';
+    }
+
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/utils/AbstractVicinityPanel.java
@@ -23,14 +23,17 @@ import org.openstreetmap.josm.gui.mappaint.ElemStyles;
 import org.openstreetmap.josm.gui.mappaint.MultiCascade;
 import org.openstreetmap.josm.gui.mappaint.StyleSource;
 import org.openstreetmap.josm.gui.mappaint.mapcss.MapCSSStyleSource;
-import org.openstreetmap.josm.plugins.pt_assistant.data.DerivedDataSet;
+import org.openstreetmap.josm.plugins.visualizeroutes.data.DerivedDataSet;
 import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationAccess;
 import org.openstreetmap.josm.tools.Pair;
 import org.openstreetmap.josm.tools.Utils;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Point2D;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
@@ -45,6 +48,7 @@ public abstract class AbstractVicinityPanel<D extends DerivedDataSet> extends JP
     protected final IRelationEditorActionAccess editorAccess;
     protected final MapView mapView;
     private final List<MapCSSStyleSource> style = Collections.unmodifiableList(readStyles());
+    private JComponent actionButtons;
 
     public AbstractVicinityPanel(D dataSetCopy,
                                  IRelationEditorActionAccess editorAccess,
@@ -98,27 +102,28 @@ public abstract class AbstractVicinityPanel<D extends DerivedDataSet> extends JP
     }
 
     private void addActionButtons() {
-        JComponent actionButtons = generateActionButtons();
+        actionButtons = generateActionButtons();
         if (actionButtons == null) {
             return;
         }
-        actionButtons.setSize(actionButtons.getPreferredSize());
-        setLocationToTopRight(mapView, actionButtons);
         mapView.add(actionButtons);
-        mapView.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                setLocationToTopRight(mapView, actionButtons);
-            }
-
-            @Override
-            public void componentShown(ComponentEvent e) {
-                setLocationToTopRight(mapView, actionButtons);
-            }
-        });
+        reLayoutActionButtons();
     }
 
-    private void setLocationToTopRight(MapView mapView, JComponent actionButtons) {
+    @Override
+    public void doLayout() {
+        super.doLayout();
+        reLayoutActionButtons();
+    }
+
+    protected void reLayoutActionButtons() {
+        if (actionButtons != null) {
+            actionButtons.setSize(actionButtons.getPreferredSize());
+            setLocationToTopRight(actionButtons);
+        }
+    }
+
+    private void setLocationToTopRight(JComponent actionButtons) {
         actionButtons.setLocation(mapView.getWidth() - actionButtons.getWidth() - 10, 10);
     }
 