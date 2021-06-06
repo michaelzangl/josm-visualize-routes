JOSM/pt_assistant

Improve styling of stop area maps, add map panel for routes.

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/PointOnRoute.java
@@ -1,0 +1,42 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing;
+
+public class PointOnRoute {
+    private final RouteSegmentWay way;
+    private final double offsetFromSegmentStart;
+    private final double offsetInRoute;
+    private final double sidewardsDistanceFromRoute;
+
+    public PointOnRoute(RouteSegmentWay way, double offsetFromSegmentStart, double offsetInRoute, double sidewardsDistanceFromRoute) {
+        this.way = way;
+        this.offsetFromSegmentStart = offsetFromSegmentStart;
+        this.offsetInRoute = offsetInRoute;
+        this.sidewardsDistanceFromRoute = sidewardsDistanceFromRoute;
+    }
+
+    public RouteSegmentWay getWay() {
+        return way;
+    }
+
+    public double getOffsetFromSegmentStart() {
+        return offsetFromSegmentStart;
+    }
+
+    public double getOffsetInRoute() {
+        return offsetInRoute;
+    }
+
+    public double getSidewardsDistanceFromRoute() {
+        return sidewardsDistanceFromRoute;
+    }
+
+
+    @Override
+    public String toString() {
+        return "PointOnRoute{" +
+            "way=" + way +
+            ", offsetFromSegmentStart=" + offsetFromSegmentStart +
+            ", offsetInRoute=" + offsetInRoute +
+            ", sidewardsDistanceFromRoute=" + sidewardsDistanceFromRoute +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/RouteSegmentWay.java
@@ -1,0 +1,51 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing;
+
+import java.util.Objects;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+
+public class RouteSegmentWay {
+    private final Way way;
+    private final boolean forward;
+    // We need it so often …
+    private final double length;
+    private final WaySuitability suitability;
+
+    public RouteSegmentWay(Way way, boolean forward, WaySuitability suitability) {
+        this.way = Objects.requireNonNull(way, "way");
+        this.forward = forward;
+        this.length = way.getLength();
+        this.suitability = Objects.requireNonNull(suitability, "suitability");
+    }
+
+    public Way getWay() {
+        return way;
+    }
+
+    public boolean isForward() {
+        return forward;
+    }
+
+    public Node firstNode() {
+        return forward ? way.firstNode() : way.lastNode();
+    }
+
+    public Node lastNode() {
+        return forward ? way.lastNode() : way.firstNode();
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    @Override
+    public String toString() {
+        return "RouteSegmentWay{" +
+            "way=" + way +
+            ", forward=" + forward +
+            ", length=" + length +
+            ", suitability=" + suitability +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/RoutingTabManager.java
@@ -1,0 +1,37 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.RelationAccess;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.utils.AbstractTabManager;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.utils.ZoomSaver;
+
+public class RoutingTabManager extends AbstractTabManager<RoutingPanel> {
+    private final ZoomSaver zoomSaver = new ZoomSaver();
+
+    public RoutingTabManager(IRelationEditorActionAccess editorAccess) {
+        super(editorAccess);
+    }
+
+    @Override
+    protected TabAndDisplay<RoutingPanel> getTabToShow(IRelationEditorActionAccess editorAccess) {
+        return new TabAndDisplay<RoutingPanel>() {
+            @Override
+            public boolean shouldDisplay() {
+                return RelationAccess.of(editorAccess).hasTag(OSMTags.KEY_RELATION_TYPE, OSMTags.KEY_ROUTE);
+            }
+
+            @Override
+            public RoutingPanel getTabContent() {
+                return new RoutingPanel(editorAccess, zoomSaver);
+            }
+
+            @Override
+            public String getTitle() {
+                return tr("Map");
+            }
+        };
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/transportmode/BicycleRouteType.java
@@ -1,0 +1,39 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing.transportmode;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class BicycleRouteType implements RouteType {
+
+    @Override
+    public String getName() {
+        return tr("Bicycle route");
+    }
+
+    @Override
+    public String getTypeIdentifier() {
+        return "bicycle";
+    }
+
+    @Override
+    public List<String> getOneWayTags() {
+        return Arrays.asList("oneway:bicycle", "oneway");
+    }
+
+    @Override
+    public List<String> getAccessTags() {
+        return Arrays.asList("bicycle", "vehicle", "access");
+    }
+
+    @Override
+    public AccessDirection mayDriveOn(Map<String, String> tags) {
+        if (!tags.containsKey("highway")) {
+            return AccessDirection.NONE;
+        } else {
+            return RouteType.super.mayDriveOn(tags);
+        }
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/transportmode/BusRouteType.java
@@ -1,0 +1,28 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing.transportmode;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BusRouteType implements RouteType {
+    @Override
+    public String getName() {
+        return tr("Bus route");
+    }
+
+    @Override
+    public String getTypeIdentifier() {
+        return "bus";
+    }
+
+    @Override
+    public List<String> getOneWayTags() {
+        return Arrays.asList("oneway");
+    }
+
+    @Override
+    public List<String> getAccessTags() {
+        return Arrays.asList("bus", "psv", "motor_vehicle", "vehicle", "access");
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/transportmode/UnknownRouteType.java
@@ -1,0 +1,22 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing.transportmode;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.util.List;
+
+public class UnknownRouteType implements RouteType {
+    @Override
+    public String getName() {
+        return tr("Unknown route");
+    }
+
+    @Override
+    public String getTypeIdentifier() {
+        return "";
+    }
+
+    @Override
+    public String getOverpassSelectorForPossibleWays() {
+        return "";
+    }
+}

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/PointOnRoute.java
@@ -1,0 +1,42 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing;
+
+public class PointOnRoute {
+    private final RouteSegmentWay way;
+    private final double offsetFromSegmentStart;
+    private final double offsetInRoute;
+    private final double sidewardsDistanceFromRoute;
+
+    public PointOnRoute(RouteSegmentWay way, double offsetFromSegmentStart, double offsetInRoute, double sidewardsDistanceFromRoute) {
+        this.way = way;
+        this.offsetFromSegmentStart = offsetFromSegmentStart;
+        this.offsetInRoute = offsetInRoute;
+        this.sidewardsDistanceFromRoute = sidewardsDistanceFromRoute;
+    }
+
+    public RouteSegmentWay getWay() {
+        return way;
+    }
+
+    public double getOffsetFromSegmentStart() {
+        return offsetFromSegmentStart;
+    }
+
+    public double getOffsetInRoute() {
+        return offsetInRoute;
+    }
+
+    public double getSidewardsDistanceFromRoute() {
+        return sidewardsDistanceFromRoute;
+    }
+
+
+    @Override
+    public String toString() {
+        return "PointOnRoute{" +
+            "way=" + way +
+            ", offsetFromSegmentStart=" + offsetFromSegmentStart +
+            ", offsetInRoute=" + offsetInRoute +
+            ", sidewardsDistanceFromRoute=" + sidewardsDistanceFromRoute +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/RouteSegmentWay.java
@@ -1,0 +1,51 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing;
+
+import java.util.Objects;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+
+public class RouteSegmentWay {
+    private final Way way;
+    private final boolean forward;
+    // We need it so often …
+    private final double length;
+    private final WaySuitability suitability;
+
+    public RouteSegmentWay(Way way, boolean forward, WaySuitability suitability) {
+        this.way = Objects.requireNonNull(way, "way");
+        this.forward = forward;
+        this.length = way.getLength();
+        this.suitability = Objects.requireNonNull(suitability, "suitability");
+    }
+
+    public Way getWay() {
+        return way;
+    }
+
+    public boolean isForward() {
+        return forward;
+    }
+
+    public Node firstNode() {
+        return forward ? way.firstNode() : way.lastNode();
+    }
+
+    public Node lastNode() {
+        return forward ? way.lastNode() : way.firstNode();
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    @Override
+    public String toString() {
+        return "RouteSegmentWay{" +
+            "way=" + way +
+            ", forward=" + forward +
+            ", length=" + length +
+            ", suitability=" + suitability +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/RoutingTabManager.java
@@ -1,0 +1,37 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing;
+
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationAccess;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.utils.AbstractTabManager;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.utils.ZoomSaver;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+public class RoutingTabManager extends AbstractTabManager<RoutingPanel> {
+    private final ZoomSaver zoomSaver = new ZoomSaver();
+
+    public RoutingTabManager(IRelationEditorActionAccess editorAccess) {
+        super(editorAccess);
+    }
+
+    @Override
+    protected TabAndDisplay<RoutingPanel> getTabToShow(IRelationEditorActionAccess editorAccess) {
+        return new TabAndDisplay<RoutingPanel>() {
+            @Override
+            public boolean shouldDisplay() {
+                return RelationAccess.of(editorAccess).hasTag(OsmRouteRelationTags.KEY_TYPE, OsmRouteRelationTags.KEY_TYPE_VALUE_ROUTE);
+            }
+
+            @Override
+            public RoutingPanel getTabContent() {
+                return new RoutingPanel(editorAccess, zoomSaver);
+            }
+
+            @Override
+            public String getTitle() {
+                return tr("Map");
+            }
+        };
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/transportmode/BicycleRouteType.java
@@ -1,0 +1,39 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.transportmode;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class BicycleRouteType implements RouteType {
+
+    @Override
+    public String getName() {
+        return tr("Bicycle route");
+    }
+
+    @Override
+    public String getTypeIdentifier() {
+        return "bicycle";
+    }
+
+    @Override
+    public List<String> getOneWayTags() {
+        return Arrays.asList("oneway:bicycle", "oneway");
+    }
+
+    @Override
+    public List<String> getAccessTags() {
+        return Arrays.asList("bicycle", "vehicle", "access");
+    }
+
+    @Override
+    public AccessDirection mayDriveOn(Map<String, String> tags) {
+        if (!tags.containsKey("highway")) {
+            return AccessDirection.NONE;
+        } else {
+            return RouteType.super.mayDriveOn(tags);
+        }
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/transportmode/BusRouteType.java
@@ -1,0 +1,28 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.transportmode;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BusRouteType implements RouteType {
+    @Override
+    public String getName() {
+        return tr("Bus route");
+    }
+
+    @Override
+    public String getTypeIdentifier() {
+        return "bus";
+    }
+
+    @Override
+    public List<String> getOneWayTags() {
+        return Arrays.asList("oneway");
+    }
+
+    @Override
+    public List<String> getAccessTags() {
+        return Arrays.asList("bus", "psv", "motor_vehicle", "vehicle", "access");
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/transportmode/UnknownRouteType.java
@@ -1,0 +1,22 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.transportmode;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.util.List;
+
+public class UnknownRouteType implements RouteType {
+    @Override
+    public String getName() {
+        return tr("Unknown route");
+    }
+
+    @Override
+    public String getTypeIdentifier() {
+        return "";
+    }
+
+    @Override
+    public String getOverpassSelectorForPossibleWays() {
+        return "";
+    }
+}