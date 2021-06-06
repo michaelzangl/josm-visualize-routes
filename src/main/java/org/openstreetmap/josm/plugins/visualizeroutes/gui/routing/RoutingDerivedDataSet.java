JOSM/pt_assistant

Allow removing members from the route relation.

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/RoutingDerivedDataSet.java
@@ -1,0 +1,116 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing;
+
+import java.util.List;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
+import org.openstreetmap.josm.plugins.pt_assistant.data.DerivedDataSet;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.RelationAccess;
+
+class RoutingDerivedDataSet extends DerivedDataSet {
+    private final IRelationEditorActionAccess editorAccess;
+    private final RouteTraverser routeTraverser;
+
+    public RoutingDerivedDataSet(IRelationEditorActionAccess editorAccess) {
+        super(editorAccess.getEditor().getLayer().getDataSet());
+        this.editorAccess = editorAccess;
+        this.routeTraverser = new RouteTraverser(RelationAccess.of(editorAccess));
+    }
+
+    public RouteTraverser getRouteTraverser() {
+        return routeTraverser;
+    }
+
+    @Override
+    protected void addAdditionalGeometry(AdditionalGeometryAccess addTo) {
+        RelationAccess relation = RelationAccess.of(editorAccess);
+
+        relation.getMembers()
+            .forEach(member -> {
+                if (member.isNode()) {
+                    addOrGetDerived(withStopOrPlatformFlag(new Node(member.getNode()), member.getRole()));
+                } else if (member.isWay()) {
+                    addOrGetDerived(withStopOrPlatformFlag(new Way(member.getWay()), member.getRole()));
+                } else if (member.isRelation()) {
+                    addOrGetDerived(withStopOrPlatformFlag(new Relation(member.getRelation()), member.getRole()));
+                }
+            });
+
+        // Now we mark the route geometry as actual driveable way.
+        List<RouteSegment> segments = routeTraverser.getSegments();
+        for (int i = 0; i < segments.size(); i++) {
+            RouteSegment segment = segments.get(i);
+            // Highlights all segments currently in the route
+            segment.getWays().forEach(way -> {
+                OsmPrimitive p = addOrGetDerived(way.getWay());
+                if (p.hasTag(RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE)) {
+                    p.put(RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE, RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE_VALUE_MULTIPLE);
+                } else {
+                    if (way.isForward()) {
+                        p.put(RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE, RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE_VALUE_FORWARD);
+                    } else {
+                        p.put(RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE, RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE_VALUE_BACKWARD);
+                    }
+                }
+            });
+            // Mark the gaps in the line / end of the line
+            // normal => everything is fine
+            // broken => there should not be a gap there.
+            OsmPrimitive first = addOrGetDerived(segment.getWays().get(0).firstNode());
+            first.put(RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_STARTS, i == 0 ? RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_VALUE_NORMAL : RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_VALUE_BROKEN);
+            OsmPrimitive last = addOrGetDerived(segment.getWays().get(segment.getWays().size() - 1).lastNode());
+            last.put(RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_ENDS, i == segments.size() - 1 ? RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_VALUE_NORMAL : RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_VALUE_BROKEN);
+        }
+
+        // Stops on the route
+        List<RouteStop> stops = routeTraverser.findStopPositions();
+        double lastOffsetOnRoute = 0;
+        for (RouteStop stop : stops) {
+            OsmPrimitive stopNode = addOrGetDerived(stop.getRelationMembers().get(0));
+            stop.getStopAttributes().getKeys().forEach((k, v) -> stopNode.put("activeRelationStop_" + k, v));
+            stopNode.put(RoutingPanel.TAG_ACTIVE_RELATION_STOP_INDEX, stop.getStopIndex() + "");
+            if (stop instanceof RouteStopPositionOnWay) {
+                stopNode.put(RoutingPanel.ACTIVE_RELATION_STOP_OFFSET,
+                    "" + (int) Math.round(((RouteStopPositionOnWay) stop).getPosition().getOffsetInRoute()));
+
+                PointOnRoute myOffsetOnRoute = ((RouteStopPositionOnWay) stop).getPosition();
+                if (myOffsetOnRoute.getOffsetInRoute() < lastOffsetOnRoute - 10) { // <10m error
+                    stopNode.put(RoutingPanel.TAG_ACTIVE_RELATION_STOP_MISORDERED, "1");
+                }
+                lastOffsetOnRoute = myOffsetOnRoute.getOffsetInRoute();
+            } else {
+                stopNode.put(RoutingPanel.TAG_ACTIVE_RELATION_STOP_TOO_FAR, "1");
+                // Do not change lastOffsetOnRoute, so that next stop might get error
+            }
+        }
+    }
+
+    private OsmPrimitive withStopOrPlatformFlag(OsmPrimitive p, String role) {
+        if (OSMTags.ROUTE_SEGMENT_PT_ROLES.contains(role)) {
+            p.put(RoutingPanel.TAG_MEMBER_OF_ACTIVE_RELATION, RoutingPanel.TAG_MEMBER_OF_ACTIVE_RELATION_VALUE_ROUTE);
+        } else if (OSMTags.PLATFORM_ROLES.contains(role)) {
+            p.put(RoutingPanel.TAG_MEMBER_OF_ACTIVE_RELATION, RoutingPanel.TAG_MEMBER_OF_ACTIVE_RELATION_VALUE_PLATFORM);
+        } else if (OSMTags.STOP_ROLE.contains(role)) {
+            p.put(RoutingPanel.TAG_MEMBER_OF_ACTIVE_RELATION, RoutingPanel.TAG_MEMBER_OF_ACTIVE_RELATION_VALUE_STOP);
+        } else {
+            p.put(RoutingPanel.TAG_MEMBER_OF_ACTIVE_RELATION, RoutingPanel.TAG_MEMBER_OF_ACTIVE_RELATION_VALUE_UNKNOWN);
+        }
+        return p;
+    }
+
+    @Override
+    protected boolean isIncluded(OsmPrimitive primitive) {
+        if (primitive instanceof Way) {
+            return primitive.hasTag("highway")
+                || primitive.hasTag("railway");
+        } else if (primitive instanceof Relation) {
+            return primitive.hasTag("type", "route");
+        } else {
+            return false;
+        }
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/stoparea/StopVicinityDerivedDataSet.java
@@ -1,0 +1,77 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.stoparea;
+
+import org.openstreetmap.josm.data.osm.BBox;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.pt_assistant.data.DerivedDataSet;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.RelationEditorAccessUtils;
+import org.openstreetmap.josm.plugins.pt_assistant.utils.StopUtils;
+
+class StopVicinityDerivedDataSet extends DerivedDataSet {
+    private final BBox bBox;
+    private final long editedRelationId;
+    private final Relation stopRelation;
+    private final IRelationEditorActionAccess editorAccess;
+
+    public StopVicinityDerivedDataSet(IRelationEditorActionAccess editorAccess) {
+        super(editorAccess.getEditor().getLayer().getDataSet());
+        this.stopRelation = editorAccess.getEditor().getRelation();
+        this.editorAccess = editorAccess;
+        this.editedRelationId = stopRelation == null ? 0 : stopRelation.getId();
+        this.bBox = new BBox();
+        RelationEditorAccessUtils.streamMembers(editorAccess)
+            // Extra space: Something around 200.500m depending on where we are on the map.
+            .forEach(p -> bBox.addPrimitive(p.getMember(), 0.005));
+
+    }
+
+    @Override
+    protected boolean isIncluded(OsmPrimitive primitive) {
+        return primitive.getType() != OsmPrimitiveType.RELATION
+            // Normal primitives: all in bbox
+            ? bBox.intersects(primitive.getBBox())
+            // Relations: all except the one we edit
+            // todo: restrict this, e.g. only PT relations + multipolygons in bbox
+            : primitive.getId() != editedRelationId;
+    }
+
+    @Override
+    protected void addAdditionalGeometry(AdditionalGeometryAccess addTo) {
+        // Now apply the relation editor changes
+        // Simulate org.openstreetmap.josm.gui.dialogs.relation.actions.SavingAction.applyChanges
+        Relation relation = new Relation();
+        editorAccess.getTagModel().applyToPrimitive(relation);
+        // This is a hack to tag our currently active relation.
+        // There is no id selector in MapCSS, so we need a way to uniquely identify our relation
+        relation.put("activePtRelation", "1");
+
+        if (stopRelation != null) {
+            addTo.addAsCopy(stopRelation, relation);
+
+            // Now we search for all sibling relations.
+            // Due to https://josm.openstreetmap.de/ticket/6129#comment:24 we cannot do it in MapCSS
+            stopRelation.getReferrers()
+                .stream()
+                .filter(r -> r instanceof Relation && StopUtils.isStopAreaGroup((Relation) r))
+                .flatMap(parent -> ((Relation) parent).getMembers().stream())
+                .map(RelationMember::getMember)
+                .filter(sibling -> sibling != stopRelation)
+                .filter(sibling -> sibling instanceof Relation) // < some group relations contain invalid members
+                .forEach(sibling -> {
+                    Relation copy = new Relation((Relation) sibling);
+                    copy.put("siblingOfActive", "1");
+                    // This will add the copy with the fake tag.
+                    addOrGetDerived(copy);
+                });
+
+        } else {
+            addTo.add(relation);
+        }
+
+        RelationEditorAccessUtils.getRelationMembers(editorAccess)
+            .forEach(m -> relation.addMember(addOrGetDerivedMember(m)));
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/utils/IncompleteMembersWarningPanel.java
@@ -1,4 +1,4 @@
-package org.openstreetmap.josm.plugins.pt_assistant.gui.stoparea;
+package org.openstreetmap.josm.plugins.pt_assistant.gui.utils;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/RoutingDerivedDataSet.java
@@ -1,0 +1,118 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing;
+
+import java.util.List;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.pt_assistant.data.DerivedDataSet;
+import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmRouteRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationAccess;
+
+import static org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.RoutingPanel.*;
+
+class RoutingDerivedDataSet extends DerivedDataSet {
+    private final IRelationEditorActionAccess editorAccess;
+    private final RouteTraverser routeTraverser;
+
+    public RoutingDerivedDataSet(IRelationEditorActionAccess editorAccess) {
+        super(editorAccess.getEditor().getLayer().getDataSet());
+        this.editorAccess = editorAccess;
+        this.routeTraverser = new RouteTraverser(RelationAccess.of(editorAccess));
+    }
+
+    public RouteTraverser getRouteTraverser() {
+        return routeTraverser;
+    }
+
+    @Override
+    protected void addAdditionalGeometry(AdditionalGeometryAccess addTo) {
+        RelationAccess relation = RelationAccess.of(editorAccess);
+
+        relation.getMembers()
+            .forEach(member -> {
+                if (member.isNode()) {
+                    addOrGetDerived(withStopOrPlatformFlag(new Node(member.getNode()), member.getRole()));
+                } else if (member.isWay()) {
+                    addOrGetDerived(withStopOrPlatformFlag(new Way(member.getWay()), member.getRole()));
+                } else if (member.isRelation()) {
+                    addOrGetDerived(withStopOrPlatformFlag(new Relation(member.getRelation()), member.getRole()));
+                }
+            });
+
+        // Now we mark the route geometry as actual driveable way.
+        List<RouteSegment> segments = routeTraverser.getSegments();
+        for (int i = 0; i < segments.size(); i++) {
+            RouteSegment segment = segments.get(i);
+            // Highlights all segments currently in the route
+            segment.getWays().forEach(way -> {
+                OsmPrimitive p = addOrGetDerived(way.getWay());
+                if (p.hasTag(RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE)) {
+                    p.put(RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE, RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE_VALUE_MULTIPLE);
+                } else {
+                    if (way.isForward()) {
+                        p.put(RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE, RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE_VALUE_FORWARD);
+                    } else {
+                        p.put(RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE, RoutingPanel.TAG_PART_OF_ACTIVE_ROUTE_VALUE_BACKWARD);
+                    }
+                }
+            });
+            // Mark the gaps in the line / end of the line
+            // normal => everything is fine
+            // broken => there should not be a gap there.
+            OsmPrimitive first = addOrGetDerived(segment.getWays().get(0).firstNode());
+            first.put(RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_STARTS, i == 0 ? RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_VALUE_NORMAL : RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_VALUE_BROKEN);
+            OsmPrimitive last = addOrGetDerived(segment.getWays().get(segment.getWays().size() - 1).lastNode());
+            last.put(RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_ENDS, i == segments.size() - 1 ? RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_VALUE_NORMAL : RoutingPanel.TAG_ACTIVE_RELATION_SEGMENT_VALUE_BROKEN);
+        }
+
+        // Stops on the route
+        List<RouteStop> stops = routeTraverser.findStopPositions();
+        double lastOffsetOnRoute = 0;
+        for (RouteStop stop : stops) {
+            OsmPrimitive stopNode = addOrGetDerived(stop.getRelationMembers().get(0));
+            stop.getStopAttributes().getKeys().forEach((k, v) -> stopNode.put("activeRelationStop_" + k, v));
+            stopNode.put(RoutingPanel.TAG_ACTIVE_RELATION_STOP_INDEX, stop.getStopIndex() + "");
+            if (stop instanceof RouteStopPositionOnWay) {
+                stopNode.put(RoutingPanel.ACTIVE_RELATION_STOP_OFFSET,
+                    "" + (int) Math.round(((RouteStopPositionOnWay) stop).getPosition().getOffsetInRoute()));
+
+                PointOnRoute myOffsetOnRoute = ((RouteStopPositionOnWay) stop).getPosition();
+                if (myOffsetOnRoute.getOffsetInRoute() < lastOffsetOnRoute - 10) { // <10m error
+                    stopNode.put(RoutingPanel.TAG_ACTIVE_RELATION_STOP_MISORDERED, "1");
+                }
+                lastOffsetOnRoute = myOffsetOnRoute.getOffsetInRoute();
+            } else {
+                stopNode.put(RoutingPanel.TAG_ACTIVE_RELATION_STOP_TOO_FAR, "1");
+                // Do not change lastOffsetOnRoute, so that next stop might get error
+            }
+        }
+    }
+
+    private OsmPrimitive withStopOrPlatformFlag(OsmPrimitive p, String role) {
+        if (OsmRouteRelationTags.ROLES_SEGMENT.contains(role)) {
+            p.put(TAG_MEMBER_OF_ACTIVE_RELATION, TAG_MEMBER_OF_ACTIVE_RELATION_VALUE_ROUTE);
+        } else if (OsmRouteRelationTags.PLATFORM_ROLES.contains(role)) {
+            p.put(TAG_MEMBER_OF_ACTIVE_RELATION, TAG_MEMBER_OF_ACTIVE_RELATION_VALUE_PLATFORM);
+        } else if (OsmRouteRelationTags.STOP_ROLES.contains(role)) {
+            p.put(TAG_MEMBER_OF_ACTIVE_RELATION, TAG_MEMBER_OF_ACTIVE_RELATION_VALUE_STOP);
+        } else {
+            p.put(TAG_MEMBER_OF_ACTIVE_RELATION, TAG_MEMBER_OF_ACTIVE_RELATION_VALUE_UNKNOWN);
+        }
+        return p;
+    }
+
+    @Override
+    protected boolean isIncluded(OsmPrimitive primitive) {
+        if (primitive instanceof Way) {
+            return primitive.hasTag("highway")
+                || primitive.hasTag("railway");
+        } else if (primitive instanceof Relation) {
+            return primitive.hasTag("type", "route");
+        } else {
+            return false;
+        }
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/stoparea/StopVicinityDerivedDataSet.java
@@ -1,0 +1,73 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.stoparea;
+
+import org.openstreetmap.josm.data.osm.*;
+import org.openstreetmap.josm.gui.dialogs.relation.actions.IRelationEditorActionAccess;
+import org.openstreetmap.josm.plugins.pt_assistant.data.DerivedDataSet;
+import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmStopAreaGroupRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationEditorAccessUtils;
+
+class StopVicinityDerivedDataSet extends DerivedDataSet {
+    private final BBox bBox;
+    private final long editedRelationId;
+    private final Relation stopRelation;
+    private final IRelationEditorActionAccess editorAccess;
+
+    public StopVicinityDerivedDataSet(IRelationEditorActionAccess editorAccess) {
+        super(editorAccess.getEditor().getLayer().getDataSet());
+        this.stopRelation = editorAccess.getEditor().getRelation();
+        this.editorAccess = editorAccess;
+        this.editedRelationId = stopRelation == null ? 0 : stopRelation.getId();
+        this.bBox = new BBox();
+        RelationEditorAccessUtils.streamMembers(editorAccess)
+            // Extra space: Something around 200.500m depending on where we are on the map.
+            .forEach(p -> bBox.addPrimitive(p.getMember(), 0.005));
+
+    }
+
+    @Override
+    protected boolean isIncluded(OsmPrimitive primitive) {
+        return primitive.getType() != OsmPrimitiveType.RELATION
+            // Normal primitives: all in bbox
+            ? bBox.intersects(primitive.getBBox())
+            // Relations: all except the one we edit
+            // todo: restrict this, e.g. only PT relations + multipolygons in bbox
+            : primitive.getId() != editedRelationId;
+    }
+
+    @Override
+    protected void addAdditionalGeometry(AdditionalGeometryAccess addTo) {
+        // Now apply the relation editor changes
+        // Simulate org.openstreetmap.josm.gui.dialogs.relation.actions.SavingAction.applyChanges
+        Relation relation = new Relation();
+        editorAccess.getTagModel().applyToPrimitive(relation);
+        // This is a hack to tag our currently active relation.
+        // There is no id selector in MapCSS, so we need a way to uniquely identify our relation
+        relation.put("activePtRelation", "1");
+
+        if (stopRelation != null) {
+            addTo.addAsCopy(stopRelation, relation);
+
+            // Now we search for all sibling relations.
+            // Due to https://josm.openstreetmap.de/ticket/6129#comment:24 we cannot do it in MapCSS
+            stopRelation.getReferrers()
+                .stream()
+                .filter(r -> r instanceof Relation && OsmStopAreaGroupRelationTags.isStopAreaGroup((Relation) r))
+                .flatMap(parent -> ((Relation) parent).getMembers().stream())
+                .map(RelationMember::getMember)
+                .filter(sibling -> sibling != stopRelation)
+                .filter(sibling -> sibling instanceof Relation) // < some group relations contain invalid members
+                .forEach(sibling -> {
+                    Relation copy = new Relation((Relation) sibling);
+                    copy.put("siblingOfActive", "1");
+                    // This will add the copy with the fake tag.
+                    addOrGetDerived(copy);
+                });
+
+        } else {
+            addTo.add(relation);
+        }
+
+        RelationEditorAccessUtils.getRelationMembers(editorAccess)
+            .forEach(m -> relation.addMember(addOrGetDerivedMember(m)));
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/utils/IncompleteMembersWarningPanel.java
@@ -1,4 +1,4 @@
-package org.openstreetmap.josm.plugins.visualizeroutes.gui.stoparea;
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.utils;
 
 import org.openstreetmap.josm.plugins.visualizeroutes.gui.utils.UnBoldLabel;
 