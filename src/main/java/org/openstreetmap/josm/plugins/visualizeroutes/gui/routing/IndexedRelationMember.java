JOSM/pt_assistant

Allow adding segemts at the end of route (only basic routing supported)

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/IndexedRelationMember.java
@@ -1,0 +1,53 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing;
+
+import java.util.Objects;
+
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.linear.RelationAccess;
+
+/**
+ * A class to store the relation member indexes along with the member
+ * <br/>
+ * A relation may contain the same member with the same role multiple times.
+ * <br/>
+ * Some times, we mix relations (e.g. superroutes). Therefore, we store the relation the member originated from, too.
+ */
+public class IndexedRelationMember {
+    private final RelationMember member;
+    private final int index;
+    private final RelationAccess relation;
+
+    public IndexedRelationMember(RelationMember member, int index, RelationAccess relation) {
+        this.member = Objects.requireNonNull(member, "member");
+        this.index = index;
+        this.relation = Objects.requireNonNull(relation, "relation");
+        if (index < 0 || index >= relation.getMembers().size()) {
+            throw new IllegalArgumentException("Index out of bounds: " + index + " for " + member);
+        }
+        if (relation.getMembers().get(index) != member) {
+            throw new IllegalArgumentException("Relation member index wrong: " + index + " for " + member);
+        }
+    }
+
+    public RelationAccess getRelation() {
+        return relation;
+    }
+
+    public RelationMember getMember() {
+        return member;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+
+    @Override
+    public String toString() {
+        return "IndexedRelationMember{" +
+            "member=" + member +
+            ", index=" + index +
+            ", relation=" + relation +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/RouteTarget.java
@@ -1,0 +1,25 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.openstreetmap.josm.data.osm.Node;
+
+public class RouteTarget {
+    private final List<RouteSegmentWay> trace;
+
+    public RouteTarget(List<RouteSegmentWay> trace) {
+        this.trace = Objects.requireNonNull(trace, "trace");
+        if (trace.size() < 1) {
+            throw new IllegalArgumentException("Trace may not be empty.");
+        }
+    }
+
+    public List<RouteSegmentWay> getTrace() {
+        return trace;
+    }
+
+    public Node getEnd() {
+        return trace.get(trace.size() - 1).lastNode();
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/Router.java
@@ -1,0 +1,127 @@
+package org.openstreetmap.josm.plugins.pt_assistant.gui.routing;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.transportmode.RouteType;
+
+/**
+ * Allows to find possible routes around a given starting point.
+ *
+ * Does a forward search on the route.
+ */
+public class Router {
+    private final RouteSegmentWay startAfter;
+    private final RouteType type;
+
+    public Router(RouteSegmentWay startAfter, RouteType type) {
+        this.startAfter = startAfter;
+        this.type = type;
+        if (startAfter.lastNode().getDataSet() == null) {
+            throw new IllegalArgumentException("The route segment is not in the dataset");
+        }
+    }
+
+    public RouteSegmentWay getStartAfter() {
+        return startAfter;
+    }
+
+    public RouteType getType() {
+        return type;
+    }
+
+    /**
+     * Does a Dijkstra search around the start node to find the next possible ways the route may take.
+     * <br/>
+     * It will only return valid way endpoints as possible points the route could go to.
+     * <br/>
+     * Although we limit by the number of way segments,
+     *
+     * @param distance The minimum distance to search - in meters. Good if a road is split into many small parts.
+     * @param waySegmentCount The mininum way segment count. Good for urban areas where there are not many way splits.
+     * @return The possible route targets, ordered by distance.
+     */
+    public List<RouteTarget> findRouteTargets(int distance, int waySegmentCount) {
+        if (distance <= 0) {
+            throw new IllegalArgumentException("Distance needs to be at least 1");
+        }
+        if (waySegmentCount <= 0) {
+            throw new IllegalArgumentException("waySegmentCount needs to be at least 1");
+        }
+
+        Map<RouteSegmentWay, RouteSegmentWay> parents = new HashMap<>();
+        Set<Node> visited = new HashSet<>();
+        Map<Node, Double> distances = new HashMap<>();
+        HashSet<RouteSegmentWay> toSearchAfterNext = new HashSet<>();
+        List<RouteTarget> targets = new ArrayList<>();
+
+        distances.put(startAfter.firstNode(), -startAfter.getLength());
+        // Put the first one in the list => prevents directly going back that way
+        visited.add(startAfter.firstNode());
+        toSearchAfterNext.add(startAfter);
+
+        while (!toSearchAfterNext.isEmpty()) {
+            RouteSegmentWay next = toSearchAfterNext
+                .stream()
+                .min(Comparator.comparing(it -> distances.get(it.firstNode()) + it.getLength()))
+                .orElseThrow(() -> new IllegalArgumentException("Expected one element in toSearchAfterNext"));
+
+            Double distanceAtStart = distances.get(next.firstNode());
+            if (distanceAtStart == null || Double.isNaN(distanceAtStart)) {
+                throw new IllegalArgumentException("Not a vialid distance: " + distanceAtStart);
+            }
+            double distanceAtEndOfNext = distanceAtStart + next.getLength();
+            distances.put(next.lastNode(), distanceAtEndOfNext);
+            visited.add(next.lastNode());
+            toSearchAfterNext.removeIf(it -> it.lastNode() == next.lastNode());
+            LinkedList<RouteSegmentWay> trace = backtrace(parents, next);
+            targets.add(new RouteTarget(trace));
+
+            next.lastNode()
+                .referrers(Way.class)
+                // We only want ways starting / ending at next.lastNode()
+                .filter(it -> it.isFirstLastNode(next.lastNode()))
+                // We only want ways that we have not seen.
+                // next.lastNode is one end and in the visited set => the other one may not be visited.
+                .filter(it -> !visited.contains(it.firstNode()) || !visited.contains(it.lastNode()))
+                // Now convert to a way segment
+                .map(it -> {
+                    RouteSegmentWay way = type.createRouteSegmentWay(it, it.firstNode().equals(next.lastNode()), startAfter.getIndexInMembers() + trace.size(), trace);
+                    // Skip the ones that are too far away
+                    if (distanceAtEndOfNext + way.getLength() > distance
+                        && parents.size() + 1 > waySegmentCount) {
+                        return null;
+                    } else {
+                        return way;
+                    }
+                })
+                .filter(Objects::nonNull)
+                // Exclude the ones that we cannot go on.
+                .filter(it -> it.getSuitability() == WaySuitability.GOOD)
+                // Handle the way segments => schedule them and mark their parent
+                .forEach(it -> {
+                    parents.put(it, next);
+                    toSearchAfterNext.add(it);
+                });
+        }
+        return targets;
+    }
+
+    private LinkedList<RouteSegmentWay> backtrace(Map<RouteSegmentWay, RouteSegmentWay> parents, RouteSegmentWay next) {
+        LinkedList<RouteSegmentWay> myParents = new LinkedList<>();
+        myParents.addFirst(next);
+        while (parents.containsKey(myParents.getFirst())) {
+            myParents.addFirst(parents.get(myParents.getFirst()));
+        }
+        return myParents;
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/pt_assistant/gui/routing/transportmode/RouteType.java
@@ -4,9 +4,20 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmUtils;
 import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.plugins.customizepublictransportstop.OSMTags;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.IndexedRelationMember;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.RouteSegmentWay;
+import org.openstreetmap.josm.plugins.pt_assistant.gui.routing.WaySuitability;
 
 /**
  * Type of the route relation
@@ -73,6 +84,102 @@ public interface RouteType {
         } else {
             return r.get("restriction");
         }
+    }
+
+    default RouteSegmentWay createRouteSegmentWay(IndexedRelationMember way, boolean forward, List<RouteSegmentWay> previous) {
+        return createRouteSegmentWay(way.getMember().getWay(), forward, way.getIndex(), previous);
+    }
+
+    default RouteSegmentWay createRouteSegmentWay(Way way, boolean forward, int indexInMembers, List<RouteSegmentWay> previous) {
+        // Check way is suited
+        RouteType.AccessDirection mayDriveOn = mayDriveOn(way.getKeys());
+        WaySuitability suitability;
+        if ((mayDriveOn == RouteType.AccessDirection.FORWARD_ONLY && !forward)
+            || mayDriveOn == RouteType.AccessDirection.BACKWARD_ONLY && forward) {
+            suitability = WaySuitability.WRONG_DIRECTION;
+        } else if (mayDriveOn == RouteType.AccessDirection.NONE) {
+            suitability = WaySuitability.WRONG_TYPE;
+        } else {
+            // Negative turn restricitons (preventing us from turning into this way)
+            // Single via node matches
+            Optional<Relation> wrongTurnRestriction = findTurnRestrictions(way, "no_")
+                .filter(r -> r.findRelationMembers("to").contains(way))
+                .filter(r -> {
+                    Set<Way> fromPath = findFromPath(r);
+                    if (fromPath.size() < previous.size()) {
+                        return false;
+                    } else {
+                        Set<Way> whereWeComeFrom = previous.subList(previous.size() - fromPath.size(), previous.size())
+                            .stream().map(RouteSegmentWay::getWay).collect(Collectors.toSet());
+                        return fromPath.equals(whereWeComeFrom)
+                            && (fromPath.size() > 1
+                            // Single via node matches
+                            || getViaNode(r).map(via -> previous.get(previous.size() - 1).lastNode().equals(via)).orElse(true));
+                    }
+                })
+                .findFirst();
+            if (wrongTurnRestriction.isPresent()) {
+                suitability = WaySuitability.CANNOT_TURN_INTO;
+            } else {
+                if (previous.size() > 0 && findTurnRestrictions(previous.get(previous.size() - 1).getWay(), "only_")
+                    // Only relations starting at previous way
+                    .filter(restriction -> restriction.getMembers()
+                        .stream()
+                        .filter(RelationMember::isWay)
+                        .filter(member -> "from".equals(member.getRole()))
+                        .map(RelationMember::getWay)
+                        .anyMatch(previous.get(previous.size() - 1).getWay()::equals))
+                    // Only restrictions continuing in our direction
+                    .anyMatch(restriction -> getViaNode(restriction)
+                        .map(viaNode -> previous.get(previous.size() - 1).lastNode().equals(viaNode))
+                        .orElseGet(() -> restriction.getMembers()
+                            .stream()
+                            .filter(RelationMember::isWay)
+                            .map(RelationMember::getWay)
+                            .anyMatch(way::equals)))) {
+                    suitability = WaySuitability.INVALID_TURN_FROM;
+                } else {
+                    suitability = WaySuitability.GOOD;
+                }
+            }
+        }
+
+        return new RouteSegmentWay(way, forward, indexInMembers, suitability);
+    }
+
+    /**
+     * Stream all turn restricitons of that type
+     * @param way The way the turn restriction should be on (no matter if it is from / to / via)
+     * @param prefix The prefix the turn restriction type needs to have
+     * @return A stream of those relations
+     */
+    default Stream<Relation> findTurnRestrictions(Way way, String prefix) {
+        return way.referrers(Relation.class)
+            .filter(r -> r.hasTag(OSMTags.KEY_RELATION_TYPE, OSMTags.RELATION_TYPE_TURN_RESTRICTION))
+            .filter(r -> {
+                String restrictionValue = getRestrictionValue(r);
+                return restrictionValue != null && restrictionValue.startsWith(prefix);
+            });
+    }
+
+    static Optional<Node> getViaNode(Relation restriction) {
+        return restriction
+            .getMembers()
+            .stream()
+            .filter(RelationMember::isNode)
+            .filter(it -> "via".equals(it.getRole()))
+            .map(RelationMember::getNode)
+            .findFirst();
+    }
+
+    static Set<Way> findFromPath(Relation restriction) {
+        return restriction
+            .getMembers()
+            .stream()
+            .filter(RelationMember::isWay)
+            .filter(it -> "via".equals(it.getRole()) || "from".equals(it.getRole()))
+            .map(RelationMember::getWay)
+            .collect(Collectors.toSet());
     }
 
     enum AccessDirection {

michaelzangl/josm-visualize-routes

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/IndexedRelationMember.java
@@ -1,0 +1,53 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing;
+
+import java.util.Objects;
+
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.linear.RelationAccess;
+
+/**
+ * A class to store the relation member indexes along with the member
+ * <br/>
+ * A relation may contain the same member with the same role multiple times.
+ * <br/>
+ * Some times, we mix relations (e.g. superroutes). Therefore, we store the relation the member originated from, too.
+ */
+public class IndexedRelationMember {
+    private final RelationMember member;
+    private final int index;
+    private final RelationAccess relation;
+
+    public IndexedRelationMember(RelationMember member, int index, RelationAccess relation) {
+        this.member = Objects.requireNonNull(member, "member");
+        this.index = index;
+        this.relation = Objects.requireNonNull(relation, "relation");
+        if (index < 0 || index >= relation.getMembers().size()) {
+            throw new IllegalArgumentException("Index out of bounds: " + index + " for " + member);
+        }
+        if (relation.getMembers().get(index) != member) {
+            throw new IllegalArgumentException("Relation member index wrong: " + index + " for " + member);
+        }
+    }
+
+    public RelationAccess getRelation() {
+        return relation;
+    }
+
+    public RelationMember getMember() {
+        return member;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+
+    @Override
+    public String toString() {
+        return "IndexedRelationMember{" +
+            "member=" + member +
+            ", index=" + index +
+            ", relation=" + relation +
+            '}';
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/RouteTarget.java
@@ -1,0 +1,25 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.openstreetmap.josm.data.osm.Node;
+
+public class RouteTarget {
+    private final List<RouteSegmentWay> trace;
+
+    public RouteTarget(List<RouteSegmentWay> trace) {
+        this.trace = Objects.requireNonNull(trace, "trace");
+        if (trace.size() < 1) {
+            throw new IllegalArgumentException("Trace may not be empty.");
+        }
+    }
+
+    public List<RouteSegmentWay> getTrace() {
+        return trace;
+    }
+
+    public Node getEnd() {
+        return trace.get(trace.size() - 1).lastNode();
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/Router.java
@@ -1,0 +1,127 @@
+package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.transportmode.RouteType;
+
+/**
+ * Allows to find possible routes around a given starting point.
+ *
+ * Does a forward search on the route.
+ */
+public class Router {
+    private final RouteSegmentWay startAfter;
+    private final RouteType type;
+
+    public Router(RouteSegmentWay startAfter, RouteType type) {
+        this.startAfter = startAfter;
+        this.type = type;
+        if (startAfter.lastNode().getDataSet() == null) {
+            throw new IllegalArgumentException("The route segment is not in the dataset");
+        }
+    }
+
+    public RouteSegmentWay getStartAfter() {
+        return startAfter;
+    }
+
+    public RouteType getType() {
+        return type;
+    }
+
+    /**
+     * Does a Dijkstra search around the start node to find the next possible ways the route may take.
+     * <br/>
+     * It will only return valid way endpoints as possible points the route could go to.
+     * <br/>
+     * Although we limit by the number of way segments,
+     *
+     * @param distance The minimum distance to search - in meters. Good if a road is split into many small parts.
+     * @param waySegmentCount The mininum way segment count. Good for urban areas where there are not many way splits.
+     * @return The possible route targets, ordered by distance.
+     */
+    public List<RouteTarget> findRouteTargets(int distance, int waySegmentCount) {
+        if (distance <= 0) {
+            throw new IllegalArgumentException("Distance needs to be at least 1");
+        }
+        if (waySegmentCount <= 0) {
+            throw new IllegalArgumentException("waySegmentCount needs to be at least 1");
+        }
+
+        Map<RouteSegmentWay, RouteSegmentWay> parents = new HashMap<>();
+        Set<Node> visited = new HashSet<>();
+        Map<Node, Double> distances = new HashMap<>();
+        HashSet<RouteSegmentWay> toSearchAfterNext = new HashSet<>();
+        List<RouteTarget> targets = new ArrayList<>();
+
+        distances.put(startAfter.firstNode(), -startAfter.getLength());
+        // Put the first one in the list => prevents directly going back that way
+        visited.add(startAfter.firstNode());
+        toSearchAfterNext.add(startAfter);
+
+        while (!toSearchAfterNext.isEmpty()) {
+            RouteSegmentWay next = toSearchAfterNext
+                .stream()
+                .min(Comparator.comparing(it -> distances.get(it.firstNode()) + it.getLength()))
+                .orElseThrow(() -> new IllegalArgumentException("Expected one element in toSearchAfterNext"));
+
+            Double distanceAtStart = distances.get(next.firstNode());
+            if (distanceAtStart == null || Double.isNaN(distanceAtStart)) {
+                throw new IllegalArgumentException("Not a vialid distance: " + distanceAtStart);
+            }
+            double distanceAtEndOfNext = distanceAtStart + next.getLength();
+            distances.put(next.lastNode(), distanceAtEndOfNext);
+            visited.add(next.lastNode());
+            toSearchAfterNext.removeIf(it -> it.lastNode() == next.lastNode());
+            LinkedList<RouteSegmentWay> trace = backtrace(parents, next);
+            targets.add(new RouteTarget(trace));
+
+            next.lastNode()
+                .referrers(Way.class)
+                // We only want ways starting / ending at next.lastNode()
+                .filter(it -> it.isFirstLastNode(next.lastNode()))
+                // We only want ways that we have not seen.
+                // next.lastNode is one end and in the visited set => the other one may not be visited.
+                .filter(it -> !visited.contains(it.firstNode()) || !visited.contains(it.lastNode()))
+                // Now convert to a way segment
+                .map(it -> {
+                    RouteSegmentWay way = type.createRouteSegmentWay(it, it.firstNode().equals(next.lastNode()), startAfter.getIndexInMembers() + trace.size(), trace);
+                    // Skip the ones that are too far away
+                    if (distanceAtEndOfNext + way.getLength() > distance
+                        && parents.size() + 1 > waySegmentCount) {
+                        return null;
+                    } else {
+                        return way;
+                    }
+                })
+                .filter(Objects::nonNull)
+                // Exclude the ones that we cannot go on.
+                .filter(it -> it.getSuitability() == WaySuitability.GOOD)
+                // Handle the way segments => schedule them and mark their parent
+                .forEach(it -> {
+                    parents.put(it, next);
+                    toSearchAfterNext.add(it);
+                });
+        }
+        return targets;
+    }
+
+    private LinkedList<RouteSegmentWay> backtrace(Map<RouteSegmentWay, RouteSegmentWay> parents, RouteSegmentWay next) {
+        LinkedList<RouteSegmentWay> myParents = new LinkedList<>();
+        myParents.addFirst(next);
+        while (parents.containsKey(myParents.getFirst())) {
+            myParents.addFirst(parents.get(myParents.getFirst()));
+        }
+        return myParents;
+    }
+}

src/main/java/org/openstreetmap/josm/plugins/visualizeroutes/gui/routing/transportmode/RouteType.java
@@ -1,12 +1,14 @@
 package org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.transportmode;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import org.openstreetmap.josm.data.osm.*;
+import org.openstreetmap.josm.plugins.visualizeroutes.constants.OsmTurnRestrictionRelationTags;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.IndexedRelationMember;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.RouteSegmentWay;
+import org.openstreetmap.josm.plugins.visualizeroutes.gui.routing.WaySuitability;
 
-import org.openstreetmap.josm.data.osm.OsmUtils;
-import org.openstreetmap.josm.data.osm.Relation;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Type of the route relation
@@ -73,6 +75,102 @@ public interface RouteType {
         } else {
             return r.get("restriction");
         }
+    }
+
+    default RouteSegmentWay createRouteSegmentWay(IndexedRelationMember way, boolean forward, List<RouteSegmentWay> previous) {
+        return createRouteSegmentWay(way.getMember().getWay(), forward, way.getIndex(), previous);
+    }
+
+    default RouteSegmentWay createRouteSegmentWay(Way way, boolean forward, int indexInMembers, List<RouteSegmentWay> previous) {
+        // Check way is suited
+        RouteType.AccessDirection mayDriveOn = mayDriveOn(way.getKeys());
+        WaySuitability suitability;
+        if ((mayDriveOn == RouteType.AccessDirection.FORWARD_ONLY && !forward)
+            || mayDriveOn == RouteType.AccessDirection.BACKWARD_ONLY && forward) {
+            suitability = WaySuitability.WRONG_DIRECTION;
+        } else if (mayDriveOn == RouteType.AccessDirection.NONE) {
+            suitability = WaySuitability.WRONG_TYPE;
+        } else {
+            // Negative turn restricitons (preventing us from turning into this way)
+            // Single via node matches
+            Optional<Relation> wrongTurnRestriction = findTurnRestrictions(way, "no_")
+                .filter(r -> r.findRelationMembers("to").contains(way))
+                .filter(r -> {
+                    Set<Way> fromPath = findFromPath(r);
+                    if (fromPath.size() < previous.size()) {
+                        return false;
+                    } else {
+                        Set<Way> whereWeComeFrom = previous.subList(previous.size() - fromPath.size(), previous.size())
+                            .stream().map(RouteSegmentWay::getWay).collect(Collectors.toSet());
+                        return fromPath.equals(whereWeComeFrom)
+                            && (fromPath.size() > 1
+                            // Single via node matches
+                            || getViaNode(r).map(via -> previous.get(previous.size() - 1).lastNode().equals(via)).orElse(true));
+                    }
+                })
+                .findFirst();
+            if (wrongTurnRestriction.isPresent()) {
+                suitability = WaySuitability.CANNOT_TURN_INTO;
+            } else {
+                if (previous.size() > 0 && findTurnRestrictions(previous.get(previous.size() - 1).getWay(), "only_")
+                    // Only relations starting at previous way
+                    .filter(restriction -> restriction.getMembers()
+                        .stream()
+                        .filter(RelationMember::isWay)
+                        .filter(member -> "from".equals(member.getRole()))
+                        .map(RelationMember::getWay)
+                        .anyMatch(previous.get(previous.size() - 1).getWay()::equals))
+                    // Only restrictions continuing in our direction
+                    .anyMatch(restriction -> getViaNode(restriction)
+                        .map(viaNode -> previous.get(previous.size() - 1).lastNode().equals(viaNode))
+                        .orElseGet(() -> restriction.getMembers()
+                            .stream()
+                            .filter(RelationMember::isWay)
+                            .map(RelationMember::getWay)
+                            .anyMatch(way::equals)))) {
+                    suitability = WaySuitability.INVALID_TURN_FROM;
+                } else {
+                    suitability = WaySuitability.GOOD;
+                }
+            }
+        }
+
+        return new RouteSegmentWay(way, forward, indexInMembers, suitability);
+    }
+
+    /**
+     * Stream all turn restricitons of that type
+     * @param way The way the turn restriction should be on (no matter if it is from / to / via)
+     * @param prefix The prefix the turn restriction type needs to have
+     * @return A stream of those relations
+     */
+    default Stream<Relation> findTurnRestrictions(Way way, String prefix) {
+        return way.referrers(Relation.class)
+            .filter(r -> r.hasTag(OsmTurnRestrictionRelationTags.KEY_TYPE, OsmTurnRestrictionRelationTags.KEY_TYPE_VALUE_TURN_RESTRICTION))
+            .filter(r -> {
+                String restrictionValue = getRestrictionValue(r);
+                return restrictionValue != null && restrictionValue.startsWith(prefix);
+            });
+    }
+
+    static Optional<Node> getViaNode(Relation restriction) {
+        return restriction
+            .getMembers()
+            .stream()
+            .filter(RelationMember::isNode)
+            .filter(it -> "via".equals(it.getRole()))
+            .map(RelationMember::getNode)
+            .findFirst();
+    }
+
+    static Set<Way> findFromPath(Relation restriction) {
+        return restriction
+            .getMembers()
+            .stream()
+            .filter(RelationMember::isWay)
+            .filter(it -> "via".equals(it.getRole()) || "from".equals(it.getRole()))
+            .map(RelationMember::getWay)
+            .collect(Collectors.toSet());
     }
 
     enum AccessDirection {